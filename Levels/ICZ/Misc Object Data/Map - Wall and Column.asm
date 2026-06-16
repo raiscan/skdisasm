57307927c8bfdd1f29adb9e52ec1018f0641cb38
Farrell <farrell@bluetoaster.net>
2026-06-16 17:15:31 +0100

    docs: label ICZ wall and column map frames

--- Levels/ICZ/Misc Object Data/Map - Wall and Column.asm
+++ Levels/ICZ/Misc Object Data/Map - Wall and Column.asm
@@ -1,19 +1,19 @@
 Map_ICZWallAndColumn_:
-		dc.w Frame_3639DC-Map_ICZWallAndColumn_
-		dc.w Frame_363A3E-Map_ICZWallAndColumn_
-		dc.w Frame_363AC4-Map_ICZWallAndColumn_
-		dc.w Frame_363B1A-Map_ICZWallAndColumn_
-		dc.w Frame_363B28-Map_ICZWallAndColumn_
-		dc.w Frame_363B30-Map_ICZWallAndColumn_
-		dc.w Frame_363B38-Map_ICZWallAndColumn_
-		dc.w Frame_363B6A-Map_ICZWallAndColumn_
-		dc.w Frame_363B7E-Map_ICZWallAndColumn_
-		dc.w Frame_363B8C-Map_ICZWallAndColumn_
-		dc.w Frame_363BAC-Map_ICZWallAndColumn_
-		dc.w Frame_363BBA-Map_ICZWallAndColumn_
-		dc.w Frame_363BBC-Map_ICZWallAndColumn_
-		dc.w Frame_363C12-Map_ICZWallAndColumn_
-Frame_3639DC:	dc.w $10
+		dc.w Map_ICZWallAndColumn_Frame0-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame1-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame2-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame3-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame4-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame5-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame6-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame7-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame8-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame9-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame10-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame11-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame12-Map_ICZWallAndColumn_
+		dc.w Map_ICZWallAndColumn_Frame13-Map_ICZWallAndColumn_
+Map_ICZWallAndColumn_Frame0:	dc.w $10	; Frame_3639DC
 		dc.b  $D4,  $D,   0,   0, $FF, $D0
 		dc.b  $D4,  $D,   0,   8, $FF, $F0
 		dc.b  $D4,  $D,   0, $10,   0, $10
@@ -30,7 +30,7 @@ Frame_3639DC:	dc.w $10
 		dc.b  $14,  $D,   0, $40, $FF, $F0
 		dc.b  $14,  $C,   0, $48,   0, $10
 		dc.b  $24,  $C,   0, $48, $FF, $D0
-Frame_363A3E:	dc.w $16
+Map_ICZWallAndColumn_Frame1:	dc.w $16	; Frame_363A3E
 		dc.b  $DC,  $D,   0,   0, $FF, $B0
 		dc.b  $DC,  $D,   0,   8, $FF, $D0
 		dc.b  $DC,  $D,   8,   8, $FF, $F0
@@ -53,7 +53,7 @@ Frame_363A3E:	dc.w $16
 		dc.b   $C,  $D,   8, $40,   0, $30
 		dc.b  $1C,  $C,   8, $48, $FF, $D0
 		dc.b  $1C,  $C,   0, $48, $FF, $F0
-Frame_363AC4:	dc.w $E
+Map_ICZWallAndColumn_Frame2:	dc.w $E	; Frame_363AC4
 		dc.b  $90,  $F,   0, $4C, $FF, $E0
 		dc.b  $90,  $F,   8, $4C,   0,   0
 		dc.b  $B0,  $F,   0, $5C, $FF, $E0
@@ -68,14 +68,14 @@ Frame_363AC4:	dc.w $E
 		dc.b  $30,  $F,   0, $6C,   0,   0
 		dc.b  $50,  $F, $10, $4C, $FF, $E0
 		dc.b  $50,  $F, $18, $4C,   0,   0
-Frame_363B1A:	dc.w 2
+Map_ICZWallAndColumn_Frame3:	dc.w 2	; Frame_363B1A
 		dc.b  $F0,  $F,   0, $4C, $FF, $E0
 		dc.b  $F0,  $F,   8, $4C,   0,   0
-Frame_363B28:	dc.w 1
+Map_ICZWallAndColumn_Frame4:	dc.w 1	; Frame_363B28
 		dc.b  $F0,   7,   0, $8C, $FF, $F8
-Frame_363B30:	dc.w 1
+Map_ICZWallAndColumn_Frame5:	dc.w 1	; Frame_363B30
 		dc.b  $F0,  $B,   0, $94, $FF, $F4
-Frame_363B38:	dc.w 8
+Map_ICZWallAndColumn_Frame6:	dc.w 8	; Frame_363B38
 		dc.b  $C0,  $F,   0, $5C, $FF, $E0
 		dc.b  $C0,  $F,   0, $6C,   0,   0
 		dc.b  $E0,  $F, $18, $6C, $FF, $E0
@@ -84,24 +84,24 @@ Frame_363B38:	dc.w 8
 		dc.b    0,  $F,   0, $6C,   0,   0
 		dc.b  $20,  $F, $18, $6C, $FF, $E0
 		dc.b  $20,  $F, $18, $5C,   0,   0
-Frame_363B6A:	dc.w 3
+Map_ICZWallAndColumn_Frame7:	dc.w 3	; Frame_363B6A
 		dc.b  $E0,   7,   0, $A0, $FF, $F0
 		dc.b  $E0,   7,   8, $A0,   0,   0
 		dc.b    0,   7,   0, $8C, $FF, $F8
-Frame_363B7E:	dc.w 2
+Map_ICZWallAndColumn_Frame8:	dc.w 2	; Frame_363B7E
 		dc.b  $F4,  $D,   0, $38, $FF, $F0
 		dc.b    4,  $C,   0, $48, $FF, $F0
-Frame_363B8C:	dc.w 5
+Map_ICZWallAndColumn_Frame9:	dc.w 5	; Frame_363B8C
 		dc.b  $DC,  $D,   0, $AC, $FF, $F0
 		dc.b  $EC,  $D,   0, $18, $FF, $F0
 		dc.b  $FC,  $D,   0, $28, $FF, $F0
 		dc.b   $C,  $D,   0, $38, $FF, $F0
 		dc.b  $1C,  $C,   0, $48, $FF, $F0
-Frame_363BAC:	dc.w 2
+Map_ICZWallAndColumn_Frame10:	dc.w 2	; Frame_363BAC
 		dc.b  $F0,  $F,   0, $7C, $FF, $E0
 		dc.b  $F0,  $F, $18, $7C,   0,   0
-Frame_363BBA:	dc.w 0
-Frame_363BBC:	dc.w $E
+Map_ICZWallAndColumn_Frame11:	dc.w 0	; Frame_363BBA
+Map_ICZWallAndColumn_Frame12:	dc.w $E	; Frame_363BBC
 		dc.b  $90,  $F,   0, $4C, $FF, $E0
 		dc.b  $90,  $F,   8, $4C,   0,   0
 		dc.b  $B0,  $F,   0, $5C, $FF, $E0
@@ -116,7 +116,7 @@ Frame_363BBC:	dc.w $E
 		dc.b  $30,  $F,   0, $6C,   0,   0
 		dc.b  $50,  $F, $18, $6C, $FF, $E0
 		dc.b  $50,  $F, $18, $5C,   0,   0
-Frame_363C12:	dc.w 8
+Map_ICZWallAndColumn_Frame13:	dc.w 8	; Frame_363C12
 		dc.b  $C0,  $F, $18, $6C, $FF, $E0
 		dc.b  $C0,  $F, $18, $5C,   0,   0
 		dc.b  $E0,  $F,   0, $5C, $FF, $E0
