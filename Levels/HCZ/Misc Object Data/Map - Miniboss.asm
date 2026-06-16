3bda0b1f1e10e51bda67dc305e98bd72d6d08618
Farrell <farrell@bluetoaster.net>
2026-06-16 07:36:25 +0100

    docs: label HCZ miniboss mappings

--- Levels/HCZ/Misc Object Data/Map - Miniboss.asm
+++ Levels/HCZ/Misc Object Data/Map - Miniboss.asm
@@ -1,90 +1,90 @@
 Map_HCZMiniboss_:
-		dc.w Frame_362A28-Map_HCZMiniboss_
-		dc.w Frame_362A42-Map_HCZMiniboss_
-		dc.w Frame_362A4A-Map_HCZMiniboss_
-		dc.w Frame_362A52-Map_HCZMiniboss_
-		dc.w Frame_362A5A-Map_HCZMiniboss_
-		dc.w Frame_362A68-Map_HCZMiniboss_
-		dc.w Frame_362A70-Map_HCZMiniboss_
-		dc.w Frame_362A78-Map_HCZMiniboss_
-		dc.w Frame_362A80-Map_HCZMiniboss_
-		dc.w Frame_362A88-Map_HCZMiniboss_
-		dc.w Frame_362A90-Map_HCZMiniboss_
-		dc.w Frame_362A98-Map_HCZMiniboss_
-		dc.w Frame_362AA0-Map_HCZMiniboss_
-		dc.w Frame_362AA8-Map_HCZMiniboss_
-		dc.w Frame_362AB0-Map_HCZMiniboss_
-		dc.w Frame_362AB8-Map_HCZMiniboss_
-		dc.w Frame_362AC0-Map_HCZMiniboss_
-		dc.w Frame_362AC8-Map_HCZMiniboss_
-		dc.w Frame_362AD0-Map_HCZMiniboss_
-		dc.w Frame_362ADE-Map_HCZMiniboss_
-		dc.w Frame_362AE6-Map_HCZMiniboss_
-		dc.w Frame_362AEE-Map_HCZMiniboss_
-		dc.w Frame_362AF6-Map_HCZMiniboss_
-		dc.w Frame_362B40-Map_HCZMiniboss_
-		dc.w Frame_362B78-Map_HCZMiniboss_
-		dc.w Frame_362BB0-Map_HCZMiniboss_
-		dc.w Frame_362BB0-Map_HCZMiniboss_
-		dc.w Frame_362BB2-Map_HCZMiniboss_
-		dc.w Frame_362BBA-Map_HCZMiniboss_
-		dc.w Frame_362BC2-Map_HCZMiniboss_
-		dc.w Frame_362BCA-Map_HCZMiniboss_
-		dc.w Frame_362BD2-Map_HCZMiniboss_
-		dc.w Frame_362BE0-Map_HCZMiniboss_
-		dc.w Frame_362BF4-Map_HCZMiniboss_
-		dc.w Frame_362C08-Map_HCZMiniboss_
-		dc.w Frame_362C16-Map_HCZMiniboss_
-Frame_362A28:	dc.w 4
+		dc.w Map_HCZMiniboss_Frame0-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame1-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame2-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame3-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame4-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame5-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame6-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame7-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame8-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame9-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame10-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame11-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame12-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame13-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame14-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame15-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame16-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame17-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame18-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame19-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame20-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame21-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame22-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame23-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame24-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame25-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame25-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame27-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame28-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame29-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame30-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame31-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame32-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame33-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame34-Map_HCZMiniboss_
+		dc.w Map_HCZMiniboss_Frame35-Map_HCZMiniboss_
+Map_HCZMiniboss_Frame0:	dc.w 4
 		dc.b  $E8,  $A,   0,   0, $FF, $E8
 		dc.b    0,  $A,   0,   9, $FF, $E8
 		dc.b  $E8,  $A,   8,   0,   0,   0
 		dc.b    0,  $A,   8,   9,   0,   0
-Frame_362A42:	dc.w 1
+Map_HCZMiniboss_Frame1:	dc.w 1
 		dc.b  $F8,   5,   0, $12, $FF, $F8
-Frame_362A4A:	dc.w 1
+Map_HCZMiniboss_Frame2:	dc.w 1
 		dc.b  $F4,  $A,   0, $16, $FF, $F4
-Frame_362A52:	dc.w 1
+Map_HCZMiniboss_Frame3:	dc.w 1
 		dc.b  $F4,  $A,   0, $1F, $FF, $F4
-Frame_362A5A:	dc.w 2
+Map_HCZMiniboss_Frame4:	dc.w 2
 		dc.b  $F2,   9,   0, $28, $FF, $F0
 		dc.b    0,  $D,   0, $2E, $FF, $F2
-Frame_362A68:	dc.w 1
+Map_HCZMiniboss_Frame5:	dc.w 1
 		dc.b  $F0,  $F,   0, $36, $FF, $F0
-Frame_362A70:	dc.w 1
+Map_HCZMiniboss_Frame6:	dc.w 1
 		dc.b  $F0,  $F,   0, $46, $FF, $F0
-Frame_362A78:	dc.w 1
+Map_HCZMiniboss_Frame7:	dc.w 1
 		dc.b  $F4,  $A,   0, $56, $FF, $F4
-Frame_362A80:	dc.w 1
+Map_HCZMiniboss_Frame8:	dc.w 1
 		dc.b  $F4,  $A,   0, $5F, $FF, $F4
-Frame_362A88:	dc.w 1
+Map_HCZMiniboss_Frame9:	dc.w 1
 		dc.b  $F8,   5,   0, $68, $FF, $F8
-Frame_362A90:	dc.w 1
+Map_HCZMiniboss_Frame10:	dc.w 1
 		dc.b  $F7,  $A,   0, $6C, $FF, $F7
-Frame_362A98:	dc.w 1
+Map_HCZMiniboss_Frame11:	dc.w 1
 		dc.b  $F7,  $A,   0, $75, $FF, $F7
-Frame_362AA0:	dc.w 1
+Map_HCZMiniboss_Frame12:	dc.w 1
 		dc.b  $F4,  $A,   0, $7E, $FF, $F4
-Frame_362AA8:	dc.w 1
+Map_HCZMiniboss_Frame13:	dc.w 1
 		dc.b  $F6,  $A,   0, $87, $FF, $F6
-Frame_362AB0:	dc.w 1
+Map_HCZMiniboss_Frame14:	dc.w 1
 		dc.b  $F4,  $A,   0, $90, $FF, $F4
-Frame_362AB8:	dc.w 1
+Map_HCZMiniboss_Frame15:	dc.w 1
 		dc.b  $F4,  $A,   0, $99, $FF, $F4
-Frame_362AC0:	dc.w 1
+Map_HCZMiniboss_Frame16:	dc.w 1
 		dc.b  $F4,  $A,   0, $A2, $FF, $F4
-Frame_362AC8:	dc.w 1
+Map_HCZMiniboss_Frame17:	dc.w 1
 		dc.b  $F4,  $A,   0, $AB, $FF, $F4
-Frame_362AD0:	dc.w 2
+Map_HCZMiniboss_Frame18:	dc.w 2
 		dc.b  $F0,   4,   0, $B4, $FF, $F8
 		dc.b  $F8,  $E,   0, $B6, $FF, $F0
-Frame_362ADE:	dc.w 1
+Map_HCZMiniboss_Frame19:	dc.w 1
 		dc.b  $F4,  $A,   0, $C2, $FF, $F4
-Frame_362AE6:	dc.w 1
+Map_HCZMiniboss_Frame20:	dc.w 1
 		dc.b  $F4,  $A,   0, $CB, $FF, $F4
-Frame_362AEE:	dc.w 1
+Map_HCZMiniboss_Frame21:	dc.w 1
 		dc.b  $F4,  $A,   0, $D4, $FF, $F4
-Frame_362AF6:	dc.w $C
+Map_HCZMiniboss_Frame22:	dc.w $C
 		dc.b  $D8,   4,   0, $DD, $FF, $F0
 		dc.b  $D8,   4,   8, $DD,   0,   0
 		dc.b  $E0,   6,   0, $DF, $FF, $F0
@@ -97,7 +97,7 @@ Frame_362AF6:	dc.w $C
 		dc.b  $28,   4,   8, $EE,   0,   0
 		dc.b  $30,   8,   0, $F0, $FF, $E8
 		dc.b  $30,   8,   8, $F0,   0,   0
-Frame_362B40:	dc.w 9
+Map_HCZMiniboss_Frame23:	dc.w 9
 		dc.b  $D8,   4,   0, $DD, $FF, $F0
 		dc.b  $D8,   4,   8, $DD,   0,   0
 		dc.b  $E0,  $A,   0, $E5, $FF, $F4
@@ -107,7 +107,7 @@ Frame_362B40:	dc.w 9
 		dc.b  $28,   4,   8, $EE,   0,   0
 		dc.b  $30,  $C,   0, $F3, $FF, $EC
 		dc.b  $30,   0,   0, $F7,   0,  $C
-Frame_362B78:	dc.w 9
+Map_HCZMiniboss_Frame24:	dc.w 9
 		dc.b  $D8,   4,   8, $DD,   0,   0
 		dc.b  $D8,   4,   0, $DD, $FF, $F0
 		dc.b  $E0,  $A,   8, $E5, $FF, $F4
@@ -117,29 +117,29 @@ Frame_362B78:	dc.w 9
 		dc.b  $28,   4,   0, $EE, $FF, $F0
 		dc.b  $30,  $C,   8, $F3, $FF, $F4
 		dc.b  $30,   0,   8, $F7, $FF, $EC
-Frame_362BB0:	dc.w 0
-Frame_362BB2:	dc.w 1
+Map_HCZMiniboss_Frame25:	dc.w 0
+Map_HCZMiniboss_Frame27:	dc.w 1
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-Frame_362BBA:	dc.w 1
+Map_HCZMiniboss_Frame28:	dc.w 1
 		dc.b  $F4,  $A,   0,   9, $FF, $F4
-Frame_362BC2:	dc.w 1
+Map_HCZMiniboss_Frame29:	dc.w 1
 		dc.b  $F4,  $A,   8,   0, $FF, $F4
-Frame_362BCA:	dc.w 1
+Map_HCZMiniboss_Frame30:	dc.w 1
 		dc.b  $F4,  $A,   8,   9, $FF, $F4
-Frame_362BD2:	dc.w 2
+Map_HCZMiniboss_Frame31:	dc.w 2
 		dc.b  $FC,   4,   0, $DD, $FF, $F0
 		dc.b  $FC,   4,   8, $DD,   0,   0
-Frame_362BE0:	dc.w 3
+Map_HCZMiniboss_Frame32:	dc.w 3
 		dc.b  $E0,   6,   0, $DF, $FF, $F8
 		dc.b  $F8,   6,   0, $DF, $FF, $F8
 		dc.b  $10,   6,   0, $DF, $FF, $F8
-Frame_362BF4:	dc.w 3
+Map_HCZMiniboss_Frame33:	dc.w 3
 		dc.b  $E0,   6,   8, $DF, $FF, $F8
 		dc.b  $F8,   6,   8, $DF, $FF, $F8
 		dc.b  $10,   6,   8, $DF, $FF, $F8
-Frame_362C08:	dc.w 2
+Map_HCZMiniboss_Frame34:	dc.w 2
 		dc.b  $F8,   4,   0, $EE, $FF, $FC
 		dc.b    0,   8,   0, $F0, $FF, $F4
-Frame_362C16:	dc.w 2
+Map_HCZMiniboss_Frame35:	dc.w 2
 		dc.b  $F8,   4,   8, $EE, $FF, $F4
 		dc.b    0,   8,   8, $F0, $FF, $F4
