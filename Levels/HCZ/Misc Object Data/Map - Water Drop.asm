e107917a672ce7daf0571b53b3fd7ea7b8c46fd4
Farrell <farrell@bluetoaster.net>
2026-06-16 07:58:19 +0100

    docs: label HCZ water drop mappings

--- Levels/HCZ/Misc Object Data/Map - Water Drop.asm
+++ Levels/HCZ/Misc Object Data/Map - Water Drop.asm
@@ -1,22 +1,22 @@
 Map_HCZWaterDrop_:
-		dc.w Frame_23795C-Map_HCZWaterDrop_
-		dc.w Frame_237964-Map_HCZWaterDrop_
-		dc.w Frame_23796C-Map_HCZWaterDrop_
-		dc.w Frame_237974-Map_HCZWaterDrop_
-		dc.w Frame_23797C-Map_HCZWaterDrop_
-		dc.w Frame_237984-Map_HCZWaterDrop_
-		dc.w Frame_23798C-Map_HCZWaterDrop_
-Frame_23795C:	dc.w 1
+		dc.w Map_HCZWaterDrop_Frame0-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame1-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame2-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame3-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame4-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame5-Map_HCZWaterDrop_
+		dc.w Map_HCZWaterDrop_Frame6-Map_HCZWaterDrop_
+Map_HCZWaterDrop_Frame0:	dc.w 1
 		dc.b  $F8,   4,   0,   0, $FF, $F8
-Frame_237964:	dc.w 1
+Map_HCZWaterDrop_Frame1:	dc.w 1
 		dc.b  $F8,   4,   0,   2, $FF, $F8
-Frame_23796C:	dc.w 1
+Map_HCZWaterDrop_Frame2:	dc.w 1
 		dc.b  $F8,   1,   0,   4, $FF, $FC
-Frame_237974:	dc.w 1
+Map_HCZWaterDrop_Frame3:	dc.w 1
 		dc.b  $F8,   1,   0,   6, $FF, $FC
-Frame_23797C:	dc.w 1
+Map_HCZWaterDrop_Frame4:	dc.w 1
 		dc.b    0,   4,   0,   8, $FF, $F8
-Frame_237984:	dc.w 1
+Map_HCZWaterDrop_Frame5:	dc.w 1
 		dc.b    0,   4,   0,  $A, $FF, $F8
-Frame_23798C:	dc.w 1
+Map_HCZWaterDrop_Frame6:	dc.w 1
 		dc.b  $FC,   0, $FC, $A4, $FF, $FC
