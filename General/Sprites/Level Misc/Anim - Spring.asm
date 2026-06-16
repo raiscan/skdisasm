db5b37e6167ab5988e566510ebbf35b8f234a432
Farrell <farrell@bluetoaster.net>
2026-06-16 19:56:27 +0100

    docs: label spring map and anim data

--- General/Sprites/Level Misc/Anim - Spring.asm
+++ General/Sprites/Level Misc/Anim - Spring.asm
@@ -1,17 +1,16 @@
 Ani_Spring_:
-		dc.w byte_2372E-Ani_Spring_
-		dc.w byte_23731-Ani_Spring_
-		dc.w byte_2373D-Ani_Spring_
-		dc.w byte_23740-Ani_Spring_
-		dc.w byte_2374C-Ani_Spring_
-		dc.w byte_2374F-Ani_Spring_
-byte_2372E:	dc.b   $F,   0, $FF
-byte_23731:	dc.b    0,   1,   0,   0,   2,   2,   2,   2,   2,   2, $FD,   0
+		dc.w Ani_Spring_VerticalIdle-Ani_Spring_
+		dc.w Ani_Spring_VerticalTriggered-Ani_Spring_
+		dc.w Ani_Spring_HorizontalIdle-Ani_Spring_
+		dc.w Ani_Spring_HorizontalTriggered-Ani_Spring_
+		dc.w Ani_Spring_DiagonalIdle-Ani_Spring_
+		dc.w Ani_Spring_DiagonalTriggered-Ani_Spring_
+Ani_Spring_VerticalIdle:	dc.b   $F,   0, $FF	; byte_2372E
+Ani_Spring_VerticalTriggered:	dc.b    0,   1,   0,   0,   2,   2,   2,   2,   2,   2, $FD,   0	; byte_23731
 
-byte_2373D:	dc.b   $F,   3, $FF
-byte_23740:	dc.b    0,   4,   3,   3,   5,   5,   5,   5,   5,   5, $FD,   2
+Ani_Spring_HorizontalIdle:	dc.b   $F,   3, $FF	; byte_2373D
+Ani_Spring_HorizontalTriggered:	dc.b    0,   4,   3,   3,   5,   5,   5,   5,   5,   5, $FD,   2	; byte_23740
 
-byte_2374C:	dc.b   $F,   7, $FF
-byte_2374F:	dc.b    0,   8,   7,   7,   9,   9,   9,   9,   9,   9, $FD,   4
+Ani_Spring_DiagonalIdle:	dc.b   $F,   7, $FF	; byte_2374C
+Ani_Spring_DiagonalTriggered:	dc.b    0,   8,   7,   7,   9,   9,   9,   9,   9,   9, $FD,   4	; byte_2374F
 		even
-
