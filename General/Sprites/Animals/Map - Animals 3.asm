3bbc7c0227bc54f23f3909c2cf78fc88442efd66
Farrell <farrell@bluetoaster.net>
2026-06-16 21:54:42 +0100

    docs: label animal mapping data

--- General/Sprites/Animals/Map - Animals 3.asm
+++ General/Sprites/Animals/Map - Animals 3.asm
@@ -1,10 +1,10 @@
 Map_Animals3_:
-		dc.w word_2CF04-Map_Animals3_
-		dc.w word_2CF0C-Map_Animals3_
-		dc.w word_2CEFC-Map_Animals3_
-word_2CEFC:	dc.w 1
+		dc.w Map_Animals3_Frame0-Map_Animals3_
+		dc.w Map_Animals3_Frame1-Map_Animals3_
+		dc.w Map_Animals3_Frame2-Map_Animals3_
+Map_Animals3_Frame2:	dc.w 1	; word_2CEFC
 		dc.b  $F4,   6,   0,   0, $FF, $F8
-word_2CF04:	dc.w 1
+Map_Animals3_Frame0:	dc.w 1	; word_2CF04
 		dc.b  $F8,   9,   0,   6, $FF, $F4
-word_2CF0C:	dc.w 1
+Map_Animals3_Frame1:	dc.w 1	; word_2CF0C
 		dc.b  $F8,   9,   0,  $C, $FF, $F4
--- General/Sprites/Animals/Map - Animals 4.asm
+++ General/Sprites/Animals/Map - Animals 4.asm
@@ -1,10 +1,10 @@
 Map_Animals4_:
-		dc.w word_2CF22-Map_Animals4_
-		dc.w word_2CF2A-Map_Animals4_
-		dc.w word_2CF1A-Map_Animals4_
-word_2CF1A:	dc.w 1
+		dc.w Map_Animals4_Frame0-Map_Animals4_
+		dc.w Map_Animals4_Frame1-Map_Animals4_
+		dc.w Map_Animals4_Frame2-Map_Animals4_
+Map_Animals4_Frame2:	dc.w 1	; word_2CF1A
 		dc.b  $F8,   6,   0,   0, $FF, $F8
-word_2CF22:	dc.w 1
+Map_Animals4_Frame0:	dc.w 1	; word_2CF22
 		dc.b  $FC,   5,   0,   6, $FF, $F8
-word_2CF2A:	dc.w 1
+Map_Animals4_Frame1:	dc.w 1	; word_2CF2A
 		dc.b  $FC,   5,   0,  $A, $FF, $F8
