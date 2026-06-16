70259212e9320e2ad003c95ecc72be0e24c1bc28
Farrell <farrell@bluetoaster.net>
2026-06-16 11:03:13 +0100

    docs: label adjacent badnik map frames

--- General/Sprites/Snale Blaster/Map - Snale Blaster.asm
+++ General/Sprites/Snale Blaster/Map - Snale Blaster.asm
@@ -1,43 +1,43 @@
 Map_SnaleBlaster_:
-		dc.w Frame_360416-Map_SnaleBlaster_
-		dc.w Frame_36042A-Map_SnaleBlaster_
-		dc.w Frame_36043E-Map_SnaleBlaster_
-		dc.w Frame_360452-Map_SnaleBlaster_
-		dc.w Frame_360466-Map_SnaleBlaster_
-		dc.w Frame_36047A-Map_SnaleBlaster_
-		dc.w Frame_360482-Map_SnaleBlaster_
-		dc.w Frame_36048A-Map_SnaleBlaster_
-		dc.w Frame_360492-Map_SnaleBlaster_
-		dc.w Frame_36049A-Map_SnaleBlaster_
-		dc.w Frame_3604A2-Map_SnaleBlaster_
-Frame_360416:	dc.w 3
+		dc.w Map_SnaleBlaster_Frame0-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame1-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame2-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame3-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame4-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame5-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame6-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame7-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame8-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame9-Map_SnaleBlaster_
+		dc.w Map_SnaleBlaster_Frame10-Map_SnaleBlaster_
+Map_SnaleBlaster_Frame0:	dc.w 3	; Frame_360416
 		dc.b  $F0,   5,   0,   7, $FF, $FC
 		dc.b  $F8,   0,   0,   0, $FF, $F4
 		dc.b    0,   9,   0,   1, $FF, $F4
-Frame_36042A:	dc.w 3
+Map_SnaleBlaster_Frame1:	dc.w 3	; Frame_36042A
 		dc.b  $F0,   5,   0,  $B, $FF, $FC
 		dc.b  $F8,   0,   0,   0, $FF, $F4
 		dc.b    0,   9,   0,   1, $FF, $F4
-Frame_36043E:	dc.w 3
+Map_SnaleBlaster_Frame2:	dc.w 3	; Frame_36043E
 		dc.b  $F0,   5,   0,  $F, $FF, $FC
 		dc.b  $F8,   0,   0,   0, $FF, $F4
 		dc.b    0,   9,   0,   1, $FF, $F4
-Frame_360452:	dc.w 3
+Map_SnaleBlaster_Frame3:	dc.w 3	; Frame_360452
 		dc.b  $F0,   5,   0, $13, $FF, $FC
 		dc.b  $F8,   0,   0,   0, $FF, $F4
 		dc.b    0,   9,   0,   1, $FF, $F4
-Frame_360466:	dc.w 3
+Map_SnaleBlaster_Frame4:	dc.w 3	; Frame_360466
 		dc.b  $F0,   5,   0, $17, $FF, $FC
 		dc.b  $F8,   0,   0,   0, $FF, $F4
 		dc.b    0,   9,   0,   1, $FF, $F4
-Frame_36047A:	dc.w 1
+Map_SnaleBlaster_Frame5:	dc.w 1	; Frame_36047A
 		dc.b  $F4,   2,   0, $1B, $FF, $FC
-Frame_360482:	dc.w 1
+Map_SnaleBlaster_Frame6:	dc.w 1	; Frame_360482
 		dc.b  $FC,   1,   0, $1E, $FF, $FC
-Frame_36048A:	dc.w 1
+Map_SnaleBlaster_Frame7:	dc.w 1	; Frame_36048A
 		dc.b  $FC,   0,   0, $21, $FF, $FC
-Frame_360492:	dc.w 1
+Map_SnaleBlaster_Frame8:	dc.w 1	; Frame_360492
 		dc.b  $FC,   0,   0, $22, $FF, $FC
-Frame_36049A:	dc.w 1
+Map_SnaleBlaster_Frame9:	dc.w 1	; Frame_36049A
 		dc.b  $FC,   0,   0, $20, $FF, $FC
-Frame_3604A2:	dc.w 0
+Map_SnaleBlaster_Frame10:	dc.w 0	; Frame_3604A2
--- General/Sprites/Turbo Spiker/Map - Turbo Spiker Hidden.asm
+++ General/Sprites/Turbo Spiker/Map - Turbo Spiker Hidden.asm
@@ -1,6 +1,6 @@
 Map_TurboSpikerHidden_:
-		dc.w word_87F42-Map_TurboSpikerHidden_
-word_87F42:	dc.w 3
+		dc.w Map_TurboSpikerHidden_Frame0-Map_TurboSpikerHidden_
+Map_TurboSpikerHidden_Frame0:	dc.w 3	; word_87F42
 		dc.b  $D0,  $F,   0, $50, $FF, $F0
 		dc.b  $F0,  $F,   0, $50, $FF, $F0
 		dc.b  $F0,   3,   0, $50,   0, $10
