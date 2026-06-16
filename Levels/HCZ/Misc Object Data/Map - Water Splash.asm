4fcc9b5abad55e0449e658269a666580797a28e6
Farrell <farrell@bluetoaster.net>
2026-06-16 08:07:21 +0100

    docs: label HCZ water splash mappings

--- Levels/HCZ/Misc Object Data/Map - Water Splash.asm
+++ Levels/HCZ/Misc Object Data/Map - Water Splash.asm
@@ -1,10 +1,10 @@
 Map_HCZWaterSplash_:
-		dc.w Frame_237C6A-Map_HCZWaterSplash_
-		dc.w Frame_237C6A-Map_HCZWaterSplash_
-		dc.w Frame_237C6A-Map_HCZWaterSplash_
-		dc.w Frame_237C6A-Map_HCZWaterSplash_
-		dc.w Frame_237C86-Map_HCZWaterSplash_
-Frame_237C6A:	dc.w 2
+		dc.w Map_HCZWaterSplash_SplashFrame-Map_HCZWaterSplash_
+		dc.w Map_HCZWaterSplash_SplashFrame-Map_HCZWaterSplash_
+		dc.w Map_HCZWaterSplash_SplashFrame-Map_HCZWaterSplash_
+		dc.w Map_HCZWaterSplash_SplashFrame-Map_HCZWaterSplash_
+		dc.w Map_HCZWaterSplash_SkimFrame-Map_HCZWaterSplash_
+Map_HCZWaterSplash_SplashFrame:	dc.w 2
 		dc.b  $F0,  $B,   0,   0, $FF, $E8
 		dc.b  $F0,  $B,   0,  $C,   0,   0
-Frame_247C78:	dc.w 0
+Map_HCZWaterSplash_EmptyFrame:	dc.w 0
