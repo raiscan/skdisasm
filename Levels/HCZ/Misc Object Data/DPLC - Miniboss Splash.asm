e206b42244e5d10854ec9edb1a225a39c1f3a99a
Farrell <farrell@bluetoaster.net>
2026-06-16 06:02:28 +0100

    docs: label HCZ miniboss splash data

--- Levels/HCZ/Misc Object Data/DPLC - Miniboss Splash.asm
+++ Levels/HCZ/Misc Object Data/DPLC - Miniboss Splash.asm
@@ -1,28 +1,28 @@
 DPLC_HCZMinibossSplash_:
-		dc.w Frame_362C90-DPLC_HCZMinibossSplash_
-		dc.w Frame_362C94-DPLC_HCZMinibossSplash_
-		dc.w Frame_362C98-DPLC_HCZMinibossSplash_
-		dc.w Frame_362C9C-DPLC_HCZMinibossSplash_
-		dc.w Frame_362CA0-DPLC_HCZMinibossSplash_
-		dc.w Frame_362CA4-DPLC_HCZMinibossSplash_
-		dc.w Frame_362CA8-DPLC_HCZMinibossSplash_
-		dc.w Frame_362CAC-DPLC_HCZMinibossSplash_
-		dc.w Frame_362CB0-DPLC_HCZMinibossSplash_
-Frame_362C90:	dc.w 0
+		dc.w DPLC_HCZMinibossSplash_Frame0-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame1-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame2-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame3-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame4-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame5-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame6-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame7-DPLC_HCZMinibossSplash_
+		dc.w DPLC_HCZMinibossSplash_Frame8-DPLC_HCZMinibossSplash_
+DPLC_HCZMinibossSplash_Frame0:	dc.w 0
 		dc.w $0005
-Frame_362C94:	dc.w 0
+DPLC_HCZMinibossSplash_Frame1:	dc.w 0
 		dc.w $006F
-Frame_362C98:	dc.w 0
+DPLC_HCZMinibossSplash_Frame2:	dc.w 0
 		dc.w $016F
-Frame_362C9C:	dc.w 0
+DPLC_HCZMinibossSplash_Frame3:	dc.w 0
 		dc.w $026F
-Frame_362CA0:	dc.w 0
+DPLC_HCZMinibossSplash_Frame4:	dc.w 0
 		dc.w $036F
-Frame_362CA4:	dc.w 0
+DPLC_HCZMinibossSplash_Frame5:	dc.w 0
 		dc.w $046B
-Frame_362CA8:	dc.w 0
+DPLC_HCZMinibossSplash_Frame6:	dc.w 0
 		dc.w $0527
-Frame_362CAC:	dc.w 0
+DPLC_HCZMinibossSplash_Frame7:	dc.w 0
 		dc.w $05A3
-Frame_362CB0:	dc.w 0
+DPLC_HCZMinibossSplash_Frame8:	dc.w 0
 		dc.w $05E3
