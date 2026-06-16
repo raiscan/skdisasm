870471f17395240b54162758f43c20257c4ce0ba
Farrell <farrell@bluetoaster.net>
2026-06-16 04:37:39 +0100

    docs: label AIZ2 bombership event assets

--- Levels/AIZ/Misc Object Data/Anim - Act 2 Bomb Explosion.asm
+++ Levels/AIZ/Misc Object Data/Anim - Act 2 Bomb Explosion.asm
@@ -1,6 +1,6 @@
 Ani_AIZ2BombExplode_:
-		dc.w byte_23C22E-Ani_AIZ2BombExplode_
-		dc.w byte_23C23A-Ani_AIZ2BombExplode_
-byte_23C22E:	dc.b    1,   3,   2,   4,   3,   5,   4,   5,   5,   5, $FC,   0
-byte_23C23A:	dc.b    6,   2,   7,   3,   8,   4,   9,   5,  $A,   5,  $B,   5, $FC,   0
+		dc.w Ani_AIZ2BombExplode_Script0-Ani_AIZ2BombExplode_
+		dc.w Ani_AIZ2BombExplode_Script1-Ani_AIZ2BombExplode_
+Ani_AIZ2BombExplode_Script0:	dc.b    1,   3,   2,   4,   3,   5,   4,   5,   5,   5, $FC,   0
+Ani_AIZ2BombExplode_Script1:	dc.b    6,   2,   7,   3,   8,   4,   9,   5,  $A,   5,  $B,   5, $FC,   0
 		even
--- Levels/AIZ/Misc Object Data/Anim - Act 2 Ship Propeller.asm
+++ Levels/AIZ/Misc Object Data/Anim - Act 2 Ship Propeller.asm
@@ -1,4 +1,4 @@
 Ani_AIZShipPropeller_:
-		dc.w byte_23C1AC-Ani_AIZShipPropeller_
-byte_23C1AC:	dc.b    2,   0,   1,   2,   3, $FF
+		dc.w Ani_AIZShipPropeller_Script0-Ani_AIZShipPropeller_
+Ani_AIZShipPropeller_Script0:	dc.b    2,   0,   1,   2,   3, $FF
 		even
