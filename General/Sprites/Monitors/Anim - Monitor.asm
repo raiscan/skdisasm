52f367d3914185733cf28d8bbd3f46791a201bb6
Farrell <farrell@bluetoaster.net>
2026-06-16 20:15:53 +0100

    docs: label monitor map and anim data

--- General/Sprites/Monitors/Anim - Monitor.asm
+++ General/Sprites/Monitors/Anim - Monitor.asm
@@ -1,24 +1,24 @@
 Ani_Monitor_:
-		dc.w byte_1DB50-Ani_Monitor_
-		dc.w byte_1DB54-Ani_Monitor_
-		dc.w byte_1DB5C-Ani_Monitor_
-		dc.w byte_1DB64-Ani_Monitor_
-		dc.w byte_1DB6C-Ani_Monitor_
-		dc.w byte_1DB74-Ani_Monitor_
-		dc.w byte_1DB7C-Ani_Monitor_
-		dc.w byte_1DB84-Ani_Monitor_
-		dc.w byte_1DB8C-Ani_Monitor_
-		dc.w byte_1DB94-Ani_Monitor_
-		dc.w byte_1DB9C-Ani_Monitor_
-byte_1DB50:	dc.b	1,   0,   1, $FF
-byte_1DB54:	dc.b	1,   0,   2,   2,   1,   2,   2, $FF
-byte_1DB5C:	dc.b	1,   0,   3,   3,   1,   3,   3, $FF
-byte_1DB64:	dc.b	1,   0,   4,   4,   1,   4,   4, $FF
-byte_1DB6C:	dc.b	1,   0,   5,   5,   1,   5,   5, $FF
-byte_1DB74:	dc.b	1,   0,   6,   6,   1,   6,   6, $FF
-byte_1DB7C:	dc.b	1,   0,   7,   7,   1,   7,   7, $FF
-byte_1DB84:	dc.b	1,   0,   8,   8,   1,   8,   8, $FF
-byte_1DB8C:	dc.b	1,   0,   9,   9,   1,   9,   9, $FF
-byte_1DB94:	dc.b	1,   0,  $A,  $A,   1,  $A,  $A, $FF
-byte_1DB9C:	dc.b	2,   0,   1,  $B, $FE,   1
+		dc.w Ani_Monitor_Eggman-Ani_Monitor_
+		dc.w Ani_Monitor_OneUp-Ani_Monitor_
+		dc.w Ani_Monitor_Eggman2-Ani_Monitor_
+		dc.w Ani_Monitor_Rings-Ani_Monitor_
+		dc.w Ani_Monitor_SpeedShoes-Ani_Monitor_
+		dc.w Ani_Monitor_FireShield-Ani_Monitor_
+		dc.w Ani_Monitor_LightningShield-Ani_Monitor_
+		dc.w Ani_Monitor_BubbleShield-Ani_Monitor_
+		dc.w Ani_Monitor_Invincibility-Ani_Monitor_
+		dc.w Ani_Monitor_SuperSonic-Ani_Monitor_
+		dc.w Ani_Monitor_Break-Ani_Monitor_
+Ani_Monitor_Eggman:		dc.b	1,   0,   1, $FF					; byte_1DB50
+Ani_Monitor_OneUp:		dc.b	1,   0,   2,   2,   1,   2,   2, $FF		; byte_1DB54
+Ani_Monitor_Eggman2:		dc.b	1,   0,   3,   3,   1,   3,   3, $FF		; byte_1DB5C
+Ani_Monitor_Rings:		dc.b	1,   0,   4,   4,   1,   4,   4, $FF		; byte_1DB64
+Ani_Monitor_SpeedShoes:		dc.b	1,   0,   5,   5,   1,   5,   5, $FF		; byte_1DB6C
+Ani_Monitor_FireShield:		dc.b	1,   0,   6,   6,   1,   6,   6, $FF		; byte_1DB74
+Ani_Monitor_LightningShield:	dc.b	1,   0,   7,   7,   1,   7,   7, $FF		; byte_1DB7C
+Ani_Monitor_BubbleShield:	dc.b	1,   0,   8,   8,   1,   8,   8, $FF		; byte_1DB84
+Ani_Monitor_Invincibility:	dc.b	1,   0,   9,   9,   1,   9,   9, $FF		; byte_1DB8C
+Ani_Monitor_SuperSonic:		dc.b	1,   0,  $A,  $A,   1,  $A,  $A, $FF		; byte_1DB94
+Ani_Monitor_Break:		dc.b	2,   0,   1,  $B, $FE,   1			; byte_1DB9C
 		even
--- General/Sprites/Monitors/Map - Monitor.asm
+++ General/Sprites/Monitors/Map - Monitor.asm
@@ -1,47 +1,47 @@
 Map_Monitor_:
-		dc.w word_1DBBA-Map_Monitor_
-		dc.w word_1DBC2-Map_Monitor_
-		dc.w word_1DBD0-Map_Monitor_
-		dc.w word_1DBDE-Map_Monitor_
-		dc.w word_1DBEC-Map_Monitor_
-		dc.w word_1DBFA-Map_Monitor_
-		dc.w word_1DC08-Map_Monitor_
-		dc.w word_1DC16-Map_Monitor_
-		dc.w word_1DC24-Map_Monitor_
-		dc.w word_1DC32-Map_Monitor_
-		dc.w word_1DC40-Map_Monitor_
-		dc.w word_1DC4E-Map_Monitor_
-word_1DBBA:	dc.w 1
+		dc.w Map_Monitor_Box-Map_Monitor_
+		dc.w Map_Monitor_EggmanIcon-Map_Monitor_
+		dc.w Map_Monitor_OneUpIcon-Map_Monitor_
+		dc.w Map_Monitor_Eggman2Icon-Map_Monitor_
+		dc.w Map_Monitor_RingsIcon-Map_Monitor_
+		dc.w Map_Monitor_SpeedShoesIcon-Map_Monitor_
+		dc.w Map_Monitor_FireShieldIcon-Map_Monitor_
+		dc.w Map_Monitor_LightningShieldIcon-Map_Monitor_
+		dc.w Map_Monitor_BubbleShieldIcon-Map_Monitor_
+		dc.w Map_Monitor_InvincibilityIcon-Map_Monitor_
+		dc.w Map_Monitor_SuperSonicIcon-Map_Monitor_
+		dc.w Map_Monitor_BrokenShell-Map_Monitor_
+Map_Monitor_Box:	dc.w 1	; word_1DBBA
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBC2:	dc.w 2
+Map_Monitor_EggmanIcon:	dc.w 2	; word_1DBC2
 		dc.b  $F3,   5,   0, $18, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBD0:	dc.w 2
+Map_Monitor_OneUpIcon:	dc.w 2	; word_1DBD0
 		dc.b  $F3,   5,   3, $10, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBDE:	dc.w 2
+Map_Monitor_Eggman2Icon:	dc.w 2	; word_1DBDE
 		dc.b  $F3,   5,   0, $1C, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBEC:	dc.w 2
+Map_Monitor_RingsIcon:	dc.w 2	; word_1DBEC
 		dc.b  $F3,   5, $20, $20, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DBFA:	dc.w 2
+Map_Monitor_SpeedShoesIcon:	dc.w 2	; word_1DBFA
 		dc.b  $F3,   5,   0, $24, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC08:	dc.w 2
+Map_Monitor_FireShieldIcon:	dc.w 2	; word_1DC08
 		dc.b  $F3,   5,   0, $30, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC16:	dc.w 2
+Map_Monitor_LightningShieldIcon:	dc.w 2	; word_1DC16
 		dc.b  $F3,   5,   0, $2C, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC24:	dc.w 2
+Map_Monitor_BubbleShieldIcon:	dc.w 2	; word_1DC24
 		dc.b  $F3,   5,   0, $34, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC32:	dc.w 2
+Map_Monitor_InvincibilityIcon:	dc.w 2	; word_1DC32
 		dc.b  $F3,   5,   0, $28, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC40:	dc.w 2
+Map_Monitor_SuperSonicIcon:	dc.w 2	; word_1DC40
 		dc.b  $F3,   5,   0, $38, $FF, $F8
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_1DC4E:	dc.w 1
+Map_Monitor_BrokenShell:	dc.w 1	; word_1DC4E
 		dc.b    0,  $D,   0, $10, $FF, $F0
