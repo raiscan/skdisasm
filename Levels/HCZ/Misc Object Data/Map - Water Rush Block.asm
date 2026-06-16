d87d0d57b740eb162f52cb6fc007759da29305cd
Farrell <farrell@bluetoaster.net>
2026-06-16 05:21:36 +0100

    docs: label HCZ water rush mappings

--- Levels/HCZ/Misc Object Data/Map - Water Rush Block.asm
+++ Levels/HCZ/Misc Object Data/Map - Water Rush Block.asm
@@ -1,8 +1,8 @@
 Map_HCZWaterRushBlock_:
-		dc.w Frame_22E770-Map_HCZWaterRushBlock_
-		dc.w Frame_22E778-Map_HCZWaterRushBlock_
-Frame_22E770:	dc.w 1
+		dc.w Map_HCZWaterRushBlock_Frame0-Map_HCZWaterRushBlock_
+		dc.w Map_HCZWaterRushBlock_Frame1-Map_HCZWaterRushBlock_
+Map_HCZWaterRushBlock_Frame0:	dc.w 1
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_22E778:	dc.w 2
+Map_HCZWaterRushBlock_Frame1:	dc.w 2
 		dc.b  $E0,  $F,   0,   0, $FF, $F0
 		dc.b    0,  $F,   0,   0, $FF, $F0
