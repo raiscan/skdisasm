e196ac43e889e2ee5a33a9016da0f7670d98b171
Farrell <farrell@bluetoaster.net>
2026-06-16 05:13:15 +0100

    docs: label HCZ fan mappings

--- Levels/HCZ/Misc Object Data/Map - Fan.asm
+++ Levels/HCZ/Misc Object Data/Map - Fan.asm
@@ -1,26 +1,26 @@
 Map_HCZFan_:
-		dc.w Frame_22F3B4-Map_HCZFan_
-		dc.w Frame_22F3C8-Map_HCZFan_
-		dc.w Frame_22F3DC-Map_HCZFan_
-		dc.w Frame_22F3F0-Map_HCZFan_
-		dc.w Frame_22F404-Map_HCZFan_
-Frame_22F3B4:	dc.w 3
+		dc.w Map_HCZFan_Frame0-Map_HCZFan_
+		dc.w Map_HCZFan_Frame1-Map_HCZFan_
+		dc.w Map_HCZFan_Frame2-Map_HCZFan_
+		dc.w Map_HCZFan_Frame3-Map_HCZFan_
+		dc.w Map_HCZFan_Frame4-Map_HCZFan_
+Map_HCZFan_Frame0:	dc.w 3
 		dc.b  $F4,  $C,   0,   0, $FF, $F0
 		dc.b  $FD,  $C,   8,   0, $FF, $F0
 		dc.b  $F4,   6,   0,  $C, $FF, $F8
-Frame_22F3C8:	dc.w 3
+Map_HCZFan_Frame1:	dc.w 3
 		dc.b  $F4,  $C, $18,   0, $FF, $F0
 		dc.b  $FD,  $C, $10,   0, $FF, $F0
 		dc.b  $F4,   6,   0,  $C, $FF, $F8
-Frame_22F3DC:	dc.w 3
+Map_HCZFan_Frame2:	dc.w 3
 		dc.b  $F4,  $C,   0,   4, $FF, $F0
 		dc.b  $FD,  $C,   8,   4, $FF, $F0
 		dc.b  $F4,   6,   0,  $C, $FF, $F8
-Frame_22F3F0:	dc.w 3
+Map_HCZFan_Frame3:	dc.w 3
 		dc.b  $F4,  $C,   0,   8, $FF, $F0
 		dc.b  $FD,  $C,   8,   8, $FF, $F0
 		dc.b  $F4,   6,   0,  $C, $FF, $F8
-Frame_22F404:	dc.w 3
+Map_HCZFan_Frame4:	dc.w 3
 		dc.b  $F4,  $C, $18,   4, $FF, $F0
 		dc.b  $FD,  $C, $10,   4, $FF, $F0
 		dc.b  $F4,   6,   0,  $C, $FF, $F8
