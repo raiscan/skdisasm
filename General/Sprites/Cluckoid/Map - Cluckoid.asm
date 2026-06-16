2c45e4f29840a370bd57819c8a86ebc77a7308bf
Farrell <farrell@bluetoaster.net>
2026-06-16 11:41:50 +0100

    docs: label Cluckoid map and dplc frames

--- General/Sprites/Cluckoid/Map - Cluckoid.asm
+++ General/Sprites/Cluckoid/Map - Cluckoid.asm
@@ -1,65 +1,65 @@
 ; Cluckoid mappings
 Map_Cluckoid_:
-		dc.w word_8E562-Map_Cluckoid_
-		dc.w word_8E576-Map_Cluckoid_
-		dc.w word_8E58A-Map_Cluckoid_
-		dc.w word_8E59E-Map_Cluckoid_
-		dc.w word_8E5AC-Map_Cluckoid_
-		dc.w word_8E5BA-Map_Cluckoid_
-		dc.w word_8E5C8-Map_Cluckoid_
-		dc.w word_8E5DC-Map_Cluckoid_
-		dc.w word_8E5F0-Map_Cluckoid_
-		dc.w word_8E604-Map_Cluckoid_
-		dc.w word_8E612-Map_Cluckoid_
-		dc.w word_8E620-Map_Cluckoid_
-		dc.w word_8E62E-Map_Cluckoid_
-		dc.w word_8E63C-Map_Cluckoid_
-word_8E562:	dc.w 3
+		dc.w Map_Cluckoid_Frame0-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame1-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame2-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame3-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame4-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame5-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame6-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame7-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame8-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame9-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame10-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame11-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame12-Map_Cluckoid_
+		dc.w Map_Cluckoid_Frame13-Map_Cluckoid_
+Map_Cluckoid_Frame0:	dc.w 3		; word_8E562
 		dc.b  $F0,  $A,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,   9,   0,   8
 		dc.b    8,   4,   0,  $B, $FF, $F8
-word_8E576:	dc.w 3
+Map_Cluckoid_Frame1:	dc.w 3		; word_8E576
 		dc.b  $F0,  $A,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,   9,   0,   8
 		dc.b    8,   4,   0,  $B, $FF, $F8
-word_8E58A:	dc.w 3
+Map_Cluckoid_Frame2:	dc.w 3		; word_8E58A
 		dc.b  $F0,  $A,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,   9,   0,   8
 		dc.b    8,   4,   0,  $B, $FF, $F8
-word_8E59E:	dc.w 2
+Map_Cluckoid_Frame3:	dc.w 2		; word_8E59E
 		dc.b  $F0,  $E,   0,   0, $FF, $F0
 		dc.b    8,   4,   0,  $C, $FF, $F8
-word_8E5AC:	dc.w 2
+Map_Cluckoid_Frame4:	dc.w 2		; word_8E5AC
 		dc.b  $F0,  $E,   0,   0, $FF, $F0
 		dc.b    8,   8,   0,  $C, $FF, $F0
-word_8E5BA:	dc.w 2
+Map_Cluckoid_Frame5:	dc.w 2		; word_8E5BA
 		dc.b  $F0,  $E,   0,   0, $FF, $F0
 		dc.b    8,   8,   0,  $C, $FF, $F0
-word_8E5C8:	dc.w 3
+Map_Cluckoid_Frame6:	dc.w 3		; word_8E5C8
 		dc.b  $F0,   8,   0,   0, $FF, $F0
 		dc.b  $F8,  $E,   0,   3, $FF, $E8
 		dc.b  $F8,   1,   0,  $F,   0,   8
-word_8E5DC:	dc.w 3
+Map_Cluckoid_Frame7:	dc.w 3		; word_8E5DC
 		dc.b  $F0,   8,   0,   0, $FF, $F0
 		dc.b  $F8,  $E,   0,   3, $FF, $E8
 		dc.b  $F8,   1,   0,  $F,   0,   8
-word_8E5F0:	dc.w 3
+Map_Cluckoid_Frame8:	dc.w 3		; word_8E5F0
 		dc.b    0,   1,   0,   0, $FF, $E8
 		dc.b  $F0,  $B,   0,   2, $FF, $F0
 		dc.b  $F8,   1,   0,  $E,   0,   8
-word_8E604:	dc.w 2
+Map_Cluckoid_Frame9:	dc.w 2		; word_8E604
 		dc.b  $F0,  $B,   0,   0, $FF, $ED
 		dc.b  $F8,   1,   0,  $C,   0,   5
-word_8E612:	dc.w 2
+Map_Cluckoid_Frame10:	dc.w 2		; word_8E612
 		dc.b  $F0,  $B,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,  $C,   0,   8
-word_8E620:	dc.w 2
+Map_Cluckoid_Frame11:	dc.w 2		; word_8E620
 		dc.b  $F0,  $B,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,  $C,   0,   8
-word_8E62E:	dc.w 2
+Map_Cluckoid_Frame12:	dc.w 2		; word_8E62E
 		dc.b  $F0,  $B,   0,   0, $FF, $F0
 		dc.b  $F8,   1,   0,  $C,   0,   8
-word_8E63C:	dc.w 3
+Map_Cluckoid_Frame13:	dc.w 3		; word_8E63C
 		dc.b  $F0,  $A,   0,   0, $FF, $F0
 		dc.b    8,   4,   0,   9, $FF, $F8
 		dc.b  $F8,   1,   0,  $B,   0,   8
