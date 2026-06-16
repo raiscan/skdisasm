19afc9332d4eca7af3cb9aec76b13f6449431bb1
Farrell <farrell@bluetoaster.net>
2026-06-16 14:56:20 +0100

    docs: label LBZ trigger bridge map frames

--- Levels/LBZ/Misc Object Data/Map - Trigger Bridge.asm
+++ Levels/LBZ/Misc Object Data/Map - Trigger Bridge.asm
@@ -1,29 +1,29 @@
 Map_LBZTriggerBridge_:
-		dc.w Frame_224E16-Map_LBZTriggerBridge_ ; ...
-		dc.w Frame_224E48-Map_LBZTriggerBridge_
-		dc.w Frame_224E74-Map_LBZTriggerBridge_
-		dc.w Frame_224E9A-Map_LBZTriggerBridge_
-		dc.w Frame_224EBA-Map_LBZTriggerBridge_
-		dc.w Frame_224ED4-Map_LBZTriggerBridge_
-		dc.w Frame_224EE8-Map_LBZTriggerBridge_
-		dc.w Frame_224EF6-Map_LBZTriggerBridge_
-		dc.w Frame_224EFE-Map_LBZTriggerBridge_
-		dc.w Frame_224F30-Map_LBZTriggerBridge_
-		dc.w Frame_224F5C-Map_LBZTriggerBridge_
-		dc.w Frame_224F82-Map_LBZTriggerBridge_
-		dc.w Frame_224FA2-Map_LBZTriggerBridge_
-		dc.w Frame_224FBC-Map_LBZTriggerBridge_
-		dc.w Frame_224FD0-Map_LBZTriggerBridge_
-		dc.w Frame_224FDE-Map_LBZTriggerBridge_
-		dc.w Frame_224FE6-Map_LBZTriggerBridge_
-		dc.w Frame_225018-Map_LBZTriggerBridge_
-		dc.w Frame_225044-Map_LBZTriggerBridge_
-		dc.w Frame_22506A-Map_LBZTriggerBridge_
-		dc.w Frame_22508A-Map_LBZTriggerBridge_
-		dc.w Frame_2250A4-Map_LBZTriggerBridge_
-		dc.w Frame_2250B8-Map_LBZTriggerBridge_
-		dc.w Frame_2250C6-Map_LBZTriggerBridge_
-Frame_224E16:	dc.w 8
+		dc.w Map_LBZTriggerBridge_Frame0-Map_LBZTriggerBridge_ ; ...
+		dc.w Map_LBZTriggerBridge_Frame1-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame2-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame3-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame4-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame5-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame6-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame7-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame8-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame9-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame10-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame11-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame12-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame13-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame14-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame15-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame16-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame17-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame18-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame19-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame20-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame21-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame22-Map_LBZTriggerBridge_
+		dc.w Map_LBZTriggerBridge_Frame23-Map_LBZTriggerBridge_
+Map_LBZTriggerBridge_Frame0:	dc.w 8	; Frame_224E16
 		dc.b $C0,  5,  0,$10,$FF,$F8
 		dc.b $D0,  5,  0,$10,$FF,$F8
 		dc.b $E0,  5,  0,$10,$FF,$F8
@@ -32,7 +32,7 @@ Frame_224E16:	dc.w 8
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224E48:	dc.w 7
+Map_LBZTriggerBridge_Frame1:	dc.w 7	; Frame_224E48
 		dc.b $D0,  5,  0,$10,$FF,$F8
 		dc.b $E0,  5,  0,$10,$FF,$F8
 		dc.b $F0,  5,  0,$10,$FF,$F8
@@ -40,34 +40,34 @@ Frame_224E48:	dc.w 7
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224E74:	dc.w 6
+Map_LBZTriggerBridge_Frame2:	dc.w 6	; Frame_224E74
 		dc.b $E0,  5,  0,$10,$FF,$F8
 		dc.b $F0,  5,  0,$10,$FF,$F8
 		dc.b   0,  5,  0,$10,$FF,$F8
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224E9A:	dc.w 5
+Map_LBZTriggerBridge_Frame3:	dc.w 5	; Frame_224E9A
 		dc.b $F0,  5,  0,$10,$FF,$F8
 		dc.b   0,  5,  0,$10,$FF,$F8
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224EBA:	dc.w 4
+Map_LBZTriggerBridge_Frame4:	dc.w 4	; Frame_224EBA
 		dc.b   0,  5,  0,$10,$FF,$F8
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224ED4:	dc.w 3
+Map_LBZTriggerBridge_Frame5:	dc.w 3	; Frame_224ED4
 		dc.b $10,  5,  0,$10,$FF,$F8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224EE8:	dc.w 2
+Map_LBZTriggerBridge_Frame6:	dc.w 2	; Frame_224EE8
 		dc.b $20,  5,  0,$10,$FF,$F8
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224EF6:	dc.w 1
+Map_LBZTriggerBridge_Frame7:	dc.w 1	; Frame_224EF6
 		dc.b $30,  5,  0,$10,$FF,$F8
-Frame_224EFE:	dc.w 8
+Map_LBZTriggerBridge_Frame8:	dc.w 8	; Frame_224EFE
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
@@ -76,7 +76,7 @@ Frame_224EFE:	dc.w 8
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224F30:	dc.w 7
+Map_LBZTriggerBridge_Frame9:	dc.w 7	; Frame_224F30
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
 		dc.b $F8,  5,  0,$10,$FF,$F0
@@ -84,34 +84,34 @@ Frame_224F30:	dc.w 7
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224F5C:	dc.w 6
+Map_LBZTriggerBridge_Frame10:	dc.w 6	; Frame_224F5C
 		dc.b $F8,  5,  0,$10,$FF,$E0
 		dc.b $F8,  5,  0,$10,$FF,$F0
 		dc.b $F8,  5,  0,$10,  0,  0
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224F82:	dc.w 5
+Map_LBZTriggerBridge_Frame11:	dc.w 5	; Frame_224F82
 		dc.b $F8,  5,  0,$10,$FF,$F0
 		dc.b $F8,  5,  0,$10,  0,  0
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224FA2:	dc.w 4
+Map_LBZTriggerBridge_Frame12:	dc.w 4	; Frame_224FA2
 		dc.b $F8,  5,  0,$10,  0,  0
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224FBC:	dc.w 3
+Map_LBZTriggerBridge_Frame13:	dc.w 3	; Frame_224FBC
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224FD0:	dc.w 2
+Map_LBZTriggerBridge_Frame14:	dc.w 2	; Frame_224FD0
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224FDE:	dc.w 1
+Map_LBZTriggerBridge_Frame15:	dc.w 1	; Frame_224FDE
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_224FE6:	dc.w 8
+Map_LBZTriggerBridge_Frame16:	dc.w 8	; Frame_224FE6
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
@@ -120,7 +120,7 @@ Frame_224FE6:	dc.w 8
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
 		dc.b $F8,  5,  0,$10,  0,$30
-Frame_225018:	dc.w 7
+Map_LBZTriggerBridge_Frame17:	dc.w 7	; Frame_225018
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
@@ -128,30 +128,30 @@ Frame_225018:	dc.w 7
 		dc.b $F8,  5,  0,$10,  0,  0
 		dc.b $F8,  5,  0,$10,  0,$10
 		dc.b $F8,  5,  0,$10,  0,$20
-Frame_225044:	dc.w 6
+Map_LBZTriggerBridge_Frame18:	dc.w 6	; Frame_225044
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
 		dc.b $F8,  5,  0,$10,$FF,$F0
 		dc.b $F8,  5,  0,$10,  0,  0
 		dc.b $F8,  5,  0,$10,  0,$10
-Frame_22506A:	dc.w 5
+Map_LBZTriggerBridge_Frame19:	dc.w 5	; Frame_22506A
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
 		dc.b $F8,  5,  0,$10,$FF,$F0
 		dc.b $F8,  5,  0,$10,  0,  0
-Frame_22508A:	dc.w 4
+Map_LBZTriggerBridge_Frame20:	dc.w 4	; Frame_22508A
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
 		dc.b $F8,  5,  0,$10,$FF,$F0
-Frame_2250A4:	dc.w 3
+Map_LBZTriggerBridge_Frame21:	dc.w 3	; Frame_2250A4
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
 		dc.b $F8,  5,  0,$10,$FF,$E0
-Frame_2250B8:	dc.w 2
+Map_LBZTriggerBridge_Frame22:	dc.w 2	; Frame_2250B8
 		dc.b $F8,  5,  0,$10,$FF,$C0
 		dc.b $F8,  5,  0,$10,$FF,$D0
-Frame_2250C6:	dc.w 1
+Map_LBZTriggerBridge_Frame23:	dc.w 1	; Frame_2250C6
 		dc.b $F8,  5,  0,$10,$FF,$C0
