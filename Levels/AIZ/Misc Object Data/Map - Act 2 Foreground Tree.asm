7735bbe4f0a84f5e65e762d21072785320a7eba9
Farrell <farrell@bluetoaster.net>
2026-06-16 12:42:13 +0100

    docs: label AIZ misc map frames

--- Levels/AIZ/Misc Object Data/Map - Act 2 Foreground Tree.asm
+++ Levels/AIZ/Misc Object Data/Map - Act 2 Foreground Tree.asm
@@ -1,6 +1,6 @@
 Map_AIZ2FGTree_:
-		dc.w word_2B99C-Map_AIZ2FGTree_
-word_2B99C:	dc.w 4
+		dc.w Map_AIZ2FGTree_Frame0-Map_AIZ2FGTree_
+Map_AIZ2FGTree_Frame0:	dc.w 4	; word_2B99C
 		dc.b  $C0,   7,   0,   0, $FF, $F8
 		dc.b  $E0,   7,   0,   0, $FF, $F8
 		dc.b    0,   7,   0,   0, $FF, $F8
--- Levels/AIZ/Misc Object Data/Map - Floating Platform.asm
+++ Levels/AIZ/Misc Object Data/Map - Floating Platform.asm
@@ -1,6 +1,6 @@
 Map_AIZFloatingPlatform_:
-		dc.w word_256A4-Map_AIZFloatingPlatform_
-word_256A4:	dc.w 4
+		dc.w Map_AIZFloatingPlatform_Frame0-Map_AIZFloatingPlatform_
+Map_AIZFloatingPlatform_Frame0:	dc.w 4	; word_256A4
 		dc.b  $E6,  $E,   0,   0, $FF, $E0
 		dc.b  $E6,  $E,   8,   0,   0,   0
 		dc.b  $FE,  $D,   0,  $C, $FF, $E0
