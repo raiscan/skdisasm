b9ec929c8f4808a1dce8290ebbb193c654a5769e
Farrell <farrell@bluetoaster.net>
2026-06-16 02:31:55 +0100

    docs: label Mantis routines

--- sonic3k.asm
+++ sonic3k.asm
@@ -185706,130 +185706,130 @@ Obj_Mantis:
 		jmp	Sprite_CheckDeleteTouch(pc)
 ; ---------------------------------------------------------------------------
 Mantis_Index:
-		dc.w loc_88E82-Mantis_Index
-		dc.w loc_88E98-Mantis_Index
-		dc.w loc_88EC4-Mantis_Index
-		dc.w loc_88EFE-Mantis_Index
-		dc.w loc_88F22-Mantis_Index
+		dc.w Mantis_Init-Mantis_Index
+		dc.w Mantis_WaitForPlayer-Mantis_Index
+		dc.w Mantis_PreJumpAnim-Mantis_Index
+		dc.w Mantis_Jump-Mantis_Index
+		dc.w Mantis_LandingAnim-Mantis_Index
 ; ---------------------------------------------------------------------------
 
-loc_88E82:
+Mantis_Init:
 		lea	ObjDat_Mantis(pc),a1
 		jsr	SetUp_ObjAttributes(pc)
 		move.b	#$29,y_radius(a0)
-		lea	ChildObjDat_88F9C(pc),a2
+		lea	Mantis_HeadChild(pc),a2
 		jmp	CreateChild1_Normal(pc)
 ; ---------------------------------------------------------------------------
 
-loc_88E98:
+Mantis_WaitForPlayer:
 		jsr	Find_SonicTails(pc)
 		bclr	#0,render_flags(a0)
 		tst.w	d0
-		beq.s	loc_88EAC
+		beq.s	Mantis_CheckPlayerRange
 		bset	#0,render_flags(a0)
 
-loc_88EAC:
+Mantis_CheckPlayerRange:
 		cmpi.w	#$40,d2
-		blo.s	loc_88EB4
+		blo.s	Mantis_StartPreJumpAnim
 		rts
 ; ---------------------------------------------------------------------------
 
-loc_88EB4:
+Mantis_StartPreJumpAnim:
 		move.b	#4,routine(a0)
-		move.l	#loc_88EE8,$34(a0)
+		move.l	#Mantis_StartJump,$34(a0)
 		rts
 ; ---------------------------------------------------------------------------
 
-loc_88EC4:
-		lea	byte_88FA4(pc),a1
+Mantis_PreJumpAnim:
+		lea	Mantis_PreJumpAnimData(pc),a1
 		jsr	Animate_RawNoSSTMultiDelay(pc)
 		tst.w	d2
-		beq.s	locret_88EE0
-		bmi.s	locret_88EE0
+		beq.s	Mantis_PreJumpReturn
+		bmi.s	Mantis_PreJumpReturn
 		moveq	#0,d0
 		move.b	anim_frame(a0),d0
-		move.w	word_88EE2(pc,d0.w),d0
+		move.w	Mantis_PreJumpYOffsetData(pc,d0.w),d0
 		add.w	d0,y_pos(a0)
 
-locret_88EE0:
+Mantis_PreJumpReturn:
 		rts
 ; ---------------------------------------------------------------------------
-word_88EE2:
+Mantis_PreJumpYOffsetData:
 		dc.w      0,    -5,  -$13
 ; ---------------------------------------------------------------------------
 
-loc_88EE8:
+Mantis_StartJump:
 		move.b	#6,routine(a0)
 		move.w	#-$600,y_vel(a0)
-		move.l	#loc_88F0E,$34(a0)
+		move.l	#Mantis_StartLandingAnim,$34(a0)
 
-locret_88EFC:
+Mantis_JumpReturn:
 		rts
 ; ---------------------------------------------------------------------------
 
-loc_88EFE:
+Mantis_Jump:
 		jsr	(MoveSprite).l
 		tst.w	y_vel(a0)
-		bmi.s	locret_88EFC
+		bmi.s	Mantis_JumpReturn
 		jmp	ObjHitFloor_DoRoutine(pc)
 ; ---------------------------------------------------------------------------
 
-loc_88F0E:
+Mantis_StartLandingAnim:
 		move.b	#8,routine(a0)
 		clr.w	y_vel(a0)
-		move.l	#loc_88F48,$34(a0)
+		move.l	#Mantis_ReturnToWait,$34(a0)
 		rts
 ; ---------------------------------------------------------------------------
 
-loc_88F22:
-		lea	byte_88FAB(pc),a1
+Mantis_LandingAnim:
+		lea	Mantis_LandingAnimData(pc),a1
 		jsr	Animate_RawNoSSTMultiDelay(pc)
 		tst.w	d2
-		beq.s	locret_88F3E
-		bmi.s	locret_88F3E
+		beq.s	Mantis_LandingReturn
+		bmi.s	Mantis_LandingReturn
 		moveq	#0,d0
 		move.b	anim_frame(a0),d0
-		move.w	word_88F40(pc,d0.w),d0
+		move.w	Mantis_LandingYOffsetData(pc,d0.w),d0
 		add.w	d0,y_pos(a0)
 
-locret_88F3E:
+Mantis_LandingReturn:
 		rts
 ; ---------------------------------------------------------------------------
-word_88F40:
+Mantis_LandingYOffsetData:
 		dc.w      0,   $12,     6,    -1
 ; ---------------------------------------------------------------------------
 
-loc_88F48:
+Mantis_ReturnToWait:
 		move.b	#2,routine(a0)
 		rts
 ; ---------------------------------------------------------------------------
 
-loc_88F50:
+Mantis_HeadChild_Main:
 		jsr	Refresh_ChildPositionAdjusted(pc)
 		moveq	#0,d0
 		move.b	routine(a0),d0
-		move.w	off_88F66(pc,d0.w),d1
-		jsr	off_88F66(pc,d1.w)
+		move.w	Mantis_HeadChild_Index(pc,d0.w),d1
+		jsr	Mantis_HeadChild_Index(pc,d1.w)
 		jmp	Child_Draw_Sprite(pc)
 ; ---------------------------------------------------------------------------
-off_88F66:
-		dc.w loc_88F6A-off_88F66
-		dc.w loc_88F72-off_88F66
+Mantis_HeadChild_Index:
+		dc.w Mantis_HeadChild_Init-Mantis_HeadChild_Index
+		dc.w Mantis_HeadChild_UpdateFrame-Mantis_HeadChild_Index
 ; ---------------------------------------------------------------------------
 
-loc_88F6A:
-		lea	word_88F96(pc),a1
+Mantis_HeadChild_Init:
+		lea	Mantis_HeadChildObjData(pc),a1
 		jmp	SetUp_ObjAttributes3(pc)
 ; ---------------------------------------------------------------------------
 
-loc_88F72:
+Mantis_HeadChild_UpdateFrame:
 		movea.w	parent3(a0),a1
 		move.b	#5,mapping_frame(a0)
 		tst.w	y_vel(a1)
-		bmi.s	locret_88F88
+		bmi.s	Mantis_HeadChild_Return
 		move.b	#4,mapping_frame(a0)
 
-locret_88F88:
+Mantis_HeadChild_Return:
 		rts
 ; ---------------------------------------------------------------------------
 ObjDat_Mantis:
@@ -185837,19 +185837,19 @@ ObjDat_Mantis:
 		dc.w make_art_tile(ArtTile_Mantis,1,0)
 		dc.w   $280
 		dc.b  $14, $1C,   0, $1A
-word_88F96:
+Mantis_HeadChildObjData:
 		dc.w   $200
 		dc.b    4,   4,   5,   0
-ChildObjDat_88F9C:
+Mantis_HeadChild:
 		dc.w 1-1
-		dc.l loc_88F50
+		dc.l Mantis_HeadChild_Main
 		dc.b   -9, -$B
-byte_88FA4:
+Mantis_PreJumpAnimData:
 		dc.b    0,   0
 		dc.b    1,   2
 		dc.b    2,   0
 		dc.b  $F4
-byte_88FAB:
+Mantis_LandingAnimData:
 		dc.b    2,   0
 		dc.b    1,   2
 		dc.b    3,   2
