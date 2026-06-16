b486faf8b7f09434fb2e253065440b8337636d65
Farrell <farrell@bluetoaster.net>
2026-06-16 15:44:19 +0100

    docs: label LBZ Death Egg small map frames

--- Levels/LBZ/Misc Object Data/Map - Death Egg Small.asm
+++ Levels/LBZ/Misc Object Data/Map - Death Egg Small.asm
@@ -1,40 +1,40 @@
 Map_LBZDeathEggSmall_:
-		dc.w Frame_364824-Map_LBZDeathEggSmall_
-		dc.w Frame_36483E-Map_LBZDeathEggSmall_
-		dc.w Frame_364846-Map_LBZDeathEggSmall_
-		dc.w Frame_36484E-Map_LBZDeathEggSmall_
-		dc.w Frame_364856-Map_LBZDeathEggSmall_
-		dc.w Frame_36485E-Map_LBZDeathEggSmall_
-		dc.w Frame_364866-Map_LBZDeathEggSmall_
-		dc.w Frame_36486E-Map_LBZDeathEggSmall_
-		dc.w Frame_364876-Map_LBZDeathEggSmall_
-		dc.w Frame_36487E-Map_LBZDeathEggSmall_
-		dc.w Frame_364886-Map_LBZDeathEggSmall_
-		dc.w Frame_36488E-Map_LBZDeathEggSmall_
-Frame_364824:	dc.w 4
+		dc.w Map_LBZDeathEggSmall_Frame0-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame1-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame2-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame3-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame4-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame5-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame6-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame7-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame8-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame9-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame10-Map_LBZDeathEggSmall_
+		dc.w Map_LBZDeathEggSmall_Frame11-Map_LBZDeathEggSmall_
+Map_LBZDeathEggSmall_Frame0:	dc.w 4	; Frame_364824
 		dc.b  $E8,  $B,   0,   0, $FF, $E8
 		dc.b  $E8,  $B,   0,  $C,   0,   0
 		dc.b    8,   9,   0, $18, $FF, $E8
 		dc.b    8,   9,   0, $1E,   0,   0
-Frame_36483E:	dc.w 1
+Map_LBZDeathEggSmall_Frame1:	dc.w 1	; Frame_36483E
 		dc.b  $FC,   0,   0, $24, $FF, $FC
-Frame_364846:	dc.w 1
+Map_LBZDeathEggSmall_Frame2:	dc.w 1	; Frame_364846
 		dc.b  $FC,   0,   0, $25, $FF, $FC
-Frame_36484E:	dc.w 1
+Map_LBZDeathEggSmall_Frame3:	dc.w 1	; Frame_36484E
 		dc.b  $FC,   0,   0, $26, $FF, $FC
-Frame_364856:	dc.w 1
+Map_LBZDeathEggSmall_Frame4:	dc.w 1	; Frame_364856
 		dc.b  $FC,   0,   0, $27, $FF, $FC
-Frame_36485E:	dc.w 1
+Map_LBZDeathEggSmall_Frame5:	dc.w 1	; Frame_36485E
 		dc.b  $FC,   0,   0, $28, $FF, $FC
-Frame_364866:	dc.w 1
+Map_LBZDeathEggSmall_Frame6:	dc.w 1	; Frame_364866
 		dc.b  $FC,   0,   0, $29, $FF, $FC
-Frame_36486E:	dc.w 1
+Map_LBZDeathEggSmall_Frame7:	dc.w 1	; Frame_36486E
 		dc.b  $F4,  $A,   0, $2A, $FF, $F4
-Frame_364876:	dc.w 1
+Map_LBZDeathEggSmall_Frame8:	dc.w 1	; Frame_364876
 		dc.b  $F4,  $A,   0, $33, $FF, $F4
-Frame_36487E:	dc.w 1
+Map_LBZDeathEggSmall_Frame9:	dc.w 1	; Frame_36487E
 		dc.b  $F4,  $A,   0, $3C, $FF, $F4
-Frame_364886:	dc.w 1
+Map_LBZDeathEggSmall_Frame10:	dc.w 1	; Frame_364886
 		dc.b  $F4,  $A,   0, $45, $FF, $F4
-Frame_36488E:	dc.w 1
+Map_LBZDeathEggSmall_Frame11:	dc.w 1	; Frame_36488E
 		dc.b  $F8,   5,   0, $4E, $FF, $F8
