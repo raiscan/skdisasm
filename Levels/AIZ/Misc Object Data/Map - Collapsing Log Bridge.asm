d61f0af82da9379d5bedfd4b9fa9280a43061f6d
Farrell <farrell@bluetoaster.net>
2026-06-16 04:05:32 +0100

    docs: label AIZ bridge mappings

--- Levels/AIZ/Misc Object Data/Map - Collapsing Log Bridge.asm
+++ Levels/AIZ/Misc Object Data/Map - Collapsing Log Bridge.asm
@@ -1,10 +1,10 @@
 Map_AIZCollapsingLogBridge_:
-		dc.w word_2B076-Map_AIZCollapsingLogBridge_
-		dc.w word_2B076-Map_AIZCollapsingLogBridge_
-		dc.w word_2B07E-Map_AIZCollapsingLogBridge_
-word_2B076:	dc.w 1
+		dc.w Map_AIZCollapsingLogBridge_Frame0-Map_AIZCollapsingLogBridge_
+		dc.w Map_AIZCollapsingLogBridge_Frame0-Map_AIZCollapsingLogBridge_
+		dc.w Map_AIZCollapsingLogBridge_Frame1-Map_AIZCollapsingLogBridge_
+Map_AIZCollapsingLogBridge_Frame0:	dc.w 1
 		dc.b  $F8,  $D,   0, $2D, $FF, $F0
-word_2B07E:	dc.w 3
+Map_AIZCollapsingLogBridge_Frame1:	dc.w 3
 		dc.b  $F8,  $D,   0, $2D, $FF, $F0
 		dc.b    8,   8,   1, $2D, $FF, $F8
 		dc.b  $10,   4,   1, $30,   0,   0
--- Levels/AIZ/Misc Object Data/Map - Flipping Bridge.asm
+++ Levels/AIZ/Misc Object Data/Map - Flipping Bridge.asm
@@ -1,51 +1,51 @@
 Map_AIZFlippingBridge_:
-		dc.w Frame_22A388-Map_AIZFlippingBridge_
-		dc.w Frame_22A396-Map_AIZFlippingBridge_
-		dc.w Frame_22A350-Map_AIZFlippingBridge_
-		dc.w Frame_22A35E-Map_AIZFlippingBridge_
-		dc.w Frame_22A36C-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-		dc.w Frame_22A37A-Map_AIZFlippingBridge_
-Frame_22A350:	dc.w 2
+		dc.w Map_AIZFlippingBridge_Frame0-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame1-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame2-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame3-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame4-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+		dc.w Map_AIZFlippingBridge_Frame5-Map_AIZFlippingBridge_
+Map_AIZFlippingBridge_Frame2:	dc.w 2
 		dc.b  $F0,   7,   0, $D6, $FF, $F0
 		dc.b  $F0,   7,   8, $D6,   0,   0
-Frame_22A35E:	dc.w 2
+Map_AIZFlippingBridge_Frame3:	dc.w 2
 		dc.b  $F4,   6,   0, $DE, $FF, $F0
 		dc.b  $F4,   6,   8, $DE,   0,   0
-Frame_22A36C:	dc.w 2
+Map_AIZFlippingBridge_Frame4:	dc.w 2
 		dc.b  $F8,   5,   0, $E4, $FF, $F0
 		dc.b  $F8,   5,   8, $E4,   0,   0
-Frame_22A37A:	dc.w 2
+Map_AIZFlippingBridge_Frame5:	dc.w 2
 		dc.b  $FC,   4,   0, $E8, $FF, $F0
 		dc.b  $FC,   4,   8, $E8,   0,   0
-Frame_22A388:	dc.w 2
+Map_AIZFlippingBridge_Frame0:	dc.w 2
 		dc.b  $F8,   5, $10, $E4, $FF, $F0
 		dc.b  $F8,   5, $18, $E4,   0,   0
-Frame_22A396:	dc.w 2
+Map_AIZFlippingBridge_Frame1:	dc.w 2
 		dc.b  $F4,   6, $10, $DE, $FF, $F0
 		dc.b  $F4,   6, $18, $DE,   0,   0
