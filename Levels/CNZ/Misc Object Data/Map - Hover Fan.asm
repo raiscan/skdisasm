73fd6c7b0ecd94940bef8db3c235ecf7cc90e208
Farrell <farrell@bluetoaster.net>
2026-06-16 08:26:21 +0100

    docs: label CNZ light fan mappings

--- Levels/CNZ/Misc Object Data/Map - Hover Fan.asm
+++ Levels/CNZ/Misc Object Data/Map - Hover Fan.asm
@@ -1,28 +1,28 @@
 Map_CNZHoverFan_:
-		dc.w Frame_231020-Map_CNZHoverFan_
-		dc.w Frame_23102E-Map_CNZHoverFan_
-		dc.w Frame_231048-Map_CNZHoverFan_
-		dc.w Frame_23106E-Map_CNZHoverFan_
-		dc.w Frame_23106E-Map_CNZHoverFan_
-		dc.w Frame_23106E-Map_CNZHoverFan_
-		dc.w Frame_23106E-Map_CNZHoverFan_
-		dc.w Frame_23106E-Map_CNZHoverFan_
-Frame_231020:	dc.w 2
+		dc.w Map_CNZHoverFan_Frame0-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame1-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame2-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame3-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame3-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame3-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame3-Map_CNZHoverFan_
+		dc.w Map_CNZHoverFan_Frame3-Map_CNZHoverFan_
+Map_CNZHoverFan_Frame0:	dc.w 2
 		dc.b  $F0,  $C, $FF, $1C, $FF, $F0
 		dc.b  $F8,  $D,   0,   0, $FF, $F0
-Frame_23102E:	dc.w 4
+Map_CNZHoverFan_Frame1:	dc.w 4
 		dc.b  $F0,  $C, $FF, $1C, $FF, $E0
 		dc.b  $F8,  $D,   0,   0, $FF, $E0
 		dc.b  $F0,  $C, $FF, $1C,   0,   0
 		dc.b  $F8,  $D,   0,   0,   0,   0
-Frame_231048:	dc.w 6
+Map_CNZHoverFan_Frame2:	dc.w 6
 		dc.b  $F0,  $C, $FF, $1C, $FF, $D0
 		dc.b  $F8,  $D,   0,   0, $FF, $D0
 		dc.b  $F0,  $C, $FF, $1C, $FF, $F0
 		dc.b  $F8,  $D,   0,   0, $FF, $F0
 		dc.b  $F0,  $C, $FF, $1C,   0, $10
 		dc.b  $F8,  $D,   0,   0,   0, $10
-Frame_23106E:	dc.w 8
+Map_CNZHoverFan_Frame3:	dc.w 8
 		dc.b  $F0,  $C, $FF, $1C, $FF, $C0
 		dc.b  $F8,  $D,   0,   0, $FF, $C0
 		dc.b  $F0,  $C, $FF, $1C, $FF, $E0
