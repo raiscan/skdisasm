8b21041334a8f0862df77204c2abc93af40c3384
Farrell <farrell@bluetoaster.net>
2026-06-16 16:22:43 +0100

    docs: label ICZ platform map frames

--- Levels/ICZ/Misc Object Data/Map - Platforms.asm
+++ Levels/ICZ/Misc Object Data/Map - Platforms.asm
@@ -1,116 +1,116 @@
 Map_ICZPlatforms_:
-		dc.w Frame_363C94-Map_ICZPlatforms_
-		dc.w Frame_363CAE-Map_ICZPlatforms_
-		dc.w Frame_363CBC-Map_ICZPlatforms_
-		dc.w Frame_363CD6-Map_ICZPlatforms_
-		dc.w Frame_363CE4-Map_ICZPlatforms_
-		dc.w Frame_363CEC-Map_ICZPlatforms_
-		dc.w Frame_363CFA-Map_ICZPlatforms_
-		dc.w Frame_363D08-Map_ICZPlatforms_
-		dc.w Frame_363D22-Map_ICZPlatforms_
-		dc.w Frame_363D2A-Map_ICZPlatforms_
-		dc.w Frame_363D32-Map_ICZPlatforms_
-		dc.w Frame_363D3A-Map_ICZPlatforms_
-		dc.w Frame_363D42-Map_ICZPlatforms_
-		dc.w Frame_363D4A-Map_ICZPlatforms_
-		dc.w Frame_363D52-Map_ICZPlatforms_
-		dc.w Frame_363D5A-Map_ICZPlatforms_
-		dc.w Frame_363D62-Map_ICZPlatforms_
-		dc.w Frame_363D6A-Map_ICZPlatforms_
-		dc.w Frame_363D72-Map_ICZPlatforms_
-		dc.w Frame_363D7A-Map_ICZPlatforms_
-		dc.w Frame_363D82-Map_ICZPlatforms_
-		dc.w Frame_363D8A-Map_ICZPlatforms_
-		dc.w Frame_363D92-Map_ICZPlatforms_
-		dc.w Frame_363D9A-Map_ICZPlatforms_
-		dc.w Frame_363DA2-Map_ICZPlatforms_
-		dc.w Frame_363DAA-Map_ICZPlatforms_
-		dc.w Frame_363DB2-Map_ICZPlatforms_
-		dc.w Frame_363DD2-Map_ICZPlatforms_
-		dc.w Frame_363E10-Map_ICZPlatforms_
-		dc.w Frame_363E18-Map_ICZPlatforms_
-		dc.w Frame_363E20-Map_ICZPlatforms_
-		dc.w Frame_363E2E-Map_ICZPlatforms_
-		dc.w Frame_363E3C-Map_ICZPlatforms_
-		dc.w Frame_363E4A-Map_ICZPlatforms_
-		dc.w Frame_363E58-Map_ICZPlatforms_
-		dc.w Frame_363E66-Map_ICZPlatforms_
-		dc.w Frame_363E6E-Map_ICZPlatforms_
-		dc.w Frame_363E76-Map_ICZPlatforms_
-		dc.w Frame_363E7E-Map_ICZPlatforms_
-		dc.w Frame_363E86-Map_ICZPlatforms_
-Frame_363C94:	dc.w 4
+		dc.w Map_ICZPlatforms_Frame0-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame1-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame2-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame3-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame4-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame5-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame6-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame7-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame8-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame9-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame10-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame11-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame12-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame13-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame14-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame15-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame16-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame17-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame18-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame19-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame20-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame21-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame22-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame23-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame24-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame25-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame26-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame27-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame28-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame29-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame30-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame31-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame32-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame33-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame34-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame35-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame36-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame37-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame38-Map_ICZPlatforms_
+		dc.w Map_ICZPlatforms_Frame39-Map_ICZPlatforms_
+Map_ICZPlatforms_Frame0:	dc.w 4	; Frame_363C94
 		dc.b  $EC,  $F,   0,   0, $FF, $E0
 		dc.b   $C,   8,   0, $10, $FF, $E8
 		dc.b  $EC,  $F,   8,   0,   0,   0
 		dc.b   $C,   8,   8, $10,   0,   0
-Frame_363CAE:	dc.w 2
+Map_ICZPlatforms_Frame1:	dc.w 2	; Frame_363CAE
 		dc.b  $F8,   5,   0, $13, $FF, $F0
 		dc.b  $F8,   5,   0, $13,   0,   0
-Frame_363CBC:	dc.w 4
+Map_ICZPlatforms_Frame2:	dc.w 4	; Frame_363CBC
 		dc.b  $EC,   9,   0, $17, $FF, $EC
 		dc.b  $EC,   6,   0, $1D,   0,   4
 		dc.b  $FC,   6,   0, $23, $FF, $EC
 		dc.b    4,   9,   0, $29, $FF, $FC
-Frame_363CD6:	dc.w 2
+Map_ICZPlatforms_Frame3:	dc.w 2	; Frame_363CD6
 		dc.b  $F0,  $B,   0, $2F, $FF, $E8
 		dc.b  $F0,  $B,   8, $2F,   0,   0
-Frame_363CE4:	dc.w 1
+Map_ICZPlatforms_Frame4:	dc.w 1	; Frame_363CE4
 		dc.b  $F0,  $F,   0, $3B, $FF, $F0
-Frame_363CEC:	dc.w 2
+Map_ICZPlatforms_Frame5:	dc.w 2	; Frame_363CEC
 		dc.b  $E8,  $E,   0, $4B, $FF, $F0
 		dc.b    0,  $E, $10, $4B, $FF, $F0
-Frame_363CFA:	dc.w 2
+Map_ICZPlatforms_Frame6:	dc.w 2	; Frame_363CFA
 		dc.b  $F4,   6,   0, $57, $FF, $F0
 		dc.b  $F4,   6,   8, $57,   0,   0
-Frame_363D08:	dc.w 4
+Map_ICZPlatforms_Frame7:	dc.w 4	; Frame_363D08
 		dc.b  $F8,   8,   0, $5D, $FF, $F0
 		dc.b  $F0,   9,   0, $60,   0,   8
 		dc.b    0,  $D,   0, $66, $FF, $E0
 		dc.b    0,  $D,   8, $66,   0,   0
-Frame_363D22:	dc.w 1
+Map_ICZPlatforms_Frame8:	dc.w 1	; Frame_363D22
 		dc.b  $F8,   5,   0, $6E, $FF, $F8
-Frame_363D2A:	dc.w 1
+Map_ICZPlatforms_Frame9:	dc.w 1	; Frame_363D2A
 		dc.b  $F8,   5,   0, $72, $FF, $F8
-Frame_363D32:	dc.w 1
+Map_ICZPlatforms_Frame10:	dc.w 1	; Frame_363D32
 		dc.b  $FC,   0,   0, $76, $FF, $FC
-Frame_363D3A:	dc.w 1
+Map_ICZPlatforms_Frame11:	dc.w 1	; Frame_363D3A
 		dc.b  $FC,   0,   0, $77, $FF, $FC
-Frame_363D42:	dc.w 1
+Map_ICZPlatforms_Frame12:	dc.w 1	; Frame_363D42
 		dc.b  $FC,   0,   0, $78, $FF, $FC
-Frame_363D4A:	dc.w 1
+Map_ICZPlatforms_Frame13:	dc.w 1	; Frame_363D4A
 		dc.b  $FC,   0,   0, $79, $FF, $FC
-Frame_363D52:	dc.w 1
+Map_ICZPlatforms_Frame14:	dc.w 1	; Frame_363D52
 		dc.b  $FC,   0,   0, $7A, $FF, $FC
-Frame_363D5A:	dc.w 1
+Map_ICZPlatforms_Frame15:	dc.w 1	; Frame_363D5A
 		dc.b  $FC,   0,   0, $7B, $FF, $FC
-Frame_363D62:	dc.w 1
+Map_ICZPlatforms_Frame16:	dc.w 1	; Frame_363D62
 		dc.b  $FC,   0,   0, $7C, $FF, $FC
-Frame_363D6A:	dc.w 1
+Map_ICZPlatforms_Frame17:	dc.w 1	; Frame_363D6A
 		dc.b  $FC,   0,   0, $7D, $FF, $FC
-Frame_363D72:	dc.w 1
+Map_ICZPlatforms_Frame18:	dc.w 1	; Frame_363D72
 		dc.b  $F8,   5,   0, $7E, $FF, $F8
-Frame_363D7A:	dc.w 1
+Map_ICZPlatforms_Frame19:	dc.w 1	; Frame_363D7A
 		dc.b  $F8,   5,   0, $82, $FF, $F8
-Frame_363D82:	dc.w 1
+Map_ICZPlatforms_Frame20:	dc.w 1	; Frame_363D82
 		dc.b  $F8,   5,   0, $86, $FF, $F8
-Frame_363D8A:	dc.w 1
+Map_ICZPlatforms_Frame21:	dc.w 1	; Frame_363D8A
 		dc.b  $F8,   5,   0, $8A, $FF, $F8
-Frame_363D92:	dc.w 1
+Map_ICZPlatforms_Frame22:	dc.w 1	; Frame_363D92
 		dc.b  $FC,   0,   0, $8E, $FF, $FC
-Frame_363D9A:	dc.w 1
+Map_ICZPlatforms_Frame23:	dc.w 1	; Frame_363D9A
 		dc.b  $F8,   5,   0, $8F, $FF, $F8
-Frame_363DA2:	dc.w 1
+Map_ICZPlatforms_Frame24:	dc.w 1	; Frame_363DA2
 		dc.b  $F8,   5,   0, $93, $FF, $F8
-Frame_363DAA:	dc.w 1
+Map_ICZPlatforms_Frame25:	dc.w 1	; Frame_363DAA
 		dc.b  $F4,  $A,   0, $97, $FF, $F4
-Frame_363DB2:	dc.w 5
+Map_ICZPlatforms_Frame26:	dc.w 5	; Frame_363DB2
 		dc.b  $F0,   1,   0,   0, $FF, $E8
 		dc.b  $F0,   5,   0,   2, $FF, $F8
 		dc.b  $F0,   1,   8,   0,   0, $10
 		dc.b    0,  $D,   0,   6, $FF, $E0
 		dc.b    0,  $D,   8,   6,   0,   0
-Frame_363DD2:	dc.w $A
+Map_ICZPlatforms_Frame27:	dc.w $A	; Frame_363DD2
 		dc.b  $D0,  $F,   0,  $E, $FF, $D8
 		dc.b  $D0,  $F,   0, $1E, $FF, $F8
 		dc.b  $F0,  $F,   0, $2E, $FF, $D8
@@ -121,31 +121,31 @@ Frame_363DD2:	dc.w $A
 		dc.b  $E8,   6,   0, $51,   0, $18
 		dc.b    0,   6,   0, $57,   0, $18
 		dc.b  $18,   6, $10, $51,   0, $18
-Frame_363E10:	dc.w 1
+Map_ICZPlatforms_Frame28:	dc.w 1	; Frame_363E10
 		dc.b  $F4,  $A,   0, $5D, $FF, $F4
-Frame_363E18:	dc.w 1
+Map_ICZPlatforms_Frame29:	dc.w 1	; Frame_363E18
 		dc.b  $F4,  $A,   0, $66, $FF, $F4
-Frame_363E20:	dc.w 2
+Map_ICZPlatforms_Frame30:	dc.w 2	; Frame_363E20
 		dc.b  $F0,   7,   0,   0, $FF, $F0
 		dc.b  $F0,   7,   8,   0,   0,   0
-Frame_363E2E:	dc.w 2
+Map_ICZPlatforms_Frame31:	dc.w 2	; Frame_363E2E
 		dc.b  $F4,  $A,   0,   8, $FF, $E8
 		dc.b  $F4,  $A,   8,   8,   0,   0
-Frame_363E3C:	dc.w 2
+Map_ICZPlatforms_Frame32:	dc.w 2	; Frame_363E3C
 		dc.b  $F8,  $D,   0, $11, $FF, $E0
 		dc.b  $F8,  $D,   0, $19,   0,   0
-Frame_363E4A:	dc.w 2
+Map_ICZPlatforms_Frame33:	dc.w 2	; Frame_363E4A
 		dc.b  $F0,   7,   0, $21, $FF, $F8
 		dc.b  $10,   4,   0, $29, $FF, $F8
-Frame_363E58:	dc.w 2
+Map_ICZPlatforms_Frame34:	dc.w 2	; Frame_363E58
 		dc.b  $F0,  $F,   0, $2B, $FF, $F0
 		dc.b  $10,  $C,   0, $3B, $FF, $F0
-Frame_363E66:	dc.w 1
+Map_ICZPlatforms_Frame35:	dc.w 1	; Frame_363E66
 		dc.b  $F8,   5,   8, $7E, $FF, $F8
-Frame_363E6E:	dc.w 1
+Map_ICZPlatforms_Frame36:	dc.w 1	; Frame_363E6E
 		dc.b  $F8,   5, $18, $86, $FF, $F8
-Frame_363E76:	dc.w 1
+Map_ICZPlatforms_Frame37:	dc.w 1	; Frame_363E76
 		dc.b  $F6,  $A,   8, $5D, $FF, $F5
-Frame_363E7E:	dc.w 1
+Map_ICZPlatforms_Frame38:	dc.w 1	; Frame_363E7E
 		dc.b  $F4,  $A, $10, $66, $FF, $F5
-Frame_363E86:	dc.w 0
+Map_ICZPlatforms_Frame39:	dc.w 0	; Frame_363E86
