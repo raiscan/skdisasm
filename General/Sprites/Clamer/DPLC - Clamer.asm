5e5af2f55bf950824f614a9b7e40522b18156ce8
Farrell <farrell@bluetoaster.net>
2026-06-16 11:26:40 +0100

    docs: label Clamer map and dplc frames

--- General/Sprites/Clamer/DPLC - Clamer.asm
+++ General/Sprites/Clamer/DPLC - Clamer.asm
@@ -1,35 +1,35 @@
 DPLC_Clamer_:
-		dc.w Frame_361A8E-DPLC_Clamer_
-		dc.w Frame_361A92-DPLC_Clamer_
-		dc.w Frame_361A96-DPLC_Clamer_
-		dc.w Frame_361A9A-DPLC_Clamer_
-		dc.w Frame_361A9E-DPLC_Clamer_
-		dc.w Frame_361AA2-DPLC_Clamer_
-		dc.w Frame_361AA6-DPLC_Clamer_
-		dc.w Frame_361AAA-DPLC_Clamer_
-		dc.w Frame_361AB0-DPLC_Clamer_
-		dc.w Frame_361AB4-DPLC_Clamer_
-		dc.w Frame_361AB8-DPLC_Clamer_
-Frame_361A8E:	dc.w 0
+		dc.w DPLC_Clamer_Frame0-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame1-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame2-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame3-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame4-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame5-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame6-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame7-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame8-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame9-DPLC_Clamer_
+		dc.w DPLC_Clamer_Frame10-DPLC_Clamer_
+DPLC_Clamer_Frame0:	dc.w 0			; Frame_361A8E
 		dc.w $000F
-Frame_361A92:	dc.w 0
+DPLC_Clamer_Frame1:	dc.w 0			; Frame_361A92
 		dc.w $010F
-Frame_361A96:	dc.w 0
+DPLC_Clamer_Frame2:	dc.w 0			; Frame_361A96
 		dc.w $020B
-Frame_361A9A:	dc.w 0
+DPLC_Clamer_Frame3:	dc.w 0			; Frame_361A9A
 		dc.w $02CB
-Frame_361A9E:	dc.w 0
+DPLC_Clamer_Frame4:	dc.w 0			; Frame_361A9E
 		dc.w $038B
-Frame_361AA2:	dc.w 0
+DPLC_Clamer_Frame5:	dc.w 0			; Frame_361AA2
 		dc.w $044F
-Frame_361AA6:	dc.w 0
+DPLC_Clamer_Frame6:	dc.w 0			; Frame_361AA6
 		dc.w $054F
-Frame_361AAA:	dc.w 1
+DPLC_Clamer_Frame7:	dc.w 1			; Frame_361AAA
 		dc.w $0641
 		dc.w $066F
-Frame_361AB0:	dc.w 0
+DPLC_Clamer_Frame8:	dc.w 0			; Frame_361AB0
 		dc.w $076F
-Frame_361AB4:	dc.w 0
+DPLC_Clamer_Frame9:	dc.w 0			; Frame_361AB4
 		dc.w $0863
-Frame_361AB8:	dc.w 0
+DPLC_Clamer_Frame10:	dc.w 0			; Frame_361AB8
 		dc.w $000F
--- General/Sprites/Clamer/Map - Clamer.asm
+++ General/Sprites/Clamer/Map - Clamer.asm
@@ -1,37 +1,37 @@
 Map_Clamer_:
-		dc.w Frame_361AD4-Map_Clamer_
-		dc.w Frame_361ADC-Map_Clamer_
-		dc.w Frame_361AE4-Map_Clamer_
-		dc.w Frame_361AEC-Map_Clamer_
-		dc.w Frame_361AF4-Map_Clamer_
-		dc.w Frame_361AFC-Map_Clamer_
-		dc.w Frame_361B04-Map_Clamer_
-		dc.w Frame_361B0C-Map_Clamer_
-		dc.w Frame_361B1A-Map_Clamer_
-		dc.w Frame_361B22-Map_Clamer_
-		dc.w Frame_361B2A-Map_Clamer_
-		dc.w Frame_361B32-Map_Clamer_
-Frame_361AD4:	dc.w 1
+		dc.w Map_Clamer_Frame0-Map_Clamer_
+		dc.w Map_Clamer_Frame1-Map_Clamer_
+		dc.w Map_Clamer_Frame2-Map_Clamer_
+		dc.w Map_Clamer_Frame3-Map_Clamer_
+		dc.w Map_Clamer_Frame4-Map_Clamer_
+		dc.w Map_Clamer_Frame5-Map_Clamer_
+		dc.w Map_Clamer_Frame6-Map_Clamer_
+		dc.w Map_Clamer_Frame7-Map_Clamer_
+		dc.w Map_Clamer_Frame8-Map_Clamer_
+		dc.w Map_Clamer_Frame9-Map_Clamer_
+		dc.w Map_Clamer_Frame10-Map_Clamer_
+		dc.w Map_Clamer_Frame11-Map_Clamer_
+Map_Clamer_Frame0:	dc.w 1			; Frame_361AD4
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_361ADC:	dc.w 1
+Map_Clamer_Frame1:	dc.w 1			; Frame_361ADC
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_361AE4:	dc.w 1
+Map_Clamer_Frame2:	dc.w 1			; Frame_361AE4
 		dc.b  $F8,  $E,   0,   0, $FF, $F0
-Frame_361AEC:	dc.w 1
+Map_Clamer_Frame3:	dc.w 1			; Frame_361AEC
 		dc.b  $F8,  $E,   0,   0, $FF, $F0
-Frame_361AF4:	dc.w 1
+Map_Clamer_Frame4:	dc.w 1			; Frame_361AF4
 		dc.b  $F8,  $E,   0,   0, $FF, $F0
-Frame_361AFC:	dc.w 1
+Map_Clamer_Frame5:	dc.w 1			; Frame_361AFC
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_361B04:	dc.w 1
+Map_Clamer_Frame6:	dc.w 1			; Frame_361B04
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_361B0C:	dc.w 2
+Map_Clamer_Frame7:	dc.w 2			; Frame_361B0C
 		dc.b  $F8,   1,   0,   0, $FF, $E8
 		dc.b  $F0,  $F,   0,   2, $FF, $F0
-Frame_361B1A:	dc.w 1
+Map_Clamer_Frame8:	dc.w 1			; Frame_361B1A
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_361B22:	dc.w 1
+Map_Clamer_Frame9:	dc.w 1			; Frame_361B22
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-Frame_361B2A:	dc.w 1
+Map_Clamer_Frame10:	dc.w 1			; Frame_361B2A
 		dc.b  $EF,  $F,   0,   0, $FF, $F0
-Frame_361B32:	dc.w 0
+Map_Clamer_Frame11:	dc.w 0			; Frame_361B32
