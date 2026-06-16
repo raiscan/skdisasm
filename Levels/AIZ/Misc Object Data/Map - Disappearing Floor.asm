1e604041f42c36ce0c510fe3b4ece80f0b3746cb
Farrell <farrell@bluetoaster.net>
2026-06-16 04:29:21 +0100

    docs: label AIZ disappearing floor data

--- Levels/AIZ/Misc Object Data/Map - Disappearing Floor.asm
+++ Levels/AIZ/Misc Object Data/Map - Disappearing Floor.asm
@@ -1,33 +1,33 @@
 Map_AIZDisappearingFloor_:
-		dc.w Frame_2294C8-Map_AIZDisappearingFloor_
-		dc.w Frame_2294CC-Map_AIZDisappearingFloor_
-		dc.w Frame_2294DA-Map_AIZDisappearingFloor_
-		dc.w Frame_2294FA-Map_AIZDisappearingFloor_
-		dc.w Frame_229514-Map_AIZDisappearingFloor_
-		dc.w Frame_229540-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame0-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame1-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame2-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame3-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame4-Map_AIZDisappearingFloor_
+		dc.w Map_AIZDisappearingFloor_Frame5-Map_AIZDisappearingFloor_
 Map_AIZDisappearingFloor2:
 Map_AIZDisappearingFloor2_:
-		dc.w Frame_229566-Map_AIZDisappearingFloor2_
-		dc.w Frame_2295A4-Map_AIZDisappearingFloor2_
-		dc.w Frame_2295D6-Map_AIZDisappearingFloor2_
-		dc.w Frame_229608-Map_AIZDisappearingFloor2_
-Frame_2294C8:	dc.w 0
+		dc.w Map_AIZDisappearingFloor2_Frame0-Map_AIZDisappearingFloor2_
+		dc.w Map_AIZDisappearingFloor2_Frame1-Map_AIZDisappearingFloor2_
+		dc.w Map_AIZDisappearingFloor2_Frame2-Map_AIZDisappearingFloor2_
+		dc.w Map_AIZDisappearingFloor2_Frame3-Map_AIZDisappearingFloor2_
+Map_AIZDisappearingFloor_Frame0:	dc.w 0
 		dc.w 0
-Frame_2294CC:	dc.w 2
+Map_AIZDisappearingFloor_Frame1:	dc.w 2
 		dc.b  $E8,   5,   3, $1D, $FF, $F0
 		dc.b  $E8,   5,  $B, $1D,   0,   0
-Frame_2294DA:	dc.w 5
+Map_AIZDisappearingFloor_Frame2:	dc.w 5
 		dc.b  $E8,   5,  $B, $1D, $FF, $E8
 		dc.b  $E8,   5,   3, $1D, $FF, $F8
 		dc.b  $E8,   5,   3, $1D,   0,   8
 		dc.b  $F8,   5, $13, $1D, $FF, $F0
 		dc.b  $F8,   5, $13, $1D,   0,   0
-Frame_2294FA:	dc.w 4
+Map_AIZDisappearingFloor_Frame3:	dc.w 4
 		dc.b  $E8,   7,   3, $21, $FF, $E0
 		dc.b  $E8,   7,  $B, $21, $FF, $F0
 		dc.b  $E8,   7,   3, $21,   0,   0
 		dc.b  $E8,   7,  $B, $21,   0, $10
-Frame_229514:	dc.w 7
+Map_AIZDisappearingFloor_Frame4:	dc.w 7
 		dc.b  $E8,  $D,   0, $2A, $FF, $E0
 		dc.b  $E8,  $D,   0, $2A,   0,   0
 		dc.b  $F8,  $D,   3, $29, $FF, $E0
@@ -35,14 +35,14 @@ Frame_229514:	dc.w 7
 		dc.b    8,   5, $1B, $1D, $FF, $E8
 		dc.b    8,   5,  $B, $1D, $FF, $F8
 		dc.b    8,   5, $13, $1D,   0,   8
-Frame_229540:	dc.w 6
+Map_AIZDisappearingFloor_Frame5:	dc.w 6
 		dc.b  $E8,  $D,   0, $2A, $FF, $E0
 		dc.b  $F8,  $D, $10, $16, $FF, $E0
 		dc.b    8,  $D,   0, $32, $FF, $E0
 		dc.b  $E8,  $D,   0, $2A,   0,   0
 		dc.b  $F8,  $D, $10, $16,   0,   0
 		dc.b    8,  $D,   8, $32,   0,   0
-Frame_229566:	dc.w $A
+Map_AIZDisappearingFloor2_Frame0:	dc.w $A
 		dc.b  $D8,  $A,   0, $49, $FF, $D0
 		dc.b  $E0,   9,   0, $52, $FF, $E8
 		dc.b  $E0,   9,   8, $52,   0,   0
@@ -53,16 +53,16 @@ Frame_229566:	dc.w $A
 		dc.b    0,   7,   8, $5C,   0, $18
 		dc.b  $10,   9,   0, $64, $FF, $E8
 		dc.b  $10,   9,   8, $64,   0,   0
-Frame_2295A4:	dc.w 8
+Map_AIZDisappearingFloor2_Frame1:	dc.w 8
 		dc.b  $E0,   7, $10, $5C, $FF, $D8
 		dc.b  $E0,   9,   8, $64, $FF, $E8
 		dc.b  $E0,   9,   0, $64,   0,   0
 		dc.b  $E0,   7, $18, $5C,   0, $18
 		dc.b    0,   7,   0, $6A, $FF, $D8
 		dc.b    0,   7,   8, $6A,   0, $18
-Frame_2295CA:	dc.b  $10,   9, $10, $64, $FF, $E8
+Map_AIZDisappearingFloor2_Frame1_Cont:	dc.b  $10,   9, $10, $64, $FF, $E8
 		dc.b  $10,   9, $18, $64,   0,   0
-Frame_2295D6:	dc.w 8
+Map_AIZDisappearingFloor2_Frame2:	dc.w 8
 		dc.b  $E0,   7, $10, $6A, $FF, $D8
 		dc.b  $E0,   9,   0, $64, $FF, $E8
 		dc.b  $E0,   9,   8, $64,   0,   0
@@ -71,7 +71,7 @@ Frame_2295D6:	dc.w 8
 		dc.b    0,   7,   8, $5C,   0, $18
 		dc.b  $10,   9,   0, $52, $FF, $E8
 		dc.b  $10,   9,   8, $52,   0,   0
-Frame_229608:	dc.w 8
+Map_AIZDisappearingFloor2_Frame3:	dc.w 8
 		dc.b  $E0,   7, $10, $5C, $FF, $D8
 		dc.b  $E0,   9, $18, $52, $FF, $E8
 		dc.b  $E0,   9, $10, $52,   0,   0
