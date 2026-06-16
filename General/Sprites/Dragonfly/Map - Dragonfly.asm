5d5b3a7f616d0a0784a8837dfabc051c56c9ccdf
Farrell <farrell@bluetoaster.net>
2026-06-16 11:49:51 +0100

    docs: label MHZ badnik map frames

--- General/Sprites/Dragonfly/Map - Dragonfly.asm
+++ General/Sprites/Dragonfly/Map - Dragonfly.asm
@@ -1,48 +1,48 @@
 ; Dragonfly mappings
 Map_Dragonfly_:
-		dc.w word_8DFEE-Map_Dragonfly_
-		dc.w word_8E008-Map_Dragonfly_
-		dc.w word_8E022-Map_Dragonfly_
-		dc.w word_8E03C-Map_Dragonfly_
-		dc.w word_8E056-Map_Dragonfly_
-		dc.w word_8E070-Map_Dragonfly_
-		dc.w word_8E078-Map_Dragonfly_
-		dc.w word_8E080-Map_Dragonfly_
-		dc.w word_8E08E-Map_Dragonfly_
-		dc.w word_8E09C-Map_Dragonfly_
-word_8DFEE:	dc.w 4
+		dc.w Map_Dragonfly_Frame0-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame1-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame2-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame3-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame4-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame5-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame6-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame7-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame8-Map_Dragonfly_
+		dc.w Map_Dragonfly_Frame9-Map_Dragonfly_
+Map_Dragonfly_Frame0:	dc.w 4		; word_8DFEE
 		dc.b  $FA,   4,   0,   4, $FF, $F8
 		dc.b  $FC,   0,   0,   6, $FF, $F0
 		dc.b  $FC,   0,   0,   6,   0,   8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_8E008:	dc.w 4
+Map_Dragonfly_Frame1:	dc.w 4		; word_8E008
 		dc.b  $FB,   4,   0,   4, $FF, $F8
 		dc.b  $FC,   0,   0,   6, $FF, $F0
 		dc.b  $FC,   0,   0,   6,   0,   8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_8E022:	dc.w 4
+Map_Dragonfly_Frame2:	dc.w 4		; word_8E022
 		dc.b  $FC,   4,   0,   4, $FF, $F8
 		dc.b  $FC,   0,   0,   6, $FF, $F0
 		dc.b  $FC,   0,   0,   6,   0,   8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_8E03C:	dc.w 4
+Map_Dragonfly_Frame3:	dc.w 4		; word_8E03C
 		dc.b  $FD,   4,   0,   4, $FF, $F8
 		dc.b  $FC,   0,   0,   6, $FF, $F0
 		dc.b  $FC,   0,   0,   6,   0,   8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_8E056:	dc.w 4
+Map_Dragonfly_Frame4:	dc.w 4		; word_8E056
 		dc.b  $FE,   4,   0,   4, $FF, $F8
 		dc.b  $FC,   0,   0,   6, $FF, $F0
 		dc.b  $FC,   0,   0,   6,   0,   8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_8E070:	dc.w 1
+Map_Dragonfly_Frame5:	dc.w 1		; word_8E070
 		dc.b  $FC,   4,   0,   7, $FF, $F8
-word_8E078:	dc.w 1
+Map_Dragonfly_Frame6:	dc.w 1		; word_8E078
 		dc.b  $FC,   0,   0,   9, $FF, $FC
-word_8E080:	dc.w 2
+Map_Dragonfly_Frame7:	dc.w 2		; word_8E080
 		dc.b  $F8,   8,   0,  $A, $FF, $D8
 		dc.b  $F8,   8,   8,  $A,   0, $10
-word_8E08E:	dc.w 2
+Map_Dragonfly_Frame8:	dc.w 2		; word_8E08E
 		dc.b    0,   8, $10,  $A, $FF, $D8
 		dc.b    0,   8, $18,  $A,   0, $10
-word_8E09C:	dc.w 0
+Map_Dragonfly_Frame9:	dc.w 0		; word_8E09C
--- General/Sprites/Mushmeanie/Map - Mushmeanie.asm
+++ General/Sprites/Mushmeanie/Map - Mushmeanie.asm
@@ -1,17 +1,17 @@
 ; Mushmeanie mappings
 Map_Mushmeanie_:
-		dc.w word_8DD00-Map_Mushmeanie_
-		dc.w word_8DD08-Map_Mushmeanie_
-		dc.w word_8DD16-Map_Mushmeanie_
-		dc.w word_8DD24-Map_Mushmeanie_
-word_8DD00:	dc.w 1
+		dc.w Map_Mushmeanie_Frame0-Map_Mushmeanie_
+		dc.w Map_Mushmeanie_Frame1-Map_Mushmeanie_
+		dc.w Map_Mushmeanie_Frame2-Map_Mushmeanie_
+		dc.w Map_Mushmeanie_Frame3-Map_Mushmeanie_
+Map_Mushmeanie_Frame0:	dc.w 1		; word_8DD00
 		dc.b  $F8,   9,   0,   0, $FF, $F4
-word_8DD08:	dc.w 2
+Map_Mushmeanie_Frame1:	dc.w 2		; word_8DD08
 		dc.b  $F8,   5,   0,   6, $FF, $F8
 		dc.b    7,   0,   0,  $A, $FF, $FC
-word_8DD16:	dc.w 2
+Map_Mushmeanie_Frame2:	dc.w 2		; word_8DD16
 		dc.b  $F8,   5,   0,   6, $FF, $F8
 		dc.b    2,   1,   0,  $B, $FF, $FC
-word_8DD24:	dc.w 2
+Map_Mushmeanie_Frame3:	dc.w 2		; word_8DD24
 		dc.b  $F8,   5,   0,   6, $FF, $F8
 		dc.b    5,   1,   0,  $D, $FF, $FC
