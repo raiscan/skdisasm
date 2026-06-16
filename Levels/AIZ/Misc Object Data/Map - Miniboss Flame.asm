292dd918d4f5ec729a0fe385f66c17b6bdf6bd1e
Farrell <farrell@bluetoaster.net>
2026-06-16 04:16:09 +0100

    docs: label AIZ miniboss mappings

--- Levels/AIZ/Misc Object Data/Map - Miniboss Flame.asm
+++ Levels/AIZ/Misc Object Data/Map - Miniboss Flame.asm
@@ -1,16 +1,16 @@
 Map_AIZMinibossFlame_:
-		dc.w Frame_361666-Map_AIZMinibossFlame_
-		dc.w Frame_36166E-Map_AIZMinibossFlame_
-		dc.w Frame_361676-Map_AIZMinibossFlame_
-		dc.w Frame_36167E-Map_AIZMinibossFlame_
-		dc.w Frame_361686-Map_AIZMinibossFlame_
-Frame_361666:	dc.w 1
+		dc.w Map_AIZMinibossFlame_Frame0-Map_AIZMinibossFlame_
+		dc.w Map_AIZMinibossFlame_Frame1-Map_AIZMinibossFlame_
+		dc.w Map_AIZMinibossFlame_Frame2-Map_AIZMinibossFlame_
+		dc.w Map_AIZMinibossFlame_Frame3-Map_AIZMinibossFlame_
+		dc.w Map_AIZMinibossFlame_Frame4-Map_AIZMinibossFlame_
+Map_AIZMinibossFlame_Frame0:	dc.w 1
 		dc.b  $EC,  $F,   0,   0, $FF, $EC
-Frame_36166E:	dc.w 1
+Map_AIZMinibossFlame_Frame1:	dc.w 1
 		dc.b  $F0,  $F,   0, $10, $FF, $F0
-Frame_361676:	dc.w 1
+Map_AIZMinibossFlame_Frame2:	dc.w 1
 		dc.b  $F0,  $F,   0, $20, $FF, $F0
-Frame_36167E:	dc.w 1
+Map_AIZMinibossFlame_Frame3:	dc.w 1
 		dc.b  $F0,  $F,   0, $30, $FF, $F0
-Frame_361686:	dc.w 1
+Map_AIZMinibossFlame_Frame4:	dc.w 1
 		dc.b  $EB,  $F,   0, $40, $FF, $F0
--- Levels/AIZ/Misc Object Data/Map - Miniboss Small.asm
+++ Levels/AIZ/Misc Object Data/Map - Miniboss Small.asm
@@ -1,10 +1,10 @@
 Map_AIZMinibossSmall_:
-		dc.w Frame_3625F0-Map_AIZMinibossSmall_
-		dc.w Frame_3625F8-Map_AIZMinibossSmall_
-		dc.w Frame_362600-Map_AIZMinibossSmall_
-Frame_3625F0:	dc.w 1
+		dc.w Map_AIZMinibossSmall_Frame0-Map_AIZMinibossSmall_
+		dc.w Map_AIZMinibossSmall_Frame1-Map_AIZMinibossSmall_
+		dc.w Map_AIZMinibossSmall_Frame2-Map_AIZMinibossSmall_
+Map_AIZMinibossSmall_Frame0:	dc.w 1
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-Frame_3625F8:	dc.w 1
+Map_AIZMinibossSmall_Frame1:	dc.w 1
 		dc.b  $F8,   5,   0,   9, $FF, $F8
-Frame_362600:	dc.w 1
+Map_AIZMinibossSmall_Frame2:	dc.w 1
 		dc.b  $FC,   0,   0,  $D, $FF, $FC
