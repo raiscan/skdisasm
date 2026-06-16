8e24c580cc846d98f08e5a08d6101fa91e2b8400
Farrell <farrell@bluetoaster.net>
2026-06-16 12:21:59 +0100

    docs: label AIZ badnik map frames

--- General/Sprites/Caterkiller Jr/Map - Caterkiller Jr.asm
+++ General/Sprites/Caterkiller Jr/Map - Caterkiller Jr.asm
@@ -1,25 +1,25 @@
 Map_CaterKillerJr_:
-		dc.w Frame_361A24-Map_CaterKillerJr_
-		dc.w Frame_361A2C-Map_CaterKillerJr_
-		dc.w Frame_361A34-Map_CaterKillerJr_
-		dc.w Frame_361A3C-Map_CaterKillerJr_
-		dc.w Frame_361A50-Map_CaterKillerJr_
-		dc.w Frame_361A64-Map_CaterKillerJr_
-Frame_361A24:	dc.w 1
+		dc.w Map_CaterKillerJr_Frame0-Map_CaterKillerJr_
+		dc.w Map_CaterKillerJr_Frame1-Map_CaterKillerJr_
+		dc.w Map_CaterKillerJr_Frame2-Map_CaterKillerJr_
+		dc.w Map_CaterKillerJr_Frame3-Map_CaterKillerJr_
+		dc.w Map_CaterKillerJr_Frame4-Map_CaterKillerJr_
+		dc.w Map_CaterKillerJr_Frame5-Map_CaterKillerJr_
+Map_CaterKillerJr_Frame0:	dc.w 1	; Frame_361A24
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-Frame_361A2C:	dc.w 1
+Map_CaterKillerJr_Frame1:	dc.w 1	; Frame_361A2C
 		dc.b  $F4,   6,   0,   4, $FF, $F8
-Frame_361A34:	dc.w 1
+Map_CaterKillerJr_Frame2:	dc.w 1	; Frame_361A34
 		dc.b  $F8,   5,   0,  $A, $FF, $F8
-Frame_361A3C:	dc.w 3
+Map_CaterKillerJr_Frame3:	dc.w 3	; Frame_361A3C
 		dc.b  $EC,   5,   0,  $E,   0,   0
 		dc.b  $FC,   8,   0, $17,   0,   0
 		dc.b    4,   5, $10,  $E,   0,   0
-Frame_361A50:	dc.w 3
+Map_CaterKillerJr_Frame4:	dc.w 3	; Frame_361A50
 		dc.b  $EC,   5,   0, $12,   0,   0
 		dc.b  $FC,   4,   0, $1A,   0,   5
 		dc.b    4,   5, $10, $12,   0,   0
-Frame_361A64:	dc.w 3
+Map_CaterKillerJr_Frame5:	dc.w 3	; Frame_361A64
 		dc.b  $F4,   0,   0, $16,   0,   8
 		dc.b  $FC,   0,   0, $1C,   0, $10
 		dc.b    4,   0, $10, $16,   0,   8
--- General/Sprites/Rhinobot/Map - Rhinobot.asm
+++ General/Sprites/Rhinobot/Map - Rhinobot.asm
@@ -1,30 +1,30 @@
 Map_Rhinobot_:
-		dc.w Frame_3615B8-Map_Rhinobot_
-		dc.w Frame_3615CC-Map_Rhinobot_
-		dc.w Frame_3615E0-Map_Rhinobot_
-		dc.w Frame_3615E8-Map_Rhinobot_
-		dc.w Frame_3615F6-Map_Rhinobot_
-		dc.w Frame_3615FE-Map_Rhinobot_
-		dc.w Frame_361606-Map_Rhinobot_
-		dc.w Frame_36160E-Map_Rhinobot_
-Frame_3615B8:	dc.w 3
+		dc.w Map_Rhinobot_Frame0-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame1-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame2-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame3-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame4-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame5-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame6-Map_Rhinobot_
+		dc.w Map_Rhinobot_Frame7-Map_Rhinobot_
+Map_Rhinobot_Frame0:	dc.w 3	; Frame_3615B8
 		dc.b    0,   1,   0,   0, $FF, $E8
 		dc.b  $F0,  $F,   0,   2, $FF, $F0
 		dc.b  $F8,   2,   0, $12,   0, $10
-Frame_3615CC:	dc.w 3
+Map_Rhinobot_Frame1:	dc.w 3	; Frame_3615CC
 		dc.b    0,   1,   0,   0, $FF, $EC
 		dc.b  $F0,   4,   0,   2, $FF, $F4
 		dc.b  $F8,  $E,   0,   4, $FF, $F4
-Frame_3615E0:	dc.w 1
+Map_Rhinobot_Frame2:	dc.w 1	; Frame_3615E0
 		dc.b  $F0,  $B,   0,   0, $FF, $F4
-Frame_3615E8:	dc.w 2
+Map_Rhinobot_Frame3:	dc.w 2	; Frame_3615E8
 		dc.b  $F0,  $F,   0,   0, $FF, $EC
 		dc.b  $F8,   1,   0, $10,   0,  $C
-Frame_3615F6:	dc.w 1
+Map_Rhinobot_Frame4:	dc.w 1	; Frame_3615F6
 		dc.b    0,   8,   0,   0, $FF, $F4
-Frame_3615FE:	dc.w 1
+Map_Rhinobot_Frame5:	dc.w 1	; Frame_3615FE
 		dc.b  $F8,   9,   0,   0, $FF, $F4
-Frame_361606:	dc.w 1
+Map_Rhinobot_Frame6:	dc.w 1	; Frame_361606
 		dc.b  $F8,   9,   0,   0, $FF, $F4
-Frame_36160E:	dc.w 1
+Map_Rhinobot_Frame7:	dc.w 1	; Frame_36160E
 		dc.b    0,   8,   0,   0, $FF, $F4
