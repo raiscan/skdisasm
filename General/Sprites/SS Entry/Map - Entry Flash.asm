7eb8e1960288c1f5e96ae0ccae04e962b93d2601
Farrell <farrell@bluetoaster.net>
2026-06-16 22:10:16 +0100

    docs: label special stage entry data

--- General/Sprites/SS Entry/Map - Entry Flash.asm
+++ General/Sprites/SS Entry/Map - Entry Flash.asm
@@ -1,23 +1,23 @@
 Map_SSEntryFlash_:
-		dc.w word_61B30-Map_SSEntryFlash_
-		dc.w word_61B56-Map_SSEntryFlash_
-		dc.w word_61B7C-Map_SSEntryFlash_
-		dc.w word_61BAE-Map_SSEntryFlash_
-word_61B30:	dc.w 6
+		dc.w Map_SSEntryFlash_Frame0-Map_SSEntryFlash_
+		dc.w Map_SSEntryFlash_Frame1-Map_SSEntryFlash_
+		dc.w Map_SSEntryFlash_Frame2-Map_SSEntryFlash_
+		dc.w Map_SSEntryFlash_Frame3-Map_SSEntryFlash_
+Map_SSEntryFlash_Frame0:	dc.w 6	; word_61B30
 		dc.b  $E0,   8,   0,   0,   0,   0
 		dc.b  $E8,   9,   0,   3,   0,   8
 		dc.b  $F8,   0,   0,   9,   0, $18
 		dc.b  $18,   8, $10,   0,   0,   0
 		dc.b    8,   9, $10,   3,   0,   8
 		dc.b    0,   0, $10,   9,   0, $18
-word_61B56:	dc.w 6
+Map_SSEntryFlash_Frame1:	dc.w 6	; word_61B56
 		dc.b  $E0,  $C,   0,   0,   0,   0
 		dc.b  $E8,  $D,   0,   4,   0,   8
 		dc.b  $F8,   4,   0,  $C,   0, $18
 		dc.b  $18,  $C, $10,   0,   0,   0
 		dc.b    8,  $D, $10,   4,   0,   8
 		dc.b    0,   4, $10,  $C,   0, $18
-word_61B7C:	dc.w 8
+Map_SSEntryFlash_Frame2:	dc.w 8	; word_61B7C
 		dc.b  $E0,   8,   0,   0,   0,   0
 		dc.b  $E8,  $E,   0,   3,   0,   0
 		dc.b  $18,   8, $10,   0,   0,   0
@@ -26,7 +26,7 @@ word_61B7C:	dc.w 8
 		dc.b  $F0,   1,   8, $13, $FF, $F8
 		dc.b  $10,   5, $18,  $F, $FF, $F0
 		dc.b    0,   1, $18, $13, $FF, $F8
-word_61BAE:	dc.w 8
+Map_SSEntryFlash_Frame3:	dc.w 8	; word_61BAE
 		dc.b  $E0,   8,   0,   0,   0,   0
 		dc.b  $E8,  $E,   0,   3,   0,   0
 		dc.b  $18,   8, $10,   0,   0,   0
@@ -35,6 +35,7 @@ word_61BAE:	dc.w 8
 		dc.b  $E8,  $E,   8,   3, $FF, $E0
 		dc.b  $18,   8, $18,   0, $FF, $E8
 		dc.b    0,  $E, $18,   3, $FF, $E0
+Map_SSEntryFlash_ExtraFrame:
 		dc.w 4
 		dc.b  $E0,   5,   0,   0,   0,   0
 		dc.b  $F0,   1,   0,   4,   0,   0
--- General/Sprites/SS Entry/Map - Entry Ring.asm
+++ General/Sprites/SS Entry/Map - Entry Ring.asm
@@ -1,53 +1,53 @@
 Map_SSEntryRing_:
-		dc.w word_619F8-Map_SSEntryRing_
-		dc.w word_61A00-Map_SSEntryRing_
-		dc.w word_61A08-Map_SSEntryRing_
-		dc.w word_61A10-Map_SSEntryRing_
-		dc.w word_61A18-Map_SSEntryRing_
-		dc.w word_61A20-Map_SSEntryRing_
-		dc.w word_61A2E-Map_SSEntryRing_
-		dc.w word_61A48-Map_SSEntryRing_
-		dc.w word_61A62-Map_SSEntryRing_
-		dc.w word_61A7C-Map_SSEntryRing_
-		dc.w word_61A96-Map_SSEntryRing_
-		dc.w word_61AA4-Map_SSEntryRing_
-word_619F8:	dc.w 1
+		dc.w Map_SSEntryRing_Frame0-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame1-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame2-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame3-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame4-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame5-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame6-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame7-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame8-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame9-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame10-Map_SSEntryRing_
+		dc.w Map_SSEntryRing_Frame11-Map_SSEntryRing_
+Map_SSEntryRing_Frame0:	dc.w 1	; word_619F8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_61A00:	dc.w 1
+Map_SSEntryRing_Frame1:	dc.w 1	; word_61A00
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_61A08:	dc.w 1
+Map_SSEntryRing_Frame2:	dc.w 1	; word_61A08
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-word_61A10:	dc.w 1
+Map_SSEntryRing_Frame3:	dc.w 1	; word_61A10
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-word_61A18:	dc.w 1
+Map_SSEntryRing_Frame4:	dc.w 1	; word_61A18
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-word_61A20:	dc.w 2
+Map_SSEntryRing_Frame5:	dc.w 2	; word_61A20
 		dc.b  $EC,  $F,   0,   0, $FF, $F0
 		dc.b   $C,  $C,   0, $10, $FF, $F0
-word_61A2E:	dc.w 4
+Map_SSEntryRing_Frame6:	dc.w 4	; word_61A2E
 		dc.b  $EC,   9,   0,   0, $FF, $EC
 		dc.b  $EC,   6,   0,   6,   0,   4
 		dc.b    4,   9,   0,  $C, $FF, $FC
 		dc.b  $FC,   6,   0, $12, $FF, $EC
-word_61A48:	dc.w 4
+Map_SSEntryRing_Frame7:	dc.w 4	; word_61A48
 		dc.b  $E8,   8,   0,   0, $FF, $F4
 		dc.b  $F0,  $F,   0,   3, $FF, $EC
 		dc.b  $F0,   3,   0, $13,   0,  $C
 		dc.b  $10,   8,   0, $17, $FF, $F4
-word_61A62:	dc.w 4
+Map_SSEntryRing_Frame8:	dc.w 4	; word_61A62
 		dc.b  $E0,  $F,   0,   0, $FF, $E0
 		dc.b  $E0,  $F,   0, $10,   0,   0
 		dc.b    0,  $F,   0, $20, $FF, $E0
 		dc.b    0,  $F,   0, $30,   0,   0
-word_61A7C:	dc.w 4
+Map_SSEntryRing_Frame9:	dc.w 4	; word_61A7C
 		dc.b  $E0,  $F,   0,   0, $FF, $E4
 		dc.b  $E0,  $B,   0, $10,   0,   4
 		dc.b    0,  $F,   0, $1C, $FF, $E4
 		dc.b    0,  $B,   0, $2C,   0,   4
-word_61A96:	dc.w 2
+Map_SSEntryRing_Frame10:	dc.w 2	; word_61A96
 		dc.b  $E0,  $B,   0,   0, $FF, $F4
 		dc.b    0,  $B,   0,  $C, $FF, $F4
-word_61AA4:	dc.w 4
+Map_SSEntryRing_Frame11:	dc.w 4	; word_61AA4
 		dc.b  $E0,  $F,   0,   0, $FF, $E4
 		dc.b  $E0,  $B,   0, $10,   0,   4
 		dc.b    0,  $F,   0, $1C, $FF, $E4
