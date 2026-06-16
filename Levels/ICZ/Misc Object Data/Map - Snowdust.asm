734b00e3de5baafca796361995f9b5615b12547f
Farrell <farrell@bluetoaster.net>
2026-06-16 13:12:03 +0100

    docs: label ICZ snowdust map frames

--- Levels/ICZ/Misc Object Data/Map - Snowdust.asm
+++ Levels/ICZ/Misc Object Data/Map - Snowdust.asm
@@ -1,19 +1,19 @@
 Map_ICZSnowdust_:
-		dc.w Frame_361F1A-Map_ICZSnowdust_
-		dc.w Frame_361F22-Map_ICZSnowdust_
-		dc.w Frame_361F2A-Map_ICZSnowdust_
-		dc.w Frame_361F32-Map_ICZSnowdust_
-		dc.w Frame_361F3A-Map_ICZSnowdust_
-		dc.w Frame_361F42-Map_ICZSnowdust_
-Frame_361F1A:	dc.w 1
+		dc.w Map_ICZSnowdust_Frame0-Map_ICZSnowdust_
+		dc.w Map_ICZSnowdust_Frame1-Map_ICZSnowdust_
+		dc.w Map_ICZSnowdust_Frame2-Map_ICZSnowdust_
+		dc.w Map_ICZSnowdust_Frame3-Map_ICZSnowdust_
+		dc.w Map_ICZSnowdust_Frame4-Map_ICZSnowdust_
+		dc.w Map_ICZSnowdust_Frame5-Map_ICZSnowdust_
+Map_ICZSnowdust_Frame0:	dc.w 1	; Frame_361F1A
 		dc.b  $F8,   0,   0,   0, $FF, $FC
-Frame_361F22:	dc.w 1
+Map_ICZSnowdust_Frame1:	dc.w 1	; Frame_361F22
 		dc.b  $F8,   0,   8,   0, $FF, $FC
-Frame_361F2A:	dc.w 1
+Map_ICZSnowdust_Frame2:	dc.w 1	; Frame_361F2A
 		dc.b  $F0,   1,   0,   1, $FF, $FC
-Frame_361F32:	dc.w 1
+Map_ICZSnowdust_Frame3:	dc.w 1	; Frame_361F32
 		dc.b  $F0,   1,   8,   1, $FF, $FC
-Frame_361F3A:	dc.w 1
+Map_ICZSnowdust_Frame4:	dc.w 1	; Frame_361F3A
 		dc.b  $E8,   2,   0,   3, $FF, $FC
-Frame_361F42:	dc.w 1
+Map_ICZSnowdust_Frame5:	dc.w 1	; Frame_361F42
 		dc.b  $E8,   2,   8,   3, $FF, $FC
