4f7b41b4d3bc29dd132137b408aff77601b5cdb0
Farrell <farrell@bluetoaster.net>
2026-06-16 12:56:13 +0100

    docs: label LBZ platform map frames

--- Levels/LBZ/Misc Object Data/Map - Moving Platform.asm
+++ Levels/LBZ/Misc Object Data/Map - Moving Platform.asm
@@ -1,13 +1,13 @@
 Map_LBZMovingPlatform_:
-		dc.w word_2533E-Map_LBZMovingPlatform_
-		dc.w word_2534C-Map_LBZMovingPlatform_
-		dc.w word_2535A-Map_LBZMovingPlatform_
-word_2533E:	dc.w 2
+		dc.w Map_LBZMovingPlatform_Frame0-Map_LBZMovingPlatform_
+		dc.w Map_LBZMovingPlatform_Frame1-Map_LBZMovingPlatform_
+		dc.w Map_LBZMovingPlatform_Frame2-Map_LBZMovingPlatform_
+Map_LBZMovingPlatform_Frame0:	dc.w 2	; word_2533E
 		dc.b  $F8,  $D,   0,   0, $FF, $E0
 		dc.b  $F8,  $D,   8,   0,   0,   0
-word_2534C:	dc.w 2
+Map_LBZMovingPlatform_Frame1:	dc.w 2	; word_2534C
 		dc.b  $F8,  $D,   0,   8, $FF, $E0
 		dc.b  $F8,  $D,   8,   0,   0,   0
-word_2535A:	dc.w 2
+Map_LBZMovingPlatform_Frame2:	dc.w 2	; word_2535A
 		dc.b    4,  $C,   0,   0, $FF, $E0
 		dc.b    4,  $C,   8,   0,   0,   0
--- Levels/LBZ/Misc Object Data/Map - Unused Bar Platform.asm
+++ Levels/LBZ/Misc Object Data/Map - Unused Bar Platform.asm
@@ -1,5 +1,5 @@
 Map_LBZUnusedBarPlatform_:
-		dc.w word_254EC-Map_LBZUnusedBarPlatform_
-word_254EC:	dc.w 2
+		dc.w Map_LBZUnusedBarPlatform_Frame0-Map_LBZUnusedBarPlatform_
+Map_LBZUnusedBarPlatform_Frame0:	dc.w 2	; word_254EC
 		dc.b    8,  $C,   0,   0, $FF, $E0
 		dc.b    8,  $C,   8,   0,   0,   0
