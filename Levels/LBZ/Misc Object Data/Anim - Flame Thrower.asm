956b223d69c25304a82d832bbcd774df31a814a7
Farrell <farrell@bluetoaster.net>
2026-06-16 13:21:46 +0100

    docs: label LBZ flame thrower data

--- Levels/LBZ/Misc Object Data/Anim - Flame Thrower.asm
+++ Levels/LBZ/Misc Object Data/Anim - Flame Thrower.asm
@@ -1,4 +1,4 @@
 Ani_LBZFlameThrower_:
-		dc.w byte_26480-Ani_LBZFlameThrower_
-byte_26480:	dc.b    3,   1,   2,   3,   4,   5,   3,   4,   6,   7,   8, $FC
+		dc.w Ani_LBZFlameThrower_Seq0-Ani_LBZFlameThrower_
+Ani_LBZFlameThrower_Seq0:	dc.b    3,   1,   2,   3,   4,   5,   3,   4,   6,   7,   8, $FC	; byte_26480
 		even
