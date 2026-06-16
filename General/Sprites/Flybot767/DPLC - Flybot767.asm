965527460c14085e16cfb52ac197ed945d7819de
Farrell <farrell@bluetoaster.net>
2026-06-16 11:19:43 +0100

    docs: label Flybot767 map and dplc frames

--- General/Sprites/Flybot767/DPLC - Flybot767.asm
+++ General/Sprites/Flybot767/DPLC - Flybot767.asm
@@ -1,86 +1,86 @@
 DPLC_Flybot767_:
-		dc.w Frame_360816-DPLC_Flybot767_
-		dc.w Frame_36081C-DPLC_Flybot767_
-		dc.w Frame_360822-DPLC_Flybot767_
-		dc.w Frame_360828-DPLC_Flybot767_
-		dc.w Frame_36082E-DPLC_Flybot767_
-		dc.w Frame_360834-DPLC_Flybot767_
-		dc.w Frame_36083A-DPLC_Flybot767_
-		dc.w Frame_360840-DPLC_Flybot767_
-		dc.w Frame_360846-DPLC_Flybot767_
-		dc.w Frame_36084C-DPLC_Flybot767_
-		dc.w Frame_360852-DPLC_Flybot767_
-		dc.w Frame_360858-DPLC_Flybot767_
-		dc.w Frame_36085E-DPLC_Flybot767_
-		dc.w Frame_360866-DPLC_Flybot767_
-		dc.w Frame_36086C-DPLC_Flybot767_
-		dc.w Frame_360870-DPLC_Flybot767_
-		dc.w Frame_360876-DPLC_Flybot767_
-		dc.w Frame_36087C-DPLC_Flybot767_
-		dc.w Frame_360882-DPLC_Flybot767_
-		dc.w Frame_360888-DPLC_Flybot767_
-		dc.w Frame_36088E-DPLC_Flybot767_
-Frame_360816:	dc.w 1
+		dc.w DPLC_Flybot767_Frame0-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame1-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame2-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame3-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame4-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame5-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame6-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame7-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame8-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame9-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame10-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame11-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame12-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame13-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame14-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame15-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame16-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame17-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame18-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame19-DPLC_Flybot767_
+		dc.w DPLC_Flybot767_Frame20-DPLC_Flybot767_
+DPLC_Flybot767_Frame0:	dc.w 1			; Frame_360816
 		dc.w $0083
 		dc.w $0007
-Frame_36081C:	dc.w 1
+DPLC_Flybot767_Frame1:	dc.w 1			; Frame_36081C
 		dc.w $00C3
 		dc.w $0007
-Frame_360822:	dc.w 1
+DPLC_Flybot767_Frame2:	dc.w 1			; Frame_360822
 		dc.w $0103
 		dc.w $0007
-Frame_360828:	dc.w 1
+DPLC_Flybot767_Frame3:	dc.w 1			; Frame_360828
 		dc.w $0143
 		dc.w $0007
-Frame_36082E:	dc.w 1
+DPLC_Flybot767_Frame4:	dc.w 1			; Frame_36082E
 		dc.w $0183
 		dc.w $0007
-Frame_360834:	dc.w 1
+DPLC_Flybot767_Frame5:	dc.w 1			; Frame_360834
 		dc.w $01C3
 		dc.w $0007
-Frame_36083A:	dc.w 1
+DPLC_Flybot767_Frame6:	dc.w 1			; Frame_36083A
 		dc.w $0202
 		dc.w $0007
-Frame_360840:	dc.w 1
+DPLC_Flybot767_Frame7:	dc.w 1			; Frame_360840
 		dc.w $0232
 		dc.w $0007
-Frame_360846:	dc.w 1
+DPLC_Flybot767_Frame8:	dc.w 1			; Frame_360846
 		dc.w $0263
 		dc.w $0007
-Frame_36084C:	dc.w 1
+DPLC_Flybot767_Frame9:	dc.w 1			; Frame_36084C
 		dc.w $02A3
 		dc.w $0007
-Frame_360852:	dc.w 1
+DPLC_Flybot767_Frame10:	dc.w 1			; Frame_360852
 		dc.w $02E7
 		dc.w $0360
-Frame_360858:	dc.w 1
+DPLC_Flybot767_Frame11:	dc.w 1			; Frame_360858
 		dc.w $0377
 		dc.w $03F0
-Frame_36085E:	dc.w 2
+DPLC_Flybot767_Frame12:	dc.w 2			; Frame_36085E
 		dc.w $0407
 		dc.w $0480
 		dc.w $0490
-Frame_360866:	dc.w 1
+DPLC_Flybot767_Frame13:	dc.w 1			; Frame_360866
 		dc.w $04A7
 		dc.w $0521
-Frame_36086C:	dc.w 0
+DPLC_Flybot767_Frame14:	dc.w 0			; Frame_36086C
 		dc.w $0547
-Frame_360870:	dc.w 1
+DPLC_Flybot767_Frame15:	dc.w 1			; Frame_360870
 		dc.w $05C1
 		dc.w $05E7
-Frame_360876:	dc.w 1
+DPLC_Flybot767_Frame16:	dc.w 1			; Frame_360876
 		dc.w $0665
 		dc.w $06C2
-Frame_36087C:	dc.w 1
+DPLC_Flybot767_Frame17:	dc.w 1			; Frame_36087C
 		dc.w $06F1
 		dc.w $0718
-Frame_360882:	dc.w 1
+DPLC_Flybot767_Frame18:	dc.w 1			; Frame_360882
 		dc.w $07A8
 		dc.w $0830
-Frame_360888:	dc.w 1
+DPLC_Flybot767_Frame19:	dc.w 1			; Frame_360888
 		dc.w $0848
 		dc.w $08D0
-Frame_36088E:	dc.w 2
+DPLC_Flybot767_Frame20:	dc.w 2			; Frame_36088E
 		dc.w $08E0
 		dc.w $08F8
 		dc.w $0980
