c0e51d4fcd5acac246c853e82a15901970665047
Farrell <farrell@bluetoaster.net>
2026-06-16 08:42:55 +0100

    docs: label CNZ balloon cannon data

--- Levels/CNZ/Misc Object Data/Anim - Balloon.asm
+++ Levels/CNZ/Misc Object Data/Anim - Balloon.asm
@@ -1,22 +1,22 @@
 Ani_CNZBalloon_:
-		dc.w byte_31872-Ani_CNZBalloon_
-		dc.w byte_31878-Ani_CNZBalloon_
-		dc.w byte_3187C-Ani_CNZBalloon_
-		dc.w byte_31882-Ani_CNZBalloon_
-		dc.w byte_31886-Ani_CNZBalloon_
-		dc.w byte_3188C-Ani_CNZBalloon_
-		dc.w byte_31890-Ani_CNZBalloon_
-		dc.w byte_31896-Ani_CNZBalloon_
-		dc.w byte_3189A-Ani_CNZBalloon_
-		dc.w byte_318A0-Ani_CNZBalloon_
-byte_31872:	dc.b    7,   0,   1,   2,   1, $FF
-byte_31878:	dc.b    2,   3,   4, $FB
-byte_3187C:	dc.b    7,   5,   6,   7,   6, $FF
-byte_31882:	dc.b    2,   8,   9, $FB
-byte_31886:	dc.b    7,  $A,  $B,  $C,  $B, $FF
-byte_3188C:	dc.b    2,  $D,  $E, $FB
-byte_31890:	dc.b    7,  $F, $10, $11, $10, $FF
-byte_31896:	dc.b    2, $12, $13, $FB
-byte_3189A:	dc.b    7, $14, $15, $16, $15, $FF
-byte_318A0:	dc.b    2, $17, $18, $FB
+		dc.w Ani_CNZBalloon_Seq0-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq1-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq2-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq3-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq4-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq5-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq6-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq7-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq8-Ani_CNZBalloon_
+		dc.w Ani_CNZBalloon_Seq9-Ani_CNZBalloon_
+Ani_CNZBalloon_Seq0:	dc.b    7,   0,   1,   2,   1, $FF
+Ani_CNZBalloon_Seq1:	dc.b    2,   3,   4, $FB
+Ani_CNZBalloon_Seq2:	dc.b    7,   5,   6,   7,   6, $FF
+Ani_CNZBalloon_Seq3:	dc.b    2,   8,   9, $FB
+Ani_CNZBalloon_Seq4:	dc.b    7,  $A,  $B,  $C,  $B, $FF
+Ani_CNZBalloon_Seq5:	dc.b    2,  $D,  $E, $FB
+Ani_CNZBalloon_Seq6:	dc.b    7,  $F, $10, $11, $10, $FF
+Ani_CNZBalloon_Seq7:	dc.b    2, $12, $13, $FB
+Ani_CNZBalloon_Seq8:	dc.b    7, $14, $15, $16, $15, $FF
+Ani_CNZBalloon_Seq9:	dc.b    2, $17, $18, $FB
 		even
--- Levels/CNZ/Misc Object Data/DPLC - Cannon.asm
+++ Levels/CNZ/Misc Object Data/DPLC - Cannon.asm
@@ -1,23 +1,23 @@
 DPLC_CNZCannon_:
-		dc.w word_31B9A-DPLC_CNZCannon_
-		dc.w word_31B94-DPLC_CNZCannon_
-		dc.w word_31B8E-DPLC_CNZCannon_
-		dc.w word_31B88-DPLC_CNZCannon_
-		dc.w word_31B84-DPLC_CNZCannon_
-		dc.w word_31B88-DPLC_CNZCannon_
-		dc.w word_31B8E-DPLC_CNZCannon_
-		dc.w word_31B94-DPLC_CNZCannon_
-		dc.w word_31B9A-DPLC_CNZCannon_
-word_31B84:	dc.w 1
+		dc.w DPLC_CNZCannon_Frame0-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame1-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame2-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame3-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame4-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame3-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame2-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame1-DPLC_CNZCannon_
+		dc.w DPLC_CNZCannon_Frame0-DPLC_CNZCannon_
+DPLC_CNZCannon_Frame4:	dc.w 1
 		dc.w $B000
-word_31B88:	dc.w 2
+DPLC_CNZCannon_Frame3:	dc.w 2
 		dc.w $700C
 		dc.w $8014
-word_31B8E:	dc.w 2
+DPLC_CNZCannon_Frame2:	dc.w 2
 		dc.w $701D
 		dc.w $B025
-word_31B94:	dc.w 2
+DPLC_CNZCannon_Frame1:	dc.w 2
 		dc.w $7031
 		dc.w $8039
-word_31B9A:	dc.w 1
+DPLC_CNZCannon_Frame0:	dc.w 1
 		dc.w $B042
