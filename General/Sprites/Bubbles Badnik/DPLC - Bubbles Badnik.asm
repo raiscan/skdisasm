f32e1766322262737a718f9a80c795f76ff4702f
Farrell <farrell@bluetoaster.net>
2026-06-16 10:40:08 +0100

    docs: label shared badnik map frames

--- General/Sprites/Bubbles Badnik/DPLC - Bubbles Badnik.asm
+++ General/Sprites/Bubbles Badnik/DPLC - Bubbles Badnik.asm
@@ -1,20 +1,20 @@
 DPLC_BubblesBadnik_:
-		dc.w Frame_361C4A-DPLC_BubblesBadnik_
-		dc.w Frame_361C50-DPLC_BubblesBadnik_
-		dc.w Frame_361C54-DPLC_BubblesBadnik_
-		dc.w Frame_361C58-DPLC_BubblesBadnik_
-		dc.w Frame_361C5E-DPLC_BubblesBadnik_
-Frame_361C4A:	dc.w 1
+		dc.w DPLC_BubblesBadnik_Frame0-DPLC_BubblesBadnik_
+		dc.w DPLC_BubblesBadnik_Frame1-DPLC_BubblesBadnik_
+		dc.w DPLC_BubblesBadnik_Frame2-DPLC_BubblesBadnik_
+		dc.w DPLC_BubblesBadnik_Frame3-DPLC_BubblesBadnik_
+		dc.w DPLC_BubblesBadnik_Frame4-DPLC_BubblesBadnik_
+DPLC_BubblesBadnik_Frame0:	dc.w 1	; Frame_361C4A
 		dc.w $000B
 		dc.w $00C2
-Frame_361C50:	dc.w 0
+DPLC_BubblesBadnik_Frame1:	dc.w 0	; Frame_361C50
 		dc.w $00FB
-Frame_361C54:	dc.w 0
+DPLC_BubblesBadnik_Frame2:	dc.w 0	; Frame_361C54
 		dc.w $01BB
-Frame_361C58:	dc.w 1
+DPLC_BubblesBadnik_Frame3:	dc.w 1	; Frame_361C58
 		dc.w $027F
 		dc.w $0371
-Frame_361C5E:	dc.w 3
+DPLC_BubblesBadnik_Frame4:	dc.w 3	; Frame_361C5E
 		dc.w $0391
 		dc.w $03BF
 		dc.w $04B3
--- General/Sprites/Bubbles Badnik/Map - Bubbles Badnik.asm
+++ General/Sprites/Bubbles Badnik/Map - Bubbles Badnik.asm
@@ -1,20 +1,20 @@
 Map_BubblesBadnik_:
-		dc.w Frame_361C72-Map_BubblesBadnik_
-		dc.w Frame_361C80-Map_BubblesBadnik_
-		dc.w Frame_361C88-Map_BubblesBadnik_
-		dc.w Frame_361C90-Map_BubblesBadnik_
-		dc.w Frame_361C9E-Map_BubblesBadnik_
-Frame_361C72:	dc.w 2
+		dc.w Map_BubblesBadnik_Frame0-Map_BubblesBadnik_
+		dc.w Map_BubblesBadnik_Frame1-Map_BubblesBadnik_
+		dc.w Map_BubblesBadnik_Frame2-Map_BubblesBadnik_
+		dc.w Map_BubblesBadnik_Frame3-Map_BubblesBadnik_
+		dc.w Map_BubblesBadnik_Frame4-Map_BubblesBadnik_
+Map_BubblesBadnik_Frame0:	dc.w 2	; Frame_361C72
 		dc.b  $F4,  $E,   0,   0, $FF, $EC
 		dc.b  $F4,   2,   0,  $C,   0,  $C
-Frame_361C80:	dc.w 1
+Map_BubblesBadnik_Frame1:	dc.w 1	; Frame_361C80
 		dc.b  $F4,  $E,   0,   0, $FF, $F0
-Frame_361C88:	dc.w 1
+Map_BubblesBadnik_Frame2:	dc.w 1	; Frame_361C88
 		dc.b  $F4,  $E,   0,   0, $FF, $F0
-Frame_361C90:	dc.w 2
+Map_BubblesBadnik_Frame3:	dc.w 2	; Frame_361C90
 		dc.b  $F0,  $F,   0,   0, $FF, $EC
 		dc.b  $F8,   1,   0, $10,   0,  $C
-Frame_361C9E:	dc.w 4
+Map_BubblesBadnik_Frame4:	dc.w 4	; Frame_361C9E
 		dc.b  $E8,   4,   0,   0, $FF, $FC
 		dc.b  $F0,  $F,   0,   2, $FF, $EC
 		dc.b  $F0,   3,   0, $12,   0,  $C
