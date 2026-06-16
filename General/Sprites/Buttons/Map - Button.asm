25044306c9009658b689f77f7ebaa5c9022f50e6
Farrell <farrell@bluetoaster.net>
2026-06-16 20:49:36 +0100

    docs: label button map data

--- General/Sprites/Buttons/Map - Button.asm
+++ General/Sprites/Buttons/Map - Button.asm
@@ -1,12 +1,12 @@
 Map_Button_:
-		dc.w word_2C724-Map_Button_
-		dc.w word_2C732-Map_Button_
-		dc.w word_2C73A-Map_Button_
-word_2C724:	dc.w 2			; DATA XREF: ROM:0002C71Eo
+		dc.w Map_Button_Frame0-Map_Button_
+		dc.w Map_Button_Frame1-Map_Button_
+		dc.w Map_Button_Frame2-Map_Button_
+Map_Button_Frame0:	dc.w 2			; word_2C724
 		dc.b  $F4,  $C,	  0,   0, $FF, $F0
 		dc.b  $FC,   4,	  0,   4, $FF, $F8
-word_2C732:	dc.w 1			; DATA XREF: ROM:0002C71Eo
+Map_Button_Frame1:	dc.w 1			; word_2C732
 		dc.b  $FC,  $C,	  0,   0, $FF, $F0
-word_2C73A:	dc.w 2			; DATA XREF: ROM:0002C71Eo
+Map_Button_Frame2:	dc.w 2			; word_2C73A
 		dc.b  $F8,  $C,	  0,   0, $FF, $F0
 		dc.b	0,   4,	  0,   4, $FF, $F8
