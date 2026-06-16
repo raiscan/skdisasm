7818a513adb6812bf4868c7c82626dca868a12d4
Farrell <farrell@bluetoaster.net>
2026-06-16 05:00:02 +0100

    docs: label HCZ conveyor spike mapping

--- Levels/HCZ/Misc Object Data/Map - Conveyor Spike.asm
+++ Levels/HCZ/Misc Object Data/Map - Conveyor Spike.asm
@@ -1,4 +1,4 @@
 Map_HCZConveyorSpike_:
-		dc.w Frame_230360-Map_HCZConveyorSpike_
-Frame_230360:	dc.w 1
+		dc.w Map_HCZConveyorSpike_Frame0-Map_HCZConveyorSpike_
+Map_HCZConveyorSpike_Frame0:	dc.w 1
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
