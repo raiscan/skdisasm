5640d187e09989bb3779f4d0db1e21b4f110e3d8
Farrell <farrell@bluetoaster.net>
2026-06-16 07:43:43 +0100

    docs: label HCZ end boss mappings

--- Levels/HCZ/Misc Object Data/Map - End Boss.asm
+++ Levels/HCZ/Misc Object Data/Map - End Boss.asm
@@ -1,196 +1,196 @@
 Map_HCZEndBoss_:
-		dc.w Frame_363538-Map_HCZEndBoss_
-		dc.w Frame_363552-Map_HCZEndBoss_
-		dc.w Frame_36355A-Map_HCZEndBoss_
-		dc.w Frame_36356E-Map_HCZEndBoss_
-		dc.w Frame_363582-Map_HCZEndBoss_
-		dc.w Frame_363590-Map_HCZEndBoss_
-		dc.w Frame_3635A4-Map_HCZEndBoss_
-		dc.w Frame_3635AC-Map_HCZEndBoss_
-		dc.w Frame_3635B4-Map_HCZEndBoss_
-		dc.w Frame_3635BC-Map_HCZEndBoss_
-		dc.w Frame_3635C4-Map_HCZEndBoss_
-		dc.w Frame_3635CC-Map_HCZEndBoss_
-		dc.w Frame_3635D4-Map_HCZEndBoss_
-		dc.w Frame_3635DC-Map_HCZEndBoss_
-		dc.w Frame_3635E4-Map_HCZEndBoss_
-		dc.w Frame_3635EC-Map_HCZEndBoss_
-		dc.w Frame_3635F4-Map_HCZEndBoss_
-		dc.w Frame_3635FC-Map_HCZEndBoss_
-		dc.w Frame_363604-Map_HCZEndBoss_
-		dc.w Frame_36360C-Map_HCZEndBoss_
-		dc.w Frame_363614-Map_HCZEndBoss_
-		dc.w Frame_36361C-Map_HCZEndBoss_
-		dc.w Frame_36362A-Map_HCZEndBoss_
-		dc.w Frame_363638-Map_HCZEndBoss_
-		dc.w Frame_363646-Map_HCZEndBoss_
-		dc.w Frame_36364E-Map_HCZEndBoss_
-		dc.w Frame_363656-Map_HCZEndBoss_
-		dc.w Frame_36365E-Map_HCZEndBoss_
-		dc.w Frame_363666-Map_HCZEndBoss_
-		dc.w Frame_36366E-Map_HCZEndBoss_
-		dc.w Frame_363676-Map_HCZEndBoss_
-		dc.w Frame_36367E-Map_HCZEndBoss_
-		dc.w Frame_363686-Map_HCZEndBoss_
-		dc.w Frame_363694-Map_HCZEndBoss_
-		dc.w Frame_3636A2-Map_HCZEndBoss_
-		dc.w Frame_3636B0-Map_HCZEndBoss_
-		dc.w Frame_3636B8-Map_HCZEndBoss_
-		dc.w Frame_3636C6-Map_HCZEndBoss_
-		dc.w Frame_3636D4-Map_HCZEndBoss_
-		dc.w Frame_3636E2-Map_HCZEndBoss_
-		dc.w Frame_3636F6-Map_HCZEndBoss_
-		dc.w Frame_36370A-Map_HCZEndBoss_
-		dc.w Frame_36371E-Map_HCZEndBoss_
-		dc.w Frame_363738-Map_HCZEndBoss_
-		dc.w Frame_363752-Map_HCZEndBoss_
-		dc.w Frame_36376C-Map_HCZEndBoss_
-		dc.w Frame_36378C-Map_HCZEndBoss_
-		dc.w Frame_3637AC-Map_HCZEndBoss_
-		dc.w Frame_3637CC-Map_HCZEndBoss_
-		dc.w Frame_3637D4-Map_HCZEndBoss_
-Frame_363538:	dc.w 4
+		dc.w Map_HCZEndBoss_Frame0-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame1-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame2-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame3-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame4-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame5-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame6-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame7-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame8-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame9-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame10-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame11-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame12-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame13-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame14-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame15-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame16-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame17-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame18-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame19-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame20-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame21-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame22-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame23-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame24-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame25-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame26-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame27-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame28-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame29-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame30-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame31-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame32-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame33-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame34-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame35-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame36-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame37-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame38-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame39-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame40-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame41-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame42-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame43-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame44-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame45-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame46-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame47-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame48-Map_HCZEndBoss_
+		dc.w Map_HCZEndBoss_Frame49-Map_HCZEndBoss_
+Map_HCZEndBoss_Frame0:	dc.w 4
 		dc.b  $FD,   5,   0,   0, $FF, $F4
 		dc.b  $FD,  $D,   0,   4,   0,   4
 		dc.b   $D,   4,   0,  $C,   0,   4
 		dc.b   $D,   9,   0,  $E,   0, $14
-Frame_363552:	dc.w 1
+Map_HCZEndBoss_Frame1:	dc.w 1
 		dc.b  $FC,   8,   0, $14, $FF, $F4
-Frame_36355A:	dc.w 3
+Map_HCZEndBoss_Frame2:	dc.w 3
 		dc.b  $FC,   8,   0, $17, $FF, $E4
 		dc.b  $FC,  $C,   0, $1A, $FF, $FC
 		dc.b    4,   4,   0, $2A, $FF, $F8
-Frame_36356E:	dc.w 3
+Map_HCZEndBoss_Frame3:	dc.w 3
 		dc.b  $FC,   4,   0, $1E, $FF, $EC
 		dc.b  $FC,   8,   0, $20, $FF, $FC
 		dc.b    4,   4,   8, $2A, $FF, $F8
-Frame_363582:	dc.w 2
+Map_HCZEndBoss_Frame4:	dc.w 2
 		dc.b  $FC,   4,   0, $23, $FF, $F8
 		dc.b    4,   4,   0, $2A, $FF, $F8
-Frame_363590:	dc.w 3
+Map_HCZEndBoss_Frame5:	dc.w 3
 		dc.b  $FC,   4,   0, $25, $FF, $EC
 		dc.b  $FC,   8,   0, $27, $FF, $FC
 		dc.b    4,   4,   8, $2A, $FF, $F8
-Frame_3635A4:	dc.w 1
+Map_HCZEndBoss_Frame6:	dc.w 1
 		dc.b  $F8,   5,   0, $2C, $FF, $F8
-Frame_3635AC:	dc.w 1
+Map_HCZEndBoss_Frame7:	dc.w 1
 		dc.b  $F8,   5,   0, $30, $FF, $F8
-Frame_3635B4:	dc.w 1
+Map_HCZEndBoss_Frame8:	dc.w 1
 		dc.b  $F8,   9,   0, $34, $FF, $F4
-Frame_3635BC:	dc.w 1
+Map_HCZEndBoss_Frame9:	dc.w 1
 		dc.b  $F0,  $A,   0, $3A, $FF, $F4
-Frame_3635C4:	dc.w 1
+Map_HCZEndBoss_Frame10:	dc.w 1
 		dc.b  $E8,  $B,   0, $43, $FF, $F4
-Frame_3635CC:	dc.w 1
+Map_HCZEndBoss_Frame11:	dc.w 1
 		dc.b  $E8,  $B,   0, $4F, $FF, $F4
-Frame_3635D4:	dc.w 1
+Map_HCZEndBoss_Frame12:	dc.w 1
 		dc.b  $E8,  $B,   0, $5B, $FF, $F4
-Frame_3635DC:	dc.w 1
+Map_HCZEndBoss_Frame13:	dc.w 1
 		dc.b  $F8,  $D,   0, $67, $FF, $F0
-Frame_3635E4:	dc.w 1
+Map_HCZEndBoss_Frame14:	dc.w 1
 		dc.b  $F8,  $D,   0, $6F, $FF, $F0
-Frame_3635EC:	dc.w 1
+Map_HCZEndBoss_Frame15:	dc.w 1
 		dc.b  $F8,  $D,   0, $77, $FF, $F0
-Frame_3635F4:	dc.w 1
+Map_HCZEndBoss_Frame16:	dc.w 1
 		dc.b  $F8,  $D,   0, $7F, $FF, $F0
-Frame_3635FC:	dc.w 1
+Map_HCZEndBoss_Frame17:	dc.w 1
 		dc.b  $F8,  $D,   0, $87, $FF, $F0
-Frame_363604:	dc.w 1
+Map_HCZEndBoss_Frame18:	dc.w 1
 		dc.b  $F8,  $D,   0, $8F, $FF, $F0
-Frame_36360C:	dc.w 1
+Map_HCZEndBoss_Frame19:	dc.w 1
 		dc.b  $F0,  $F,   0, $97, $FF, $F0
-Frame_363614:	dc.w 1
+Map_HCZEndBoss_Frame20:	dc.w 1
 		dc.b  $F0,  $F,   0, $A7, $FF, $F0
-Frame_36361C:	dc.w 2
+Map_HCZEndBoss_Frame21:	dc.w 2
 		dc.b  $F4,  $E,   0, $B7, $FF, $EC
 		dc.b  $F4,   2,   0, $C3,   0,  $C
-Frame_36362A:	dc.w 2
+Map_HCZEndBoss_Frame22:	dc.w 2
 		dc.b  $F4,  $E,   0, $C6, $FF, $EC
 		dc.b  $F4,   2,   0, $D2,   0,  $C
-Frame_363638:	dc.w 2
+Map_HCZEndBoss_Frame23:	dc.w 2
 		dc.b  $F4,  $E,   0, $D5, $FF, $EC
 		dc.b  $F4,   2,   0, $E1,   0,  $C
-Frame_363646:	dc.w 1
+Map_HCZEndBoss_Frame24:	dc.w 1
 		dc.b  $FC,   8,   0, $E4, $FF, $F4
-Frame_36364E:	dc.w 1
+Map_HCZEndBoss_Frame25:	dc.w 1
 		dc.b  $F4,   9,   0, $E7, $FF, $F4
-Frame_363656:	dc.w 1
+Map_HCZEndBoss_Frame26:	dc.w 1
 		dc.b  $FC,  $C,   0, $ED, $FF, $F0
-Frame_36365E:	dc.w 1
+Map_HCZEndBoss_Frame27:	dc.w 1
 		dc.b  $F8,   9,   8, $34, $FF, $F4
-Frame_363666:	dc.w 1
+Map_HCZEndBoss_Frame28:	dc.w 1
 		dc.b  $F0,  $A,   8, $3A, $FF, $F4
-Frame_36366E:	dc.w 1
+Map_HCZEndBoss_Frame29:	dc.w 1
 		dc.b  $E8,  $B,   8, $43, $FF, $F4
-Frame_363676:	dc.w 1
+Map_HCZEndBoss_Frame30:	dc.w 1
 		dc.b  $E8,  $B,   8, $4F, $FF, $F4
-Frame_36367E:	dc.w 1
+Map_HCZEndBoss_Frame31:	dc.w 1
 		dc.b  $E8,  $B,   8, $5B, $FF, $F4
-Frame_363686:	dc.w 2
+Map_HCZEndBoss_Frame32:	dc.w 2
 		dc.b  $F4,  $E,   8, $B7, $FF, $F4
 		dc.b  $F4,   2,   8, $C3, $FF, $EC
-Frame_363694:	dc.w 2
+Map_HCZEndBoss_Frame33:	dc.w 2
 		dc.b  $F4,  $E,   8, $C6, $FF, $F4
 		dc.b  $F4,   2,   8, $D2, $FF, $EC
-Frame_3636A2:	dc.w 2
+Map_HCZEndBoss_Frame34:	dc.w 2
 		dc.b  $F4,  $E,   8, $D5, $FF, $F4
 		dc.b  $F4,   2,   8, $E1, $FF, $EC
-Frame_3636B0:	dc.w 1
+Map_HCZEndBoss_Frame35:	dc.w 1
 		dc.b  $FC,  $C,   8, $ED, $FF, $F0
-Frame_3636B8:	dc.w 2
+Map_HCZEndBoss_Frame36:	dc.w 2
 		dc.b  $F0,  $D,   0, $6F, $FF, $F0
 		dc.b    0,  $D,   0, $67, $FF, $F0
-Frame_3636C6:	dc.w 2
+Map_HCZEndBoss_Frame37:	dc.w 2
 		dc.b  $F0,  $D,   0, $7F, $FF, $F0
 		dc.b    0,  $D,   0, $77, $FF, $F0
-Frame_3636D4:	dc.w 2
+Map_HCZEndBoss_Frame38:	dc.w 2
 		dc.b  $F0,  $D,   0, $8F, $FF, $F0
 		dc.b    0,  $D,   0, $87, $FF, $F0
-Frame_3636E2:	dc.w 3
+Map_HCZEndBoss_Frame39:	dc.w 3
 		dc.b  $E8,  $D,   0, $67, $FF, $F0
 		dc.b  $F8,  $D,   0, $6F, $FF, $F0
 		dc.b    8,  $D,   0, $67, $FF, $F0
-Frame_3636F6:	dc.w 3
+Map_HCZEndBoss_Frame40:	dc.w 3
 		dc.b  $E8,  $D,   0, $77, $FF, $F0
 		dc.b  $F8,  $D,   0, $7F, $FF, $F0
 		dc.b    8,  $D,   0, $77, $FF, $F0
-Frame_36370A:	dc.w 3
+Map_HCZEndBoss_Frame41:	dc.w 3
 		dc.b  $E8,  $D,   0, $87, $FF, $F0
 		dc.b  $F8,  $D,   0, $8F, $FF, $F0
 		dc.b    8,  $D,   0, $87, $FF, $F0
-Frame_36371E:	dc.w 4
+Map_HCZEndBoss_Frame42:	dc.w 4
 		dc.b  $E0,  $D,   0, $6F, $FF, $F0
 		dc.b  $F0,  $D,   0, $67, $FF, $F0
 		dc.b    0,  $D,   0, $6F, $FF, $F0
 		dc.b  $10,  $D,   0, $67, $FF, $F0
-Frame_363738:	dc.w 4
+Map_HCZEndBoss_Frame43:	dc.w 4
 		dc.b  $E0,  $D,   0, $7F, $FF, $F0
 		dc.b  $F0,  $D,   0, $77, $FF, $F0
 		dc.b    0,  $D,   0, $7F, $FF, $F0
 		dc.b  $10,  $D,   0, $77, $FF, $F0
-Frame_363752:	dc.w 4
+Map_HCZEndBoss_Frame44:	dc.w 4
 		dc.b  $E0,  $D,   0, $8F, $FF, $F0
 		dc.b  $F0,  $D,   0, $87, $FF, $F0
 		dc.b    0,  $D,   0, $8F, $FF, $F0
 		dc.b  $10,  $D,   0, $87, $FF, $F0
-Frame_36376C:	dc.w 5
+Map_HCZEndBoss_Frame45:	dc.w 5
 		dc.b  $D8,  $D,   0, $67, $FF, $F0
 		dc.b  $E8,  $D,   0, $6F, $FF, $F0
 		dc.b  $F8,  $D,   0, $67, $FF, $F0
 		dc.b    8,  $D,   0, $6F, $FF, $F0
 		dc.b  $18,  $D,   0, $67, $FF, $F0
-Frame_36378C:	dc.w 5
+Map_HCZEndBoss_Frame46:	dc.w 5
 		dc.b  $D8,  $D,   0, $77, $FF, $F0
 		dc.b  $E8,  $D,   0, $7F, $FF, $F0
 		dc.b  $F8,  $D,   0, $77, $FF, $F0
 		dc.b    8,  $D,   0, $7F, $FF, $F0
 		dc.b  $18,  $D,   0, $77, $FF, $F0
-Frame_3637AC:	dc.w 5
+Map_HCZEndBoss_Frame47:	dc.w 5
 		dc.b  $D8,  $D,   0, $87, $FF, $F0
 		dc.b  $E8,  $D,   0, $8F, $FF, $F0
 		dc.b  $F8,  $D,   0, $87, $FF, $F0
 		dc.b    8,  $D,   0, $8F, $FF, $F0
 		dc.b  $18,  $D,   0, $87, $FF, $F0
-Frame_3637CC:	dc.w 1
+Map_HCZEndBoss_Frame48:	dc.w 1
 		dc.b  $F4,   9,   8, $34, $FF, $F4
-Frame_3637D4:	dc.w 0
+Map_HCZEndBoss_Frame49:	dc.w 0
