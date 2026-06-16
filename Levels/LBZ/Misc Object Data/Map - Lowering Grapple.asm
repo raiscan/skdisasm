2efb2117afd3161821e0a281374afb53b7800db7
Farrell <farrell@bluetoaster.net>
2026-06-16 14:29:25 +0100

    docs: label LBZ lowering grapple map frames

--- Levels/LBZ/Misc Object Data/Map - Lowering Grapple.asm
+++ Levels/LBZ/Misc Object Data/Map - Lowering Grapple.asm
@@ -1,42 +1,42 @@
 Map_LBZLoweringGrapple_:
-		dc.w Frame_227DDA-Map_LBZLoweringGrapple_
-		dc.w Frame_227DEE-Map_LBZLoweringGrapple_
-		dc.w Frame_227E08-Map_LBZLoweringGrapple_
-		dc.w Frame_227E28-Map_LBZLoweringGrapple_
-		dc.w Frame_227E4E-Map_LBZLoweringGrapple_
-		dc.w Frame_227E7A-Map_LBZLoweringGrapple_
-		dc.w Frame_227EAC-Map_LBZLoweringGrapple_
-		dc.w Frame_227EE4-Map_LBZLoweringGrapple_
-		dc.w Frame_227F22-Map_LBZLoweringGrapple_
-		dc.w Frame_227F66-Map_LBZLoweringGrapple_
-		dc.w Frame_227FB0-Map_LBZLoweringGrapple_
-		dc.w Frame_228000-Map_LBZLoweringGrapple_
-		dc.w Frame_228056-Map_LBZLoweringGrapple_
-		dc.w Frame_2280B2-Map_LBZLoweringGrapple_
-		dc.w Frame_2280B2-Map_LBZLoweringGrapple_
-Frame_227DDA:	dc.w 3
+		dc.w Map_LBZLoweringGrapple_Frame0-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame1-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame2-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame3-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame4-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame5-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame6-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame7-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame8-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame9-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame10-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame11-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame12-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame13-Map_LBZLoweringGrapple_
+		dc.w Map_LBZLoweringGrapple_Frame13-Map_LBZLoweringGrapple_
+Map_LBZLoweringGrapple_Frame0:	dc.w 3	; Frame_227DDA
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227DEE:	dc.w 4
+Map_LBZLoweringGrapple_Frame1:	dc.w 4	; Frame_227DEE
 		dc.b  $40,   5,   0, $3A, $FF, $F8
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227E08:	dc.w 5
+Map_LBZLoweringGrapple_Frame2:	dc.w 5	; Frame_227E08
 		dc.b  $30,   5,   0, $3A, $FF, $F8
 		dc.b  $40,   5,   0, $3A, $FF, $F8
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227E28:	dc.w 6
+Map_LBZLoweringGrapple_Frame3:	dc.w 6	; Frame_227E28
 		dc.b  $20,   5,   0, $3A, $FF, $F8
 		dc.b  $30,   5,   0, $3A, $FF, $F8
 		dc.b  $40,   5,   0, $3A, $FF, $F8
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227E4E:	dc.w 7
+Map_LBZLoweringGrapple_Frame4:	dc.w 7	; Frame_227E4E
 		dc.b  $10,   5,   0, $3A, $FF, $F8
 		dc.b  $20,   5,   0, $3A, $FF, $F8
 		dc.b  $30,   5,   0, $3A, $FF, $F8
@@ -44,7 +44,7 @@ Frame_227E4E:	dc.w 7
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227E7A:	dc.w 8
+Map_LBZLoweringGrapple_Frame5:	dc.w 8	; Frame_227E7A
 		dc.b    0,   5,   0, $3A, $FF, $F8
 		dc.b  $10,   5,   0, $3A, $FF, $F8
 		dc.b  $20,   5,   0, $3A, $FF, $F8
@@ -53,7 +53,7 @@ Frame_227E7A:	dc.w 8
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227EAC:	dc.w 9
+Map_LBZLoweringGrapple_Frame6:	dc.w 9	; Frame_227EAC
 		dc.b  $F0,   5,   0, $3A, $FF, $F8
 		dc.b    0,   5,   0, $3A, $FF, $F8
 		dc.b  $10,   5,   0, $3A, $FF, $F8
@@ -63,7 +63,7 @@ Frame_227EAC:	dc.w 9
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227EE4:	dc.w $A
+Map_LBZLoweringGrapple_Frame7:	dc.w $A	; Frame_227EE4
 		dc.b  $E0,   5,   0, $3A, $FF, $F8
 		dc.b  $F0,   5,   0, $3A, $FF, $F8
 		dc.b    0,   5,   0, $3A, $FF, $F8
@@ -74,7 +74,7 @@ Frame_227EE4:	dc.w $A
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227F22:	dc.w $B
+Map_LBZLoweringGrapple_Frame8:	dc.w $B	; Frame_227F22
 		dc.b  $D0,   5,   0, $3A, $FF, $F8
 		dc.b  $E0,   5,   0, $3A, $FF, $F8
 		dc.b  $F0,   5,   0, $3A, $FF, $F8
@@ -86,7 +86,7 @@ Frame_227F22:	dc.w $B
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227F66:	dc.w $C
+Map_LBZLoweringGrapple_Frame9:	dc.w $C	; Frame_227F66
 		dc.b  $C0,   5,   0, $3A, $FF, $F8
 		dc.b  $D0,   5,   0, $3A, $FF, $F8
 		dc.b  $E0,   5,   0, $3A, $FF, $F8
@@ -99,7 +99,7 @@ Frame_227F66:	dc.w $C
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_227FB0:	dc.w $D
+Map_LBZLoweringGrapple_Frame10:	dc.w $D	; Frame_227FB0
 		dc.b  $B0,   5,   0, $3A, $FF, $F8
 		dc.b  $C0,   5,   0, $3A, $FF, $F8
 		dc.b  $D0,   5,   0, $3A, $FF, $F8
@@ -113,7 +113,7 @@ Frame_227FB0:	dc.w $D
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_228000:	dc.w $E
+Map_LBZLoweringGrapple_Frame11:	dc.w $E	; Frame_228000
 		dc.b  $A0,   5,   0, $3A, $FF, $F8
 		dc.b  $B0,   5,   0, $3A, $FF, $F8
 		dc.b  $C0,   5,   0, $3A, $FF, $F8
@@ -128,7 +128,7 @@ Frame_228000:	dc.w $E
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_228056:	dc.w $F
+Map_LBZLoweringGrapple_Frame12:	dc.w $F	; Frame_228056
 		dc.b  $90,   5,   0, $3A, $FF, $F8
 		dc.b  $A0,   5,   0, $3A, $FF, $F8
 		dc.b  $B0,   5,   0, $3A, $FF, $F8
@@ -144,7 +144,7 @@ Frame_228056:	dc.w $F
 		dc.b  $50,   5,   0, $3A, $FF, $F8
 		dc.b  $60,   5, $E1, $49, $FF, $F8
 		dc.b  $70,   5, $E1, $4E, $FF, $F8
-Frame_2280B2:	dc.w $10
+Map_LBZLoweringGrapple_Frame13:	dc.w $10	; Frame_2280B2
 		dc.b  $80,   5,   0, $3A, $FF, $F8
 		dc.b  $90,   5,   0, $3A, $FF, $F8
 		dc.b  $A0,   5,   0, $3A, $FF, $F8
