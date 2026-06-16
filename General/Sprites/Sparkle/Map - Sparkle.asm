37bb5539a541843b2b1d80a65dcf725a3437b688
Farrell <farrell@bluetoaster.net>
2026-06-16 19:14:19 +0100

    docs: label Sparkle map frames

--- General/Sprites/Sparkle/Map - Sparkle.asm
+++ General/Sprites/Sparkle/Map - Sparkle.asm
@@ -1,39 +1,39 @@
 Map_Sparkle_:
-		dc.w Frame_361B46-Map_Sparkle_
-		dc.w Frame_361B4E-Map_Sparkle_
-		dc.w Frame_361B56-Map_Sparkle_
-		dc.w Frame_361B70-Map_Sparkle_
-		dc.w Frame_361B8A-Map_Sparkle_
-		dc.w Frame_361BA4-Map_Sparkle_
-		dc.w Frame_361BBE-Map_Sparkle_
-		dc.w Frame_361BC6-Map_Sparkle_
-		dc.w Frame_361BCE-Map_Sparkle_
-Frame_361B46:	dc.w 1
+		dc.w Map_Sparkle_Frame0-Map_Sparkle_
+		dc.w Map_Sparkle_Frame1-Map_Sparkle_
+		dc.w Map_Sparkle_Frame2-Map_Sparkle_
+		dc.w Map_Sparkle_Frame3-Map_Sparkle_
+		dc.w Map_Sparkle_Frame4-Map_Sparkle_
+		dc.w Map_Sparkle_Frame5-Map_Sparkle_
+		dc.w Map_Sparkle_Frame6-Map_Sparkle_
+		dc.w Map_Sparkle_Frame7-Map_Sparkle_
+		dc.w Map_Sparkle_Frame8-Map_Sparkle_
+Map_Sparkle_Frame0:	dc.w 1	; Frame_361B46
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-Frame_361B4E:	dc.w 1
+Map_Sparkle_Frame1:	dc.w 1	; Frame_361B4E
 		dc.b  $F4,  $A,   0,   9, $FF, $F4
-Frame_361B56:	dc.w 4
+Map_Sparkle_Frame2:	dc.w 4	; Frame_361B56
 		dc.b  $C0,   7,   0, $1E, $FF, $F8
 		dc.b  $E0,   7,   0, $1E, $FF, $F8
 		dc.b    0,   7,   0, $1E, $FF, $F8
 		dc.b  $20,   7,   0, $1E, $FF, $F8
-Frame_361B70:	dc.w 4
+Map_Sparkle_Frame3:	dc.w 4	; Frame_361B70
 		dc.b  $C0,  $B,   0, $12, $FF, $F4
 		dc.b  $E0,  $B,   0, $12, $FF, $F4
 		dc.b    0,  $B,   0, $12, $FF, $F4
 		dc.b  $20,  $B,   0, $12, $FF, $F4
-Frame_361B8A:	dc.w 4
+Map_Sparkle_Frame4:	dc.w 4	; Frame_361B8A
 		dc.b  $C0,  $B,   8, $12, $FF, $F4
 		dc.b  $E0,  $B,   8, $12, $FF, $F4
 		dc.b    0,  $B,   8, $12, $FF, $F4
 		dc.b  $20,  $B,   8, $12, $FF, $F4
-Frame_361BA4:	dc.w 4
+Map_Sparkle_Frame5:	dc.w 4	; Frame_361BA4
 		dc.b  $C0,  $B, $10, $12, $FF, $F4
 		dc.b  $E0,  $B, $10, $12, $FF, $F4
 		dc.b    0,  $B, $10, $12, $FF, $F4
 		dc.b  $20,  $B, $10, $12, $FF, $F4
-Frame_361BBE:	dc.w 1
+Map_Sparkle_Frame6:	dc.w 1	; Frame_361BBE
 		dc.b  $F8,   5,   0, $26, $FF, $F8
-Frame_361BC6:	dc.w 1
+Map_Sparkle_Frame7:	dc.w 1	; Frame_361BC6
 		dc.b  $F8,   5,   0, $2A, $FF, $F8
-Frame_361BCE:	dc.w 0
+Map_Sparkle_Frame8:	dc.w 0	; Frame_361BCE
