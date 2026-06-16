379c94f65ea74f7283cd3255fcc979e79d760a56
Farrell <farrell@bluetoaster.net>
2026-06-16 03:57:11 +0100

    docs: label AIZ log mappings

--- Levels/AIZ/Misc Object Data/Map - Spiked Log.asm
+++ Levels/AIZ/Misc Object Data/Map - Spiked Log.asm
@@ -1,102 +1,102 @@
 Map_AIZSpikedLog_:
-		dc.w Frame_22B110-Map_AIZSpikedLog_
-		dc.w Frame_22B130-Map_AIZSpikedLog_
-		dc.w Frame_22B150-Map_AIZSpikedLog_
-		dc.w Frame_22B170-Map_AIZSpikedLog_
-		dc.w Frame_22B190-Map_AIZSpikedLog_
-		dc.w Frame_22B1B0-Map_AIZSpikedLog_
-		dc.w Frame_22B1D0-Map_AIZSpikedLog_
-		dc.w Frame_22B1F0-Map_AIZSpikedLog_
-		dc.w Frame_22B210-Map_AIZSpikedLog_
-		dc.w Frame_22B230-Map_AIZSpikedLog_
-		dc.w Frame_22B250-Map_AIZSpikedLog_
-		dc.w Frame_22B26A-Map_AIZSpikedLog_
-		dc.w Frame_22B27E-Map_AIZSpikedLog_
-		dc.w Frame_22B28C-Map_AIZSpikedLog_
-		dc.w Frame_22B2A0-Map_AIZSpikedLog_
-		dc.w Frame_22B2BA-Map_AIZSpikedLog_
-Frame_22B110:	dc.w 5
+		dc.w Map_AIZSpikedLog_Frame0-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame1-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame2-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame3-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame4-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame5-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame6-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame7-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame8-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame9-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame10-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame11-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame12-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame13-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame14-Map_AIZSpikedLog_
+		dc.w Map_AIZSpikedLog_Frame15-Map_AIZSpikedLog_
+Map_AIZSpikedLog_Frame0:	dc.w 5
 		dc.b  $F0,   0,   0,  $B, $FF, $EC
 		dc.b  $F0,   1,   0,  $C, $FF, $FC
 		dc.b  $F3,   1,   0,  $E,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B130:	dc.w 5
+Map_AIZSpikedLog_Frame1:	dc.w 5
 		dc.b  $F0,   1,   0,  $C, $FF, $EC
 		dc.b  $F3,   1,   0,  $E, $FF, $FC
 		dc.b  $F6,   1,   0, $10,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B150:	dc.w 5
+Map_AIZSpikedLog_Frame2:	dc.w 5
 		dc.b  $F3,   1,   0,  $E, $FF, $EC
 		dc.b  $F6,   1,   0, $10, $FF, $FC
 		dc.b  $F9,   1,   0, $12,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B170:	dc.w 5
+Map_AIZSpikedLog_Frame3:	dc.w 5
 		dc.b  $F6,   1,   0, $10, $FF, $EC
 		dc.b  $F9,   1,   0, $12, $FF, $FC
 		dc.b  $FC,   0,   0, $14,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B190:	dc.w 5
+Map_AIZSpikedLog_Frame4:	dc.w 5
 		dc.b  $F9,   1,   0, $12, $FF, $EC
 		dc.b  $FC,   0,   0, $14, $FF, $FC
 		dc.b  $F7,   1, $10, $12,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B1B0:	dc.w 5
+Map_AIZSpikedLog_Frame5:	dc.w 5
 		dc.b  $FC,   0,   0, $14, $FF, $EC
 		dc.b  $F7,   1, $10, $12, $FF, $FC
 		dc.b  $FA,   1, $10, $10,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B1D0:	dc.w 5
+Map_AIZSpikedLog_Frame6:	dc.w 5
 		dc.b  $F7,   1, $10, $12, $FF, $EC
 		dc.b  $FA,   1, $10, $10, $FF, $FC
 		dc.b  $FD,   1, $10,  $E,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B1F0:	dc.w 5
+Map_AIZSpikedLog_Frame7:	dc.w 5
 		dc.b  $FA,   1, $10, $10, $FF, $EC
 		dc.b  $FD,   1, $10,  $E, $FF, $FC
 		dc.b    0,   1, $10,  $C,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B210:	dc.w 5
+Map_AIZSpikedLog_Frame8:	dc.w 5
 		dc.b  $FD,   1, $10,  $E, $FF, $EC
 		dc.b    0,   1, $10,  $C, $FF, $FC
 		dc.b    8,   0, $10,  $B,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B230:	dc.w 5
+Map_AIZSpikedLog_Frame9:	dc.w 5
 		dc.b    0,   1, $10,  $C, $FF, $EC
 		dc.b    8,   0, $10,  $B, $FF, $FC
 		dc.b    8,   0, $10,  $A,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B250:	dc.w 4
+Map_AIZSpikedLog_Frame10:	dc.w 4
 		dc.b    8,   0, $10,  $B, $FF, $EC
 		dc.b    8,   0, $10,  $A, $FF, $FC
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B26A:	dc.w 3
+Map_AIZSpikedLog_Frame11:	dc.w 3
 		dc.b    8,   0, $10,  $A, $FF, $EC
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B27E:	dc.w 2
+Map_AIZSpikedLog_Frame12:	dc.w 2
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B28C:	dc.w 3
+Map_AIZSpikedLog_Frame13:	dc.w 3
 		dc.b  $F0,   0,   0,  $A,   0,  $C
 		dc.b  $F8,   9,   8, $15,   0,   0
 		dc.b  $F8,   9,   8, $1B, $FF, $E8
-Frame_22B2A0:	dc.w 4
+Map_AIZSpikedLog_Frame14:	dc.w 4
 		dc.b  $F0,   0,   0,  $A, $FF, $FC
 		dc.b  $F0,   0,   0,  $B,   0,  $C
 		dc.b  $F8,   9,   0, $15, $FF, $E8
 		dc.b  $F8,   9,   0, $1B,   0,   0
-Frame_22B2BA:	dc.w 5
+Map_AIZSpikedLog_Frame15:	dc.w 5
 		dc.b  $F0,   0,   0,  $A, $FF, $EC
 		dc.b  $F0,   0,   0,  $B, $FF, $FC
 		dc.b  $F0,   1,   0,  $C,   0,  $C
