9dce60e0aca40ac08d398018c3a4ec19a0d1a4a0
Farrell <farrell@bluetoaster.net>
2026-06-16 09:07:44 +0100

    docs: label CNZ boss mapping frames

--- Levels/CNZ/Misc Object Data/Map - End Boss.asm
+++ Levels/CNZ/Misc Object Data/Map - End Boss.asm
@@ -1,66 +1,66 @@
 Map_CNZEndBoss_:
-		dc.w Frame_3609C4-Map_CNZEndBoss_
-		dc.w Frame_3609EA-Map_CNZEndBoss_
-		dc.w Frame_3609F8-Map_CNZEndBoss_
-		dc.w Frame_360A06-Map_CNZEndBoss_
-		dc.w Frame_360A14-Map_CNZEndBoss_
-		dc.w Frame_360A22-Map_CNZEndBoss_
-		dc.w Frame_360A3C-Map_CNZEndBoss_
-		dc.w Frame_360A62-Map_CNZEndBoss_
-		dc.w Frame_360A88-Map_CNZEndBoss_
-		dc.w Frame_360A96-Map_CNZEndBoss_
-		dc.w Frame_360A98-Map_CNZEndBoss_
-		dc.w Frame_360AA0-Map_CNZEndBoss_
-		dc.w Frame_360AB4-Map_CNZEndBoss_
-Frame_3609C4:	dc.w 6
+		dc.w Map_CNZEndBoss_Frame0-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame1-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame2-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame3-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame4-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame5-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame6-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame7-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame8-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame9-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame10-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame11-Map_CNZEndBoss_
+		dc.w Map_CNZEndBoss_Frame12-Map_CNZEndBoss_
+Map_CNZEndBoss_Frame0:	dc.w 6
 		dc.b  $EC,  $F,   0,   0, $FF, $E0
 		dc.b  $F4,   2,   0, $10, $FF, $D8
 		dc.b   $C,   8,   0, $13, $FF, $E8
 		dc.b  $EC,  $F,   8,   0,   0,   0
 		dc.b  $F4,   2,   8, $10,   0, $20
 		dc.b   $C,   8,   8, $13,   0,   0
-Frame_3609EA:	dc.w 2
+Map_CNZEndBoss_Frame1:	dc.w 2
 		dc.b  $F0,   3,   0, $16, $FF, $F8
 		dc.b  $F0,   3,   8, $16,   0,   0
-Frame_3609F8:	dc.w 2
+Map_CNZEndBoss_Frame2:	dc.w 2
 		dc.b  $F0,   3,   0, $1A, $FF, $F8
 		dc.b  $F0,   3,   8, $1A,   0,   0
-Frame_360A06:	dc.w 2
+Map_CNZEndBoss_Frame3:	dc.w 2
 		dc.b  $F0,   3,   0, $1E, $FF, $F8
 		dc.b  $F0,   3,   8, $1E,   0,   0
-Frame_360A14:	dc.w 2
+Map_CNZEndBoss_Frame4:	dc.w 2
 		dc.b  $F0,   7,   0, $22, $FF, $F0
 		dc.b  $F0,   7,   8, $22,   0,   0
-Frame_360A22:	dc.w 4
+Map_CNZEndBoss_Frame5:	dc.w 4
 		dc.b  $F0,   5,   0, $2A, $FF, $F0
 		dc.b  $F0,   5,   8, $2A,   0,   0
 		dc.b    0,   5, $10, $2A, $FF, $F0
 		dc.b    0,   5, $18, $2A,   0,   0
-Frame_360A3C:	dc.w 6
+Map_CNZEndBoss_Frame6:	dc.w 6
 		dc.b  $C0,  $E,   0, $2E, $FF, $F0
 		dc.b  $D8,   8,   0, $3A, $FF, $F8
 		dc.b  $E0,   7,   0, $3D, $FF, $F8
 		dc.b    0,  $E,   0, $2E, $FF, $F0
 		dc.b  $18,   8,   0, $3A, $FF, $F8
 		dc.b  $20,   7,   0, $3D, $FF, $F8
-Frame_360A62:	dc.w 6
+Map_CNZEndBoss_Frame7:	dc.w 6
 		dc.b  $C0,   7,   0, $3D, $FF, $F8
 		dc.b  $E0,  $E,   0, $2E, $FF, $F0
 		dc.b  $F8,   8,   0, $3A, $FF, $F8
 		dc.b    0,   7,   0, $3D, $FF, $F8
 		dc.b  $20,  $E,   0, $2E, $FF, $F0
 		dc.b  $38,   8,   0, $3A, $FF, $F8
-Frame_360A88:	dc.w 2
+Map_CNZEndBoss_Frame8:	dc.w 2
 		dc.b  $F0,   3,   0, $45, $FF, $F8
 		dc.b  $F0,   3,   8, $45,   0,   0
-Frame_360A96:	dc.w 0
-Frame_360A98:	dc.w 1
+Map_CNZEndBoss_Frame9:	dc.w 0
+Map_CNZEndBoss_Frame10:	dc.w 1
 		dc.b  $F0,   7,   0, $22, $FF, $F0
-Frame_360AA0:	dc.w 3
+Map_CNZEndBoss_Frame11:	dc.w 3
 		dc.b  $EC,  $F,   0,   0, $FF, $F4
 		dc.b  $F4,   2,   0, $10, $FF, $EC
 		dc.b   $C,   8,   0, $13, $FF, $FC
-Frame_360AB4:	dc.w 3
+Map_CNZEndBoss_Frame12:	dc.w 3
 		dc.b  $EC,  $F,   8,   0, $FF, $EC
 		dc.b  $F4,   2,   8, $10,   0,  $C
 		dc.b   $C,   8,   8, $13, $FF, $EC
--- Levels/CNZ/Misc Object Data/Map - Miniboss.asm
+++ Levels/CNZ/Misc Object Data/Map - Miniboss.asm
@@ -1,34 +1,34 @@
 Map_CNZMiniboss_:
-		dc.w Frame_362F00-Map_CNZMiniboss_
-		dc.w Frame_362F26-Map_CNZMiniboss_
-		dc.w Frame_362F88-Map_CNZMiniboss_
-		dc.w Frame_362FEA-Map_CNZMiniboss_
-		dc.w Frame_36304C-Map_CNZMiniboss_
-		dc.w Frame_3630AE-Map_CNZMiniboss_
-		dc.w Frame_363110-Map_CNZMiniboss_
-		dc.w Frame_363172-Map_CNZMiniboss_
-		dc.w Frame_363186-Map_CNZMiniboss_
-		dc.w Frame_36319A-Map_CNZMiniboss_
-		dc.w Frame_3631AE-Map_CNZMiniboss_
-		dc.w Frame_3631BC-Map_CNZMiniboss_
-		dc.w Frame_3631CA-Map_CNZMiniboss_
-		dc.w Frame_3631D2-Map_CNZMiniboss_
-		dc.w Frame_3631DA-Map_CNZMiniboss_
-		dc.w Frame_3631E2-Map_CNZMiniboss_
-		dc.w Frame_3631F0-Map_CNZMiniboss_
-		dc.w Frame_3631FE-Map_CNZMiniboss_
-		dc.w Frame_363200-Map_CNZMiniboss_
-		dc.w Frame_363208-Map_CNZMiniboss_
-		dc.w Frame_363210-Map_CNZMiniboss_
-		dc.w Frame_36321E-Map_CNZMiniboss_
-Frame_362F00:	dc.w 6
+		dc.w Map_CNZMiniboss_Frame0-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame1-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame2-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame3-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame4-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame5-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame6-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame7-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame8-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame9-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame10-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame11-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame12-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame13-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame14-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame15-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame16-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame17-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame18-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame19-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame20-Map_CNZMiniboss_
+		dc.w Map_CNZMiniboss_Frame21-Map_CNZMiniboss_
+Map_CNZMiniboss_Frame0:	dc.w 6
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b  $10,   5,   0, $10, $FF, $F0
 		dc.b  $10,   5,   8, $10,   0,   0
 		dc.b  $1C,   5,   0, $14, $FF, $F0
 		dc.b  $1C,   5,   8, $14,   0,   0
-Frame_362F26:	dc.w $10
+Map_CNZMiniboss_Frame1:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $A,   4,   0, $23, $FF, $F0
@@ -45,7 +45,7 @@ Frame_362F26:	dc.w $10
 		dc.b  $14,   4,   8, $23,   0,   0
 		dc.b  $1C,   5,   0, $14, $FF, $F0
 		dc.b  $1C,   5,   8, $14,   0,   0
-Frame_362F88:	dc.w $10
+Map_CNZMiniboss_Frame2:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $B,   4,   0, $23, $FF, $F0
@@ -62,7 +62,7 @@ Frame_362F88:	dc.w $10
 		dc.b  $1A,   4,   8, $23,   0,   0
 		dc.b  $22,   5,   0, $14, $FF, $F0
 		dc.b  $22,   5,   8, $14,   0,   0
-Frame_362FEA:	dc.w $10
+Map_CNZMiniboss_Frame3:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $C,   4,   0, $23, $FF, $F0
@@ -79,7 +79,7 @@ Frame_362FEA:	dc.w $10
 		dc.b  $20,   4,   8, $23,   0,   0
 		dc.b  $28,   5,   0, $14, $FF, $F0
 		dc.b  $28,   5,   8, $14,   0,   0
-Frame_36304C:	dc.w $10
+Map_CNZMiniboss_Frame4:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $D,   4,   0, $23, $FF, $F0
@@ -96,7 +96,7 @@ Frame_36304C:	dc.w $10
 		dc.b  $26,   4,   8, $23,   0,   0
 		dc.b  $2E,   5,   0, $14, $FF, $F0
 		dc.b  $2E,   5,   8, $14,   0,   0
-Frame_3630AE:	dc.w $10
+Map_CNZMiniboss_Frame5:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $E,   4,   0, $23, $FF, $F0
@@ -113,7 +113,7 @@ Frame_3630AE:	dc.w $10
 		dc.b  $2C,   4,   8, $23,   0,   0
 		dc.b  $34,   5,   0, $14, $FF, $F0
 		dc.b  $34,   5,   8, $14,   0,   0
-Frame_363110:	dc.w $10
+Map_CNZMiniboss_Frame6:	dc.w $10
 		dc.b  $F0,  $F,   0,   0, $FF, $E0
 		dc.b  $F0,  $F,   8,   0,   0,   0
 		dc.b   $F,   4,   0, $23, $FF, $F0
@@ -130,44 +130,44 @@ Frame_363110:	dc.w $10
 		dc.b  $32,   4,   8, $23,   0,   0
 		dc.b  $3A,   5,   0, $14, $FF, $F0
 		dc.b  $3A,   5,   8, $14,   0,   0
-Frame_363172:	dc.w 3
+Map_CNZMiniboss_Frame7:	dc.w 3
 		dc.b  $F8,   8,   0, $18, $FF, $E8
 		dc.b  $F8,   8,   8, $18,   0,   0
 		dc.b    0,   8,   0, $20, $FF, $F4
-Frame_363186:	dc.w 3
+Map_CNZMiniboss_Frame8:	dc.w 3
 		dc.b  $F8,  $C,   0, $1B, $FF, $EC
 		dc.b  $F8,   0,   0, $1F,   0,  $C
 		dc.b    0,   8,   8, $20, $FF, $F4
-Frame_36319A:	dc.w 3
+Map_CNZMiniboss_Frame9:	dc.w 3
 		dc.b  $F8,  $C,   8, $1B, $FF, $F4
 		dc.b  $F8,   0,   8, $1F, $FF, $EC
 		dc.b    0,   8,   0, $20, $FF, $F4
-Frame_3631AE:	dc.w 2
+Map_CNZMiniboss_Frame10:	dc.w 2
 		dc.b  $E8,   1,   0, $25, $FF, $F4
 		dc.b  $F8,  $B,   0, $27, $FF, $F4
-Frame_3631BC:	dc.w 2
+Map_CNZMiniboss_Frame11:	dc.w 2
 		dc.b  $E8,   3,   0, $33, $FF, $F4
 		dc.b  $F8,   7,   0, $37, $FF, $FC
-Frame_3631CA:	dc.w 1
+Map_CNZMiniboss_Frame12:	dc.w 1
 		dc.b  $F8,   5,   0, $3F, $FF, $F8
-Frame_3631D2:	dc.w 1
+Map_CNZMiniboss_Frame13:	dc.w 1
 		dc.b  $F8,   5,   0, $43, $FF, $F8
-Frame_3631DA:	dc.w 1
+Map_CNZMiniboss_Frame14:	dc.w 1
 		dc.b  $F8,   5,   0, $47, $FF, $F8
-Frame_3631E2:	dc.w 2
+Map_CNZMiniboss_Frame15:	dc.w 2
 		dc.b  $E8,   1,   8, $25,   0,   4
 		dc.b  $F8,  $B,   8, $27, $FF, $F4
-Frame_3631F0:	dc.w 2
+Map_CNZMiniboss_Frame16:	dc.w 2
 		dc.b  $E8,   3,   8, $33,   0,   4
 		dc.b  $F8,   7,   8, $37, $FF, $F4
-Frame_3631FE:	dc.w 0
-Frame_363200:	dc.w 1
+Map_CNZMiniboss_Frame17:	dc.w 0
+Map_CNZMiniboss_Frame18:	dc.w 1
 		dc.b  $F0,  $F,   0,   0, $FF, $F0
-Frame_363208:	dc.w 1
+Map_CNZMiniboss_Frame19:	dc.w 1
 		dc.b  $F0,  $F,   8,   0, $FF, $F0
-Frame_363210:	dc.w 2
+Map_CNZMiniboss_Frame20:	dc.w 2
 		dc.b  $FC,   4,   0, $23, $FF, $F0
 		dc.b  $FC,   4,   8, $23,   0,   0
-Frame_36321E:	dc.w 2
+Map_CNZMiniboss_Frame21:	dc.w 2
 		dc.b  $F8,   5,   0, $14, $FF, $F0
 		dc.b  $F8,   5,   8, $14,   0,   0
