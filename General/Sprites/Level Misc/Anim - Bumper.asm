1ce9e4046cabc20a20911f7fe54da427b77a03c2
Farrell <farrell@bluetoaster.net>
2026-06-16 19:25:02 +0100

    docs: label Bumper map and anim data

--- General/Sprites/Level Misc/Anim - Bumper.asm
+++ General/Sprites/Level Misc/Anim - Bumper.asm
@@ -1,6 +1,6 @@
 Ani_Bumper_:
-		dc.w byte_33098-Ani_Bumper_
-		dc.w byte_3309B-Ani_Bumper_
-byte_33098:	dc.b   $F,   0, $FF
-byte_3309B:	dc.b    3,   1,   0,   1, $FD,   0
+		dc.w Ani_Bumper_Idle-Ani_Bumper_
+		dc.w Ani_Bumper_Hit-Ani_Bumper_
+Ani_Bumper_Idle:	dc.b   $F,   0, $FF	; byte_33098
+Ani_Bumper_Hit:	dc.b    3,   1,   0,   1, $FD,   0	; byte_3309B
 		even
--- General/Sprites/Level Misc/Map - Bumper.asm
+++ General/Sprites/Level Misc/Map - Bumper.asm
@@ -1,9 +1,9 @@
 Map_Bumper_:
-		dc.w Frame_2322D2-Map_Bumper_
-		dc.w Frame_2322E0-Map_Bumper_
-Frame_2322D2:	dc.w 2
+		dc.w Map_Bumper_Frame0-Map_Bumper_
+		dc.w Map_Bumper_Frame1-Map_Bumper_
+Map_Bumper_Frame0:	dc.w 2	; Frame_2322D2
 		dc.b  $F0,   7,   0,   0, $FF, $F0
 		dc.b  $F0,   7,   8,   0,   0,   0
-Frame_2322E0:	dc.w 2
+Map_Bumper_Frame1:	dc.w 2	; Frame_2322E0
 		dc.b  $F0,   7,   0,   8, $FF, $F0
 		dc.b  $F0,   7,   8,   8,   0,   0
