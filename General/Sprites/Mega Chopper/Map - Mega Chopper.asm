ee94903dbb20b671e7b13e736fc5eb6eac96e3e4
Farrell <farrell@bluetoaster.net>
2026-06-16 17:34:28 +0100

    docs: label Mega Chopper map frames

--- General/Sprites/Mega Chopper/Map - Mega Chopper.asm
+++ General/Sprites/Mega Chopper/Map - Mega Chopper.asm
@@ -1,17 +1,17 @@
 Map_MegaChopper_:
-		dc.w Frame_360F2E-Map_MegaChopper_
-		dc.w Frame_360F3C-Map_MegaChopper_
-		dc.w Frame_360F4A-Map_MegaChopper_
-		dc.w Frame_360F58-Map_MegaChopper_
-Frame_360F2E:	dc.w 2
+		dc.w Map_MegaChopper_Frame0-Map_MegaChopper_
+		dc.w Map_MegaChopper_Frame1-Map_MegaChopper_
+		dc.w Map_MegaChopper_Frame2-Map_MegaChopper_
+		dc.w Map_MegaChopper_Frame3-Map_MegaChopper_
+Map_MegaChopper_Frame0:	dc.w 2	; Frame_360F2E
 		dc.b  $F8,   5,   0,   0, $FF, $F4
 		dc.b  $F8,   1,   0,   8,   0,   4
-Frame_360F3C:	dc.w 2
+Map_MegaChopper_Frame1:	dc.w 2	; Frame_360F3C
 		dc.b  $F8,   5,   0,   0, $FF, $F4
 		dc.b  $F8,   1,   0,  $A,   0,   4
-Frame_360F4A:	dc.w 2
+Map_MegaChopper_Frame2:	dc.w 2	; Frame_360F4A
 		dc.b  $F8,   5,   0,   4, $FF, $F4
 		dc.b  $F8,   1,   0,   8,   0,   4
-Frame_360F58:	dc.w 2
+Map_MegaChopper_Frame3:	dc.w 2	; Frame_360F58
 		dc.b  $F8,   5,   0,   4, $FF, $F4
 		dc.b  $F8,   1,   0,  $A,   0,   4
