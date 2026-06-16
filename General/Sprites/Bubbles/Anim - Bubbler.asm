f7650f3589b6c9152aacd0ebeaeb5b19ac04069e
Farrell <farrell@bluetoaster.net>
2026-06-16 19:05:29 +0100

    docs: label Bubbler anim and map data

--- General/Sprites/Bubbles/Anim - Bubbler.asm
+++ General/Sprites/Bubbles/Anim - Bubbler.asm
@@ -1,18 +1,18 @@
 Ani_Bubbler_:
-		dc.w byte_2FC90-Ani_Bubbler_
-		dc.w byte_2FC95-Ani_Bubbler_
-		dc.w byte_2FC9B-Ani_Bubbler_
-		dc.w byte_2FCA2-Ani_Bubbler_
-		dc.w byte_2FCA6-Ani_Bubbler_
-		dc.w byte_2FCA6-Ani_Bubbler_
-		dc.w byte_2FCA8-Ani_Bubbler_
-		dc.w byte_2FCA8-Ani_Bubbler_
-		dc.w byte_2FCAC-Ani_Bubbler_
-byte_2FC90:	dc.b   $E,   0,   1,   2, $FC
-byte_2FC95:	dc.b   $E,   1,   2,   3,   4, $FC
-byte_2FC9B:	dc.b   $E,   2,   3,   4,   5,   6, $FC
-byte_2FCA2:	dc.b    2,   5,   6, $FC
-byte_2FCA6:	dc.b    4, $FC
-byte_2FCA8:	dc.b    4,   7,   8, $FC
-byte_2FCAC:	dc.b   $F, $13, $14, $15, $FF
+		dc.w Ani_Bubbler_Script0-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script1-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script2-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script3-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script4-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script4-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script5-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script5-Ani_Bubbler_
+		dc.w Ani_Bubbler_Script6-Ani_Bubbler_
+Ani_Bubbler_Script0:	dc.b   $E,   0,   1,   2, $FC	; byte_2FC90
+Ani_Bubbler_Script1:	dc.b   $E,   1,   2,   3,   4, $FC	; byte_2FC95
+Ani_Bubbler_Script2:	dc.b   $E,   2,   3,   4,   5,   6, $FC	; byte_2FC9B
+Ani_Bubbler_Script3:	dc.b    2,   5,   6, $FC	; byte_2FCA2
+Ani_Bubbler_Script4:	dc.b    4, $FC	; byte_2FCA6
+Ani_Bubbler_Script5:	dc.b    4,   7,   8, $FC	; byte_2FCA8
+Ani_Bubbler_Script6:	dc.b   $F, $13, $14, $15, $FF	; byte_2FCAC
 		even
