ca5d4b4c89cfde0fab31beccd66593a3ed3da062
Farrell <farrell@bluetoaster.net>
2026-06-16 03:14:58 +0100

    docs: label AIZ rock mappings

--- Levels/LRZ/Misc Object Data/Map - Breakable Rock 2.asm
+++ Levels/LRZ/Misc Object Data/Map - Breakable Rock 2.asm
@@ -1,34 +1,34 @@
 Map_LRZBreakableRock2_:
-		dc.w word_20492-Map_LRZBreakableRock2_
-		dc.w word_204A0-Map_LRZBreakableRock2_
-		dc.w word_204AE-Map_LRZBreakableRock2_
-		dc.w word_204BC-Map_LRZBreakableRock2_
-		dc.w word_20492-Map_LRZBreakableRock2_
-		dc.w word_204A0-Map_LRZBreakableRock2_
-		dc.w word_204AE-Map_LRZBreakableRock2_
-		dc.w word_204BC-Map_LRZBreakableRock2_
-		dc.w word_204CA-Map_LRZBreakableRock2_
-		dc.w word_204E4-Map_LRZBreakableRock2_
-		dc.w word_204E4-Map_LRZBreakableRock2_
-		dc.w word_204CA-Map_LRZBreakableRock2_
-word_20492:	dc.w 2
+		dc.w Map_LRZBreakableRock2_Frame0-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame1-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame2-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame3-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame0-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame1-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame2-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame3-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame8-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame9-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame9-Map_LRZBreakableRock2_
+		dc.w Map_LRZBreakableRock2_Frame8-Map_LRZBreakableRock2_
+Map_LRZBreakableRock2_Frame0:	dc.w 2
 		dc.b  $F0,  $D,   0,   0, $FF, $F0
 		dc.b    0,  $D,   0,   8, $FF, $F0
-word_204A0:	dc.w 2
+Map_LRZBreakableRock2_Frame1:	dc.w 2
 		dc.b  $F0,  $D,   8,   0, $FF, $F0
 		dc.b    0,  $D,   8,   8, $FF, $F0
-word_204AE:	dc.w 2
+Map_LRZBreakableRock2_Frame2:	dc.w 2
 		dc.b  $F0,  $D, $18,   8, $FF, $F0
 		dc.b    0,  $D, $18,   0, $FF, $F0
-word_204BC:	dc.w 2
+Map_LRZBreakableRock2_Frame3:	dc.w 2
 		dc.b  $F0,  $D, $10,   8, $FF, $F0
 		dc.b    0,  $D, $10,   0, $FF, $F0
-word_204CA:	dc.w 4
+Map_LRZBreakableRock2_Frame8:	dc.w 4
 		dc.b  $E0,  $D,   0,   0, $FF, $F0
 		dc.b  $F0,  $D,   0,   8, $FF, $F0
 		dc.b    0,  $D,   8,   0, $FF, $F0
 		dc.b  $10,  $D,   8,   8, $FF, $F0
-word_204E4:	dc.w 4
+Map_LRZBreakableRock2_Frame9:	dc.w 4
 		dc.b  $F0,  $D,   0,   0, $FF, $E0
 		dc.b  $F0,  $D, $10,   8,   0,   0
 		dc.b    0,  $D,   0,   8, $FF, $E0
