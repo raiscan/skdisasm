f1277085cd92bed595e4e93f2fcb73cfc5b6e075
Farrell <farrell@bluetoaster.net>
2026-06-16 18:55:48 +0100

    docs: label Orbinaut map frames

--- General/Sprites/Orbinaut/Map - Orbinaut.asm
+++ General/Sprites/Orbinaut/Map - Orbinaut.asm
@@ -1,7 +1,7 @@
 Map_Orbinaut_:
-		dc.w Frame_3604A8-Map_Orbinaut_
-		dc.w Frame_3604B0-Map_Orbinaut_
-Frame_3604A8:	dc.w 1
+		dc.w Map_Orbinaut_Frame0-Map_Orbinaut_
+		dc.w Map_Orbinaut_Frame1-Map_Orbinaut_
+Map_Orbinaut_Frame0:	dc.w 1	; Frame_3604A8
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-Frame_3604B0:	dc.w 1
+Map_Orbinaut_Frame1:	dc.w 1	; Frame_3604B0
 		dc.b  $F8,   5,   0,   4, $FF, $F8
