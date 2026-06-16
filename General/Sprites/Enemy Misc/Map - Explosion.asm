0df024a2f23490fa1806498c7960f6eb1f66b201
Farrell <farrell@bluetoaster.net>
2026-06-16 20:35:30 +0100

    docs: label explosion map data

--- General/Sprites/Enemy Misc/Map - Explosion.asm
+++ General/Sprites/Enemy Misc/Map - Explosion.asm
@@ -1,16 +1,16 @@
 Map_Explosion_:
-		dc.w word_1E762-Map_Explosion_
-		dc.w word_1E76A-Map_Explosion_
-		dc.w word_1E772-Map_Explosion_
-		dc.w word_1E77A-Map_Explosion_
-		dc.w word_1E782-Map_Explosion_
-word_1E762:	dc.w 1
+		dc.w Map_Explosion_Frame0-Map_Explosion_
+		dc.w Map_Explosion_Frame1-Map_Explosion_
+		dc.w Map_Explosion_Frame2-Map_Explosion_
+		dc.w Map_Explosion_Frame3-Map_Explosion_
+		dc.w Map_Explosion_Frame4-Map_Explosion_
+Map_Explosion_Frame0:	dc.w 1	; word_1E762
 		dc.b  $F8,   5,   0,   0, $FF, $F8
-word_1E76A:	dc.w 1
+Map_Explosion_Frame1:	dc.w 1	; word_1E76A
 		dc.b  $F0,  $F,   0,   4, $FF, $F0
-word_1E772:	dc.w 1
+Map_Explosion_Frame2:	dc.w 1	; word_1E772
 		dc.b  $F0,  $F,   0, $14, $FF, $F0
-word_1E77A:	dc.w 1
+Map_Explosion_Frame3:	dc.w 1	; word_1E77A
 		dc.b  $F0,  $F,   0, $24, $FF, $F0
-word_1E782:	dc.w 1
+Map_Explosion_Frame4:	dc.w 1	; word_1E782
 		dc.b  $F0,  $F,   0, $34, $FF, $F0
