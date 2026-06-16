2510b9f34c9031e30f50f68f2c7fc5a746ed617d
Farrell <farrell@bluetoaster.net>
2026-06-16 10:22:58 +0100

    docs: label MGZ boss map frames

--- Levels/MGZ/Misc Object Data/Map - End Boss.asm
+++ Levels/MGZ/Misc Object Data/Map - End Boss.asm
@@ -1,177 +1,177 @@
 Map_MGZEndBoss_:
-		dc.w Frame_36266A-Map_MGZEndBoss_
-		dc.w Frame_36268A-Map_MGZEndBoss_
-		dc.w Frame_362698-Map_MGZEndBoss_
-		dc.w Frame_3626A6-Map_MGZEndBoss_
-		dc.w Frame_3626C0-Map_MGZEndBoss_
-		dc.w Frame_3626D4-Map_MGZEndBoss_
-		dc.w Frame_3626DC-Map_MGZEndBoss_
-		dc.w Frame_3626E4-Map_MGZEndBoss_
-		dc.w Frame_3626EC-Map_MGZEndBoss_
-		dc.w Frame_3626FA-Map_MGZEndBoss_
-		dc.w Frame_362702-Map_MGZEndBoss_
-		dc.w Frame_36270A-Map_MGZEndBoss_
-		dc.w Frame_362712-Map_MGZEndBoss_
-		dc.w Frame_36271A-Map_MGZEndBoss_
-		dc.w Frame_362722-Map_MGZEndBoss_
-		dc.w Frame_36272A-Map_MGZEndBoss_
-		dc.w Frame_362738-Map_MGZEndBoss_
-		dc.w Frame_362746-Map_MGZEndBoss_
-		dc.w Frame_362754-Map_MGZEndBoss_
-		dc.w Frame_36275C-Map_MGZEndBoss_
-		dc.w Frame_36276A-Map_MGZEndBoss_
-		dc.w Frame_362772-Map_MGZEndBoss_
-		dc.w Frame_36277A-Map_MGZEndBoss_
-		dc.w Frame_362782-Map_MGZEndBoss_
-		dc.w Frame_36278A-Map_MGZEndBoss_
-		dc.w Frame_362792-Map_MGZEndBoss_
-		dc.w Frame_36279A-Map_MGZEndBoss_
-		dc.w Frame_3627A2-Map_MGZEndBoss_
-		dc.w Frame_3627AA-Map_MGZEndBoss_
-		dc.w Frame_3627B2-Map_MGZEndBoss_
-		dc.w Frame_3627CC-Map_MGZEndBoss_
-		dc.w Frame_3627E0-Map_MGZEndBoss_
-		dc.w Frame_3627FA-Map_MGZEndBoss_
-		dc.w Frame_362808-Map_MGZEndBoss_
-		dc.w Frame_362816-Map_MGZEndBoss_
-		dc.w Frame_362824-Map_MGZEndBoss_
-		dc.w Frame_362832-Map_MGZEndBoss_
-		dc.w Frame_36283A-Map_MGZEndBoss_
-		dc.w Frame_362842-Map_MGZEndBoss_
-		dc.w Frame_36284A-Map_MGZEndBoss_
-		dc.w Frame_362852-Map_MGZEndBoss_
-		dc.w Frame_36285A-Map_MGZEndBoss_
-		dc.w Frame_362862-Map_MGZEndBoss_
-		dc.w Frame_36286A-Map_MGZEndBoss_
-		dc.w Frame_362872-Map_MGZEndBoss_
-		dc.w Frame_36287A-Map_MGZEndBoss_
-		dc.w Frame_362882-Map_MGZEndBoss_
-		dc.w Frame_36288A-Map_MGZEndBoss_
-		dc.w Frame_362898-Map_MGZEndBoss_
-Frame_36266A:	dc.w 5
+		dc.w Map_MGZEndBoss_Frame0-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame1-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame2-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame3-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame4-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame5-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame6-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame7-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame8-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame9-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame10-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame11-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame12-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame13-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame14-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame15-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame16-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame17-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame18-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame19-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame20-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame21-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame22-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame23-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame24-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame25-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame26-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame27-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame28-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame29-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame30-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame31-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame32-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame33-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame34-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame35-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame36-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame37-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame38-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame39-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame40-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame41-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame42-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame43-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame44-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame45-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame46-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame47-Map_MGZEndBoss_
+		dc.w Map_MGZEndBoss_Frame48-Map_MGZEndBoss_
+Map_MGZEndBoss_Frame0:	dc.w 5	; Frame_36266A
 		dc.b  $E0,  $E,   0,   0, $FF, $FC
 		dc.b  $F8,   1,   0,  $C, $FF, $DC
 		dc.b  $F8,  $F,   0,  $E, $FF, $E4
 		dc.b  $F8,  $F,   0, $1E,   0,   4
 		dc.b  $18,  $C,   0, $2E, $FF, $F4
-Frame_36268A:	dc.w 2
+Map_MGZEndBoss_Frame1:	dc.w 2	; Frame_36268A
 		dc.b  $F0,   6,   0, $32, $FF, $F0
 		dc.b    8,  $C,   0, $38, $FF, $F0
-Frame_362698:	dc.w 2
+Map_MGZEndBoss_Frame2:	dc.w 2	; Frame_362698
 		dc.b  $F4,  $E,   0, $3C, $FF, $E8
 		dc.b    4,   4,   0, $48,   0,   8
-Frame_3626A6:	dc.w 4
+Map_MGZEndBoss_Frame3:	dc.w 4	; Frame_3626A6
 		dc.b  $E8,  $A,   0, $4A,   0,   0
 		dc.b  $F0,   5,   0, $53, $FF, $F0
 		dc.b    0,  $A,   0, $57, $FF, $E8
 		dc.b    0,   5,   0, $60,   0,   0
-Frame_3626C0:	dc.w 3
+Map_MGZEndBoss_Frame4:	dc.w 3	; Frame_3626C0
 		dc.b  $EC,  $C,   0, $64, $FF, $F0
 		dc.b  $F4,  $D,   0, $68, $FF, $F0
 		dc.b    4,  $D,   0, $68, $FF, $F0
-Frame_3626D4:	dc.w 1
+Map_MGZEndBoss_Frame5:	dc.w 1	; Frame_3626D4
 		dc.b  $F0,  $F,   0, $70, $FF, $F0
-Frame_3626DC:	dc.w 1
+Map_MGZEndBoss_Frame6:	dc.w 1	; Frame_3626DC
 		dc.b  $F8,   5,   0, $80, $FF, $F8
-Frame_3626E4:	dc.w 1
+Map_MGZEndBoss_Frame7:	dc.w 1	; Frame_3626E4
 		dc.b  $F4,  $A,   0, $84, $FF, $F4
-Frame_3626EC:	dc.w 2
+Map_MGZEndBoss_Frame8:	dc.w 2	; Frame_3626EC
 		dc.b  $F4,   6,   0, $8D, $FF, $F4
 		dc.b  $FC,   0,   0, $93,   0,   4
-Frame_3626FA:	dc.w 1
+Map_MGZEndBoss_Frame9:	dc.w 1	; Frame_3626FA
 		dc.b  $F4,  $A,   0, $94, $FF, $F4
-Frame_362702:	dc.w 1
+Map_MGZEndBoss_Frame10:	dc.w 1	; Frame_362702
 		dc.b  $F4,  $A,   0, $9D, $FF, $F4
-Frame_36270A:	dc.w 1
+Map_MGZEndBoss_Frame11:	dc.w 1	; Frame_36270A
 		dc.b  $F4,  $A,   0, $A6, $FF, $F4
-Frame_362712:	dc.w 1
+Map_MGZEndBoss_Frame12:	dc.w 1	; Frame_362712
 		dc.b  $F4,  $A,   0, $AF, $FF, $F4
-Frame_36271A:	dc.w 1
+Map_MGZEndBoss_Frame13:	dc.w 1	; Frame_36271A
 		dc.b  $F4,  $A,   0, $B8, $FF, $F4
-Frame_362722:	dc.w 1
+Map_MGZEndBoss_Frame14:	dc.w 1	; Frame_362722
 		dc.b  $F4,  $A,   0, $C1, $FF, $F4
-Frame_36272A:	dc.w 2
+Map_MGZEndBoss_Frame15:	dc.w 2	; Frame_36272A
 		dc.b  $F4,   0,   0, $CA, $FF, $FC
 		dc.b  $FC,   9,   0, $CB, $FF, $F4
-Frame_362738:	dc.w 2
+Map_MGZEndBoss_Frame16:	dc.w 2	; Frame_362738
 		dc.b  $F4,   0,   0, $D1, $FF, $FC
 		dc.b  $FC,   9,   0, $D2, $FF, $F4
-Frame_362746:	dc.w 2
+Map_MGZEndBoss_Frame17:	dc.w 2	; Frame_362746
 		dc.b  $F4,   0,   0, $D8, $FF, $FC
 		dc.b  $FC,   9,   0, $D9, $FF, $F4
-Frame_362754:	dc.w 1
+Map_MGZEndBoss_Frame18:	dc.w 1	; Frame_362754
 		dc.b  $F4,  $A,   0, $DF, $FF, $F4
-Frame_36275C:	dc.w 2
+Map_MGZEndBoss_Frame19:	dc.w 2	; Frame_36275C
 		dc.b  $F4,   6,   0, $E8, $FF, $F4
 		dc.b  $FC,   0,   0, $EE,   0,   4
-Frame_36276A:	dc.w 1
+Map_MGZEndBoss_Frame20:	dc.w 1	; Frame_36276A
 		dc.b  $F4,  $A,   0, $EF, $FF, $F4
-Frame_362772:	dc.w 1
+Map_MGZEndBoss_Frame21:	dc.w 1	; Frame_362772
 		dc.b  $F8,   5,   0, $F8, $FF, $F8
-Frame_36277A:	dc.w 1
+Map_MGZEndBoss_Frame22:	dc.w 1	; Frame_36277A
 		dc.b  $F8,   5,   0, $FC, $FF, $F8
-Frame_362782:	dc.w 1
+Map_MGZEndBoss_Frame23:	dc.w 1	; Frame_362782
 		dc.b  $F8,   5,   1,   0, $FF, $F8
-Frame_36278A:	dc.w 1
+Map_MGZEndBoss_Frame24:	dc.w 1	; Frame_36278A
 		dc.b  $F8,   5,   1,   4, $FF, $F8
-Frame_362792:	dc.w 1
+Map_MGZEndBoss_Frame25:	dc.w 1	; Frame_362792
 		dc.b  $F8,   5,   1,   8, $FF, $F8
-Frame_36279A:	dc.w 1
+Map_MGZEndBoss_Frame26:	dc.w 1	; Frame_36279A
 		dc.b  $F4,  $A,   1,  $C, $FF, $F4
-Frame_3627A2:	dc.w 1
+Map_MGZEndBoss_Frame27:	dc.w 1	; Frame_3627A2
 		dc.b  $F8,   5,   1, $15, $FF, $F8
-Frame_3627AA:	dc.w 1
+Map_MGZEndBoss_Frame28:	dc.w 1	; Frame_3627AA
 		dc.b  $F4,   2,   1, $19, $FF, $FC
-Frame_3627B2:	dc.w 4
+Map_MGZEndBoss_Frame29:	dc.w 4	; Frame_3627B2
 		dc.b  $E0,  $F,   1, $16, $FF, $E0
 		dc.b  $E0,  $F,   1, $26,   0,   0
 		dc.b    0,  $F,   1, $36, $FF, $E0
 		dc.b    0,  $F,   1, $46,   0,   0
-Frame_3627CC:	dc.w 3
+Map_MGZEndBoss_Frame30:	dc.w 3	; Frame_3627CC
 		dc.b   $C,  $C, $10, $64, $FF, $F0
 		dc.b  $FC,  $D, $10, $68, $FF, $F0
 		dc.b  $EC,  $D, $10, $68, $FF, $F0
-Frame_3627E0:	dc.w 4
+Map_MGZEndBoss_Frame31:	dc.w 4	; Frame_3627E0
 		dc.b    0,  $A, $10, $4A,   0,   0
 		dc.b    0,   5, $10, $53, $FF, $F0
 		dc.b  $E8,  $A, $10, $57, $FF, $E8
 		dc.b  $F0,   5, $10, $60,   0,   0
-Frame_3627FA:	dc.w 2
+Map_MGZEndBoss_Frame32:	dc.w 2	; Frame_3627FA
 		dc.b    4,   0, $10, $CA, $FF, $FC
 		dc.b  $F4,   9, $10, $CB, $FF, $F4
-Frame_362808:	dc.w 2
+Map_MGZEndBoss_Frame33:	dc.w 2	; Frame_362808
 		dc.b    4,   0, $10, $D1, $FF, $FC
 		dc.b  $F4,   9, $10, $D2, $FF, $F4
-Frame_362816:	dc.w 2
+Map_MGZEndBoss_Frame34:	dc.w 2	; Frame_362816
 		dc.b    4,   0, $10, $D8, $FF, $FC
 		dc.b  $F4,   9, $10, $D9, $FF, $F4
-Frame_362824:	dc.w 2
+Map_MGZEndBoss_Frame35:	dc.w 2	; Frame_362824
 		dc.b  $F4,   6, $10, $8D, $FF, $F4
 		dc.b  $FC,   0, $10, $93,   0,   4
-Frame_362832:	dc.w 1
+Map_MGZEndBoss_Frame36:	dc.w 1	; Frame_362832
 		dc.b  $F4,  $A,   9,  $C, $FF, $F4
-Frame_36283A:	dc.w 1
+Map_MGZEndBoss_Frame37:	dc.w 1	; Frame_36283A
 		dc.b  $F4,  $A, $19,  $C, $FF, $F4
-Frame_362842:	dc.w 1
+Map_MGZEndBoss_Frame38:	dc.w 1	; Frame_362842
 		dc.b  $F4,  $A, $18, $AF, $FF, $F4
-Frame_36284A:	dc.w 1
+Map_MGZEndBoss_Frame39:	dc.w 1	; Frame_36284A
 		dc.b  $F4,  $A, $18, $B8, $FF, $F4
-Frame_362852:	dc.w 1
+Map_MGZEndBoss_Frame40:	dc.w 1	; Frame_362852
 		dc.b  $F4,  $A, $18, $C1, $FF, $F4
-Frame_36285A:	dc.w 1
+Map_MGZEndBoss_Frame41:	dc.w 1	; Frame_36285A
 		dc.b  $FC,   8,   1, $1C, $FF, $F4
-Frame_362862:	dc.w 1
+Map_MGZEndBoss_Frame42:	dc.w 1	; Frame_362862
 		dc.b  $F8,   5,   8, $F8, $FF, $F8
-Frame_36286A:	dc.w 1
+Map_MGZEndBoss_Frame43:	dc.w 1	; Frame_36286A
 		dc.b  $F8,   5, $18, $F8, $FF, $F8
-Frame_362872:	dc.w 1
+Map_MGZEndBoss_Frame44:	dc.w 1	; Frame_362872
 		dc.b  $FC,   8, $11, $1C, $FF, $F4
-Frame_36287A:	dc.w 1
+Map_MGZEndBoss_Frame45:	dc.w 1	; Frame_36287A
 		dc.b  $F4,   2,   9, $19, $FF, $FC
-Frame_362882:	dc.w 1
+Map_MGZEndBoss_Frame46:	dc.w 1	; Frame_362882
 		dc.b  $F4,  $E,   0,   0, $FF, $F0
-Frame_36288A:	dc.w 2
+Map_MGZEndBoss_Frame47:	dc.w 2	; Frame_36288A
 		dc.b  $F0,   1,   0,  $C, $FF, $EC
 		dc.b  $F0,  $F,   0,  $E, $FF, $F4
-Frame_362898:	dc.w 1
+Map_MGZEndBoss_Frame48:	dc.w 1	; Frame_362898
 		dc.b  $F0,  $F,   0, $1E, $FF, $F0
--- Levels/MGZ/Misc Object Data/Map - Miniboss.asm
+++ Levels/MGZ/Misc Object Data/Map - Miniboss.asm
@@ -1,38 +1,38 @@
 Map_MGZMiniboss_:
-		dc.w Frame_361980-Map_MGZMiniboss_
-		dc.w Frame_3619A6-Map_MGZMiniboss_
-		dc.w Frame_3619CC-Map_MGZMiniboss_
-		dc.w Frame_3619F2-Map_MGZMiniboss_
-		dc.w Frame_3619FA-Map_MGZMiniboss_
-		dc.w Frame_361A08-Map_MGZMiniboss_
-		dc.w Frame_361A10-Map_MGZMiniboss_
-Frame_361980:	dc.w 6
+		dc.w Map_MGZMiniboss_Frame0-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame1-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame2-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame3-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame4-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame5-Map_MGZMiniboss_
+		dc.w Map_MGZMiniboss_Frame6-Map_MGZMiniboss_
+Map_MGZMiniboss_Frame0:	dc.w 6	; Frame_361980
 		dc.b  $F4,  $A,   0,   0, $FF, $D8
 		dc.b  $F4,   6,   0,   9, $FF, $F0
 		dc.b  $F4,   6,   8,   9,   0,   0
 		dc.b  $F4,  $A,   8,   0,   0, $10
 		dc.b  $DE,   6,   0,  $F, $FF, $DC
 		dc.b  $DE,   6,   0,  $F,   0, $14
-Frame_3619A6:	dc.w 6
+Map_MGZMiniboss_Frame1:	dc.w 6	; Frame_3619A6
 		dc.b  $F4,  $A,   0,   0, $FF, $D8
 		dc.b  $F4,   6,   0,   9, $FF, $F0
 		dc.b  $F4,   6,   8,   9,   0,   0
 		dc.b  $F4,  $A,   8,   0,   0, $10
 		dc.b  $DE,   6,   0, $15, $FF, $DC
 		dc.b  $DE,   6,   0, $15,   0, $14
-Frame_3619CC:	dc.w 6
+Map_MGZMiniboss_Frame2:	dc.w 6	; Frame_3619CC
 		dc.b  $F4,  $A,   0,   0, $FF, $D8
 		dc.b  $F4,   6,   0,   9, $FF, $F0
 		dc.b  $F4,   6,   8,   9,   0,   0
 		dc.b  $F4,  $A,   8,   0,   0, $10
 		dc.b  $DE,   6,   0, $1B, $FF, $DC
 		dc.b  $DE,   6,   0, $1B,   0, $14
-Frame_3619F2:	dc.w 1
+Map_MGZMiniboss_Frame3:	dc.w 1	; Frame_3619F2
 		dc.b  $F4,  $A,   0,   0, $FF, $F4
-Frame_3619FA:	dc.w 2
+Map_MGZMiniboss_Frame4:	dc.w 2	; Frame_3619FA
 		dc.b  $F4,   6,   0,   9, $FF, $F0
 		dc.b  $F4,   6,   8,   9,   0,   0
-Frame_361A08:	dc.w 1
+Map_MGZMiniboss_Frame5:	dc.w 1	; Frame_361A08
 		dc.b  $F4,  $A,   8,   0, $FF, $F4
-Frame_361A10:	dc.w 1
+Map_MGZMiniboss_Frame6:	dc.w 1	; Frame_361A10
 		dc.b  $F4,   6,   0,  $F, $FF, $F8
