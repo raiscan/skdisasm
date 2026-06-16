3c3ea46574dafa6866e471a6230b3f3bcc947203
Farrell <farrell@bluetoaster.net>
2026-06-16 17:01:15 +0100

    docs: label MHZ breakable wall map frames

--- Levels/MHZ/Misc Object Data/Map - Breakable Wall.asm
+++ Levels/MHZ/Misc Object Data/Map - Breakable Wall.asm
@@ -1,12 +1,12 @@
 Map_MHZBreakableWall_:
-		dc.w word_21BD2-Map_MHZBreakableWall_
-		dc.w word_21BEC-Map_MHZBreakableWall_
-word_21BD2:	dc.w 4
+		dc.w Map_MHZBreakableWall_Frame0-Map_MHZBreakableWall_
+		dc.w Map_MHZBreakableWall_Frame1-Map_MHZBreakableWall_
+Map_MHZBreakableWall_Frame0:	dc.w 4	; word_21BD2
 		dc.b  $E0,  $D,   0,   0, $FF, $F0
 		dc.b  $F0,  $D, $18,   0, $FF, $F0
 		dc.b    0,  $D,   0,   0, $FF, $F0
 		dc.b  $10,  $D, $18,   0, $FF, $F0
-word_21BEC:	dc.w 8
+Map_MHZBreakableWall_Frame1:	dc.w 8	; word_21BEC
 		dc.b  $E0,   5,   0,   0, $FF, $F0
 		dc.b  $E0,   5,   0,   4,   0,   0
 		dc.b  $F0,   5, $18,   4, $FF, $F0
