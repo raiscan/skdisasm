c8022c3c52368dc73205b62fb24337811e728de4
Farrell <farrell@bluetoaster.net>
2026-06-16 21:46:50 +0100

    docs: label air countdown animation data

--- General/Sprites/Bubbles/Anim - Air Countdown.asm
+++ General/Sprites/Bubbles/Anim - Air Countdown.asm
@@ -1,31 +1,31 @@
 Ani_AirCountdown_:
-		dc.w byte_186E2-Ani_AirCountdown_
-		dc.w byte_186EB-Ani_AirCountdown_
-		dc.w byte_186F4-Ani_AirCountdown_
-		dc.w byte_186FD-Ani_AirCountdown_
-		dc.w byte_18706-Ani_AirCountdown_
-		dc.w byte_1870F-Ani_AirCountdown_
-		dc.w byte_18718-Ani_AirCountdown_
-		dc.w byte_1871D-Ani_AirCountdown_
-		dc.w byte_18725-Ani_AirCountdown_
-		dc.w byte_1872D-Ani_AirCountdown_
-		dc.w byte_18735-Ani_AirCountdown_
-		dc.w byte_1873D-Ani_AirCountdown_
-		dc.w byte_18745-Ani_AirCountdown_
-		dc.w byte_1874D-Ani_AirCountdown_
-		dc.w byte_1874F-Ani_AirCountdown_
-byte_186E2:	dc.b    5,   0,   1,   2,   3,   4,   9,  $D, $FC
-byte_186EB:	dc.b    5,   0,   1,   2,   3,   4,  $C, $12, $FC
-byte_186F4:	dc.b    5,   0,   1,   2,   3,   4,  $C, $11, $FC
-byte_186FD:	dc.b    5,   0,   1,   2,   3,   4,  $B, $10, $FC
-byte_18706:	dc.b    5,   0,   1,   2,   3,   4,  $C,  $F, $FC
-byte_1870F:	dc.b    5,   0,   1,   2,   3,   4,  $A,  $E, $FC
-byte_18718:	dc.b   $E,   0,   1,   2, $FC
-byte_1871D:	dc.b    7, $16,  $D, $16,  $D, $16,  $D, $FC
-byte_18725:	dc.b    7, $16, $12, $16, $12, $16, $12, $FC
-byte_1872D:	dc.b    7, $16, $11, $16, $11, $16, $11, $FC
-byte_18735:	dc.b    7, $16, $10, $16, $10, $16, $10, $FC
-byte_1873D:	dc.b    7, $16,  $F, $16,  $F, $16,  $F, $FC
-byte_18745:	dc.b    7, $16,  $E, $16,  $E, $16,  $E, $FC
-byte_1874D:	dc.b   $E, $FC
-byte_1874F:	dc.b   $E,   1,   2,   3,   4, $FC,   0
+		dc.w Ani_AirCountdown_Number0-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number1-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number2-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number3-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number4-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number5-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_BubbleRise-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number0Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number1Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number2Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number3Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number4Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Number5Pop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_BubblePop-Ani_AirCountdown_
+		dc.w Ani_AirCountdown_Script14-Ani_AirCountdown_
+Ani_AirCountdown_Number0:	dc.b    5,   0,   1,   2,   3,   4,   9,  $D, $FC	; byte_186E2
+Ani_AirCountdown_Number1:	dc.b    5,   0,   1,   2,   3,   4,  $C, $12, $FC	; byte_186EB
+Ani_AirCountdown_Number2:	dc.b    5,   0,   1,   2,   3,   4,  $C, $11, $FC	; byte_186F4
+Ani_AirCountdown_Number3:	dc.b    5,   0,   1,   2,   3,   4,  $B, $10, $FC	; byte_186FD
+Ani_AirCountdown_Number4:	dc.b    5,   0,   1,   2,   3,   4,  $C,  $F, $FC	; byte_18706
+Ani_AirCountdown_Number5:	dc.b    5,   0,   1,   2,   3,   4,  $A,  $E, $FC	; byte_1870F
+Ani_AirCountdown_BubbleRise:	dc.b   $E,   0,   1,   2, $FC	; byte_18718
+Ani_AirCountdown_Number0Pop:	dc.b    7, $16,  $D, $16,  $D, $16,  $D, $FC	; byte_1871D
+Ani_AirCountdown_Number1Pop:	dc.b    7, $16, $12, $16, $12, $16, $12, $FC	; byte_18725
+Ani_AirCountdown_Number2Pop:	dc.b    7, $16, $11, $16, $11, $16, $11, $FC	; byte_1872D
+Ani_AirCountdown_Number3Pop:	dc.b    7, $16, $10, $16, $10, $16, $10, $FC	; byte_18735
+Ani_AirCountdown_Number4Pop:	dc.b    7, $16,  $F, $16,  $F, $16,  $F, $FC	; byte_1873D
+Ani_AirCountdown_Number5Pop:	dc.b    7, $16,  $E, $16,  $E, $16,  $E, $FC	; byte_18745
+Ani_AirCountdown_BubblePop:	dc.b   $E, $FC	; byte_1874D
+Ani_AirCountdown_Script14:	dc.b   $E,   1,   2,   3,   4, $FC,   0	; byte_1874F
